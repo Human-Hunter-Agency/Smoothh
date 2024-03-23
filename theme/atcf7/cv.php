cd518789b5a67f157ca642cf7f62b9ed0f5e66d5
Kamil <k.medrala99@gmail.com>
2024-03-23 14:29:29 +0100

    [form-cv] basic cv form component

--- theme/atcf7/cv.php
+++ theme/atcf7/cv.php
@@ -9,17 +9,17 @@ if (!defined('ABSPATH')) {
 
 <div class="form-cv">
   <div class="mb-[18px] lg:mb-[42px] flex flex-col lg:flex-row gap-[18px]">
-      [text* your-name id:your-name placeholder "Imię i nazwisko"]
+      [text* your-name placeholder "Imię i nazwisko"]
 
-      [tel* your-phone id:your-phone placeholder "Telefon"]
+      [tel* your-phone placeholder "Telefon"]
       
-      [email* your-email id:your-email placeholder "Adres e-mail"]
+      [email* your-email placeholder "Adres e-mail"]
       
-      [file* your-file id:your-file placeholder "Załącz CV"]
+      [file* your-file placeholder "Załącz CV"]
   </div>
 
   <div class="w-full mb-[30px]">
-    [textarea* your-message id:your-message x3 placeholder "Wiadomość..."]
+    [textarea* your-message x3 placeholder "Wiadomość..."]
   </div>
 
   <div class="text-center">
