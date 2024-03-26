2b7e02382a6003e116e3e264a21ef7403090b91e
Kamil <k.medrala99@gmail.com>
2024-03-26 15:39:11 +0100

    [tiles-icons] grid toggle

--- theme/flexible-content/sections/tiles-icons.php
+++ theme/flexible-content/sections/tiles-icons.php
@@ -4,7 +4,7 @@
     $header = $args['header'];
     $description = $args['description'];
     $tiles_list = $args['tiles_list'];
-    $is_grid = $args['grid_3'];
+    $grid = $args['grid'];
     $text_below = $args['text_below'];
     $button = $args['button'];
 
@@ -28,19 +28,28 @@
     <div class="relative z-0 w-full overflow-hidden !pb-5">
         <?php if ($tiles_list) : ?>
             <div class="swiper !container !overflow-visible" data-js="swiper-tiles-mobile">
-                <div class="swiper-wrapper xl:!transform-none <?php if ($is_grid) : ?> xl:px-[100px] xl:box-border xl:flex-wrap xl:!gap-x-[49px] xl:!gap-y-20 xl:justify-center [&_.swiper-slide]:basis-[calc(33%_-_33px)]  <?php else : ?> xl:!gap-[60px] <?php endif; ?>">
+                <div class="swiper-wrapper xl:!transform-none 
+                    <?php if ($grid == 3) : ?> xl:px-[100px] xl:box-border xl:flex-wrap xl:!gap-x-[49px] xl:!gap-y-20 xl:justify-center [&_.swiper-slide]:basis-[calc(33%_-_33px)] 
+                    <?php elseif ($grid == 5) : ?> xl:flex-wrap xl:!gap-y-[60px] xl:!gap-x-10 xl:justify-center [&_.swiper-slide]:basis-[calc(20%_-_48px)]  
+                    <?php else : ?> xl:!gap-[60px] 
+                    <?php endif; ?>">
+                    
                     <?php foreach($tiles_list as $tile) : ?>
                         <div class="swiper-slide xl:flex-1 !flex items-center flex-col text-center">
                             <?php if ($tile['icon'] && $tile['icon']['url'] ) : ?>
-                                <div class="w-full border-2 border-[#EFEFEF] rounded-2xl <?php if ($is_grid) : ?> px-4 py-8  md:py-12 md:px-6 lg:py-[77px] md:mb-14  <?php else : ?> p-4 md:p-6  <?php endif; ?>">
+                                <div class="w-full border-2 border-[#EFEFEF] rounded-2xl 
+                                    <?php if ($grid == 3) : ?> px-4 py-8  md:py-12 md:px-6 lg:py-[77px] md:mb-14  
+                                    <?php elseif ($grid == 5) : ?> p-4 md:p-6 mb-9
+                                    <?php else : ?> p-4 md:p-6 mb-5  
+                                    <?php endif; ?>">
                                     <img src="<?php echo $tile['icon']['url']; ?>" class="mx-auto object-contain size-16 md:size-24 xl:size-[125px]" >
                                 </div>
                             <?php endif; ?>
                             <?php if ($tile['title']) : ?>
-                                <h3 class="text-base md:text-xl text-primary mb-2.5 min-h-14 px-5"><?php echo esc_html($tile['title']); ?></h3>
+                                <h3 class="text-base md:text-xl text-primary mb-2.5 min-h-14 px-5 font-semibold"><?php echo esc_html($tile['title']); ?></h3>
                             <?php endif; ?>
                             <?php if ($tile['description']) : ?>
-                                <div class="prose-smooth <?php if (!$tile['title']) : ?> prose-base md:prose-xl <?php else : ?> prose-sm md:prose-base <?php endif; ?>"><?php echo $tile['description']; ?></div>
+                                <div class="prose-smooth prose-strong:font-semibold <?php if (!$tile['title']) : ?> prose-base md:prose-xl <?php else : ?> prose-sm md:prose-base <?php endif; ?>"><?php echo $tile['description']; ?></div>
                             <?php endif; ?>
                         </div>
                     <?php endforeach; ?>
