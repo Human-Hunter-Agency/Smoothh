466b2b0cb9c82b255ba52f421f87773794cfa8f6
Kamil <k.medrala99@gmail.com>
2024-03-29 14:24:47 +0100

    [blog post] cat list styles

--- theme/single.php
+++ theme/single.php
@@ -29,12 +29,12 @@ get_header();
 </section><!-- #primary -->
 <section class="container mt-10 md:mt-20">
 	<h2 class="text-3xl md:text-5xl text-bold font-bold mb-5 md:mb-10 text-center">Zobacz pozostałe materiały eksperckie z naszego bloga</h2>
-	<ul class="px-2.5 py-2 rounded-full bg-[#F2F2F2] flex items-center gap-2 max-w-screen-md w-fit flex-wrap mx-auto mb-5 md:mb-10">
+	<ul class="p-2 rounded-2xl bg-[#F2F2F2] flex items-center gap-2 max-w-screen-md w-fit flex-wrap mx-auto mb-5 md:mb-10">
 		<?php
 		$categories = get_categories();
 		foreach ($categories as $category) : ?>
 			<li class="flex-1">
-				<button class="w-full px-5 py-2 font-semibold rounded-2xl text-base bg-white text-primary hover:bg-primary/10 transition duration-300 whitespace-nowrap"><?php echo $category->name ?></button>
+				<button class="w-full px-5 py-2 font-semibold rounded-[10px] text-base bg-white text-primary hover:bg-primary/10 transition duration-300 whitespace-nowrap"><?php echo $category->name ?></button>
 			</li>
 		<?php endforeach; ?>
 	</ul>
