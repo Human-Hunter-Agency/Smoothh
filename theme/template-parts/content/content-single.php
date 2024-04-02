3e29b14f2d1cdeeb7485ef829738a4885320c735
Kamil <k.medrala99@gmail.com>
2024-04-02 14:16:58 +0200

    loading posts test

--- theme/template-parts/content/content-single.php
+++ theme/template-parts/content/content-single.php
@@ -96,23 +96,25 @@
 		);
 	?>
 		<div class="w-full relative min-h-32 <?php if ($i !== 0) {echo 'hidden';} ?>" data-js="<?php echo 'tab-content-' . $category->term_id; ?>">
-			<?php 
-			if ($i === 0) :
+			<ul>
+			<?php if ($i === 0) :
 				++$i;
 				$posts = get_posts($args);
 				foreach ($posts as $post) : ?>
-				<div class="post-tile">
+				<li class="post-tile">
 					<h4><?php echo $post->post_title; ?></h4>
 					<p><?php echo get_the_excerpt($post->ID); ?></p>
-				</div>
+				</li>
 			
 			<?php
 				endforeach;
+				endif;
 			?>
+			</ul>
 				<div class="w-full p-10 hidden" data-js="<?php echo 'tab-loader-' . $category->term_id; ?>">
 					<span class="mx-auto block size-7 border-2 border-solid border-secondary border-b-transparent animate-spin"></span>
 				</div>
-
+			<?php if ($i === 0) :?>
 				<button data-js="<?php echo 'tab-loadmore-' . $category->term_id; ?>" class="<?php if ($count <= $visible_posts) {echo 'hidden';} ?>flex gap-4 items-center rounded-2xl text-base font-bold py-[15px] px-5 md:px-8 text-white bg-gradient-to-b from-primary to-secondary transition duration-200">
 					Więcej wpisów
 					<svg class="shrink-0 -rotate-90" width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -120,9 +122,7 @@
                         <path class="fill-white" d="M9 12.986L5.75 7.5H7.7L9.468 10.451L11.314 7.5H13.16L9.845 12.986H9Z"></path>
                     </svg>
                 </button>
-			<?php 
-			endif; 
-			?>
+			<?php endif; ?>
 		</div>
 		
 	<?php endforeach; ?>
