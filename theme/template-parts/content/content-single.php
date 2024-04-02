6f2d3b03e0ee70ae956f4bef1ee8262ea0c86814
Kamil <k.medrala99@gmail.com>
2024-04-02 10:52:33 +0200

    post cta margins

--- theme/template-parts/content/content-single.php
+++ theme/template-parts/content/content-single.php
@@ -71,7 +71,7 @@
 
 </article><!-- #post-${ID} -->
 
-<section class="container mt-10 md:mt-20">
+<section class="container my-10 md:mt-20 md:mb-16">
 	<h2 class="text-3xl md:text-5xl text-bold font-bold mb-5 md:mb-10 text-center">Zobacz pozostałe materiały eksperckie z naszego bloga</h2>
 	<ul class="p-2 rounded-2xl bg-[#F2F2F2] flex items-center gap-2 max-w-screen-md w-fit flex-wrap mx-auto mb-5 md:mb-10">
 		<?php
