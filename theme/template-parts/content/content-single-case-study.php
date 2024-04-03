cb36885fbd4e6f66ec9973de7bd19246d11d00ce
Tomek <TK-54-4B>
2024-04-03 20:21:05 +0200

    case study acf fields update

--- theme/template-parts/content/content-single-case-study.php
+++ theme/template-parts/content/content-single-case-study.php
@@ -162,11 +162,6 @@ $client_logos = get_field('client_logos', 'option');
 	?>
 		<div class="relative py-10 md:py-20">
 			<div class="container">
-				<?php if ($header) : ?>
-					<h2 class="text-center font-bold text-2xl md:text-3xl lg:text-5xl mb-9 md:mb-14">
-						<?php echo $header ?>
-					</h2>
-				<?php endif; ?>
 				<div class="flex flex-wrap gap-5 justify-center items-center">
 					<?php foreach ($client_logos as $logo) : ?>
 						<img class="object-contain grayscale w-[76px] md:w-36 xl:w-[188px]" src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>" alt="<?php echo $logo['title']; ?>" />
