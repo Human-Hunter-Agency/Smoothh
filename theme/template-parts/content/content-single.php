58d3088a570a63f897fd76fe8ce65674445c4e41
Kamil <k.medrala99@gmail.com>
2024-04-03 13:23:13 +0200

    button visibility test

--- theme/template-parts/content/content-single.php
+++ theme/template-parts/content/content-single.php
@@ -117,8 +117,8 @@
 					<span class="mx-auto block size-7 border-2 border-solid border-primary rounded-full border-b-transparent animate-spin"></span>
 				</div>
 				<?php echo $count . ' | ' . $visible_posts ?>
-				<?php echo ($count <= $visible_posts) ? 'true' : 'false'; ?>
-				<button data-js="<?php echo 'tab-loadmore-' . $category->term_id; ?>" class="<?php if ($count <= $visible_posts || $i > 0) {echo 'hidden';} ?> flex mx-auto mt-10 gap-4 items-center rounded-2xl text-base font-bold py-[15px] px-5 md:px-8 text-white bg-gradient-to-b from-primary to-secondary transition duration-200 disabled:pointer-events-none disabled:opacity-80">
+				<?php echo (($count <= $visible_posts) || ($i > 0)) ? 'true' : 'false'; ?>
+				<button data-js="<?php echo 'tab-loadmore-' . $category->term_id; ?>" class="<?php if (($count <= $visible_posts) || ($i > 0)) {echo 'hidden';} ?> flex mx-auto mt-10 gap-4 items-center rounded-2xl text-base font-bold py-[15px] px-5 md:px-8 text-white bg-gradient-to-b from-primary to-secondary transition duration-200 disabled:pointer-events-none disabled:opacity-80">
 					Więcej wpisów
 					<svg class="shrink-0 -rotate-90" width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <circle class="stroke-white" cx="9.5" cy="9.5" r="9"></circle>
