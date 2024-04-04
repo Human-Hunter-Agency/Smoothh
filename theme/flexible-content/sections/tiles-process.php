1446b6f2bc3f5e705f25357ad916b1f8d4be445d
Kamil <k.medrala99@gmail.com>
2024-04-04 14:05:19 +0200

    tiles_process section

--- theme/flexible-content/sections/tiles-process.php
+++ theme/flexible-content/sections/tiles-process.php
@@ -13,12 +13,12 @@
             <h2 class="text-3xl md:text-5xl text-bold font-bold mb-9 text-center" ><?php echo esc_html($header); ?></h1>
         <?php endif; ?>        
     </div>
-    <div class="w-full overflow-hidden xl:px-[100px]">
+    <div class="w-full overflow-hidden 2xl:px-[100px]">
         <?php if ($tiles) : ?>
             <div class="swiper !container !overflow-visible" data-js="swiper-tiles-mobile">
-                <div class="swiper-wrapper xl:!gap-9 xl:!transform-none xl:flex-wrap xl:basis-[calc(33%-70px)]">
+                <div class="swiper-wrapper xl:!gap-[100px] xl:!transform-none xl:!flex-wrap">
                     <?php foreach($tiles as $tile) : ?>
-                        <div class="swiper-slide !h-auto xl:flex-1">
+                        <div class="swiper-slide !h-auto xl:flex-1 xl:!basis-[calc(33%-70px)]">
                             <div class="flex flex-col items-center text-center p-3 md:p-6 pb-5 md:pb-10">
                                 <?php if ($tile['icon'] && $tile['icon']['url'] ) : ?>
                                     <img src="<?php echo $tile['icon']['url']; ?>" class="object-contain size-20 md:size-32 xl:size-[170px] mb-8 md:mb-[50px]" >
