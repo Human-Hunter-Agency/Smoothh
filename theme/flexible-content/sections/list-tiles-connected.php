45c4eade5cd3ea9a6b2c9c5dd346fb5db456d557
Tomek <TK-54-4B>
2024-04-04 17:25:48 +0200

    pricing page dotted connection test

--- theme/flexible-content/sections/list-tiles-connected.php
+++ theme/flexible-content/sections/list-tiles-connected.php
@@ -35,7 +35,7 @@ $list = $args['list'];
                                 <?php else : ?>
 
                                     <div class="flex items-center justify-center size-[72px] rounded-2xl border border-primary"><?php echo $i; ?></div>
-                                    <div class="w-full h-[2px] border-t-[1px] border-t-dotted border-t-primary"></div>
+                                    <div class="w-full h-[2px] border-t-[1px] border-dotted border-t-primary"></div>
 
                                     <?php if ($item['description']) : ?>
                                         <div class="prose prose-sm md:prose-base"><?php echo $item['description']; ?></div>
