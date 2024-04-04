565640801c54a97c56166752b0326d4b336609aa
Kamil <k.medrala99@gmail.com>
2024-04-04 14:27:08 +0200

    tiles_process swiper desktop tiles px

--- theme/flexible-content/sections/tiles-process.php
+++ theme/flexible-content/sections/tiles-process.php
@@ -6,7 +6,7 @@
 
 ?>
 
-<div class="w-full py-10 md:py-[70px] mb:pb-20 text-white bg-gradient-to-b from-primary to-secondary mt-10 md:mt-20">
+<div class="w-full py-10 md:py-[70px] mb:pb-20 text-white bg-gradient-to-b from-secondary to-primary mt-10 md:mt-20 mb-20">
 
     <div class="relative z-0 container">
         <?php if ($header) : ?>
@@ -18,13 +18,13 @@
             <div class="swiper !container !overflow-visible mx-auto !max-w-screen-lg" data-js="swiper-tiles-mobile-narrow">
                 <div class="swiper-wrapper xl:gap-16 2xl:!gap-x-[100px] xl:!transform-none xl:!flex-wrap">
                     <?php foreach($tiles as $tile) : ?>
-                        <div class="swiper-slide !h-auto xl:flex-1 xl:!basis-[calc(33%-70px)]">
+                        <div class="swiper-slide !h-auto xl:flex-1 xl:!basis-[calc(33%-70px)] px-3 md:px-5">
                             <div class="flex flex-col items-center text-center ">
                                 <?php if ($tile['icon'] && $tile['icon']['url'] ) : ?>
                                     <img src="<?php echo $tile['icon']['url']; ?>" class="object-contain size-20 md:size-28 xl:size-[170px] mb-8 md:mb-14" >
                                 <?php endif; ?>
                                 <?php if ($tile['description']) : ?>
-                                    <p class="text-sm md:text-base"><?php echo $tile['description']; ?></p>
+                                    <div class="prose prose-sm md:prose-base !text-white"><?php echo $tile['description']; ?></div>
                                 <?php endif; ?>
                             </div>
                         </div>
