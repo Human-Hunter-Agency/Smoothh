521fd35c6b364a17f6a4badc8a19aea94fb883b7
Tomek <TK-54-4B>
2024-04-04 18:15:42 +0200

    pricing page dotted connection test

--- theme/flexible-content/sections/list-tiles-connected.php
+++ theme/flexible-content/sections/list-tiles-connected.php
@@ -34,11 +34,8 @@ $list = $args['list'];
 
                                 <?php else : ?>
 
-                                    <div class="flex items-center justify-center size-[72px] rounded-2xl border border-primary bg-white relative z-[1]">
-                                        <div class="<?php echo $i == 1 || $i == 6 ? 'w-1/2' : 'w-[200%]'; ?> h-[2px] border-t-[1px] border-dotted border-t-[#8117EE50] absolute top-1/2 <?php echo $i == 5 || $i == 9 ? '!w-1/2 left-0' : 'right-0'; ?>  z-[-1]"></div>
-                                        <?php echo $i; ?>
-                                    </div>
-
+                                    <div class="flex items-center justify-center size-[72px] rounded-2xl border border-primary bg-white z-[1]"><?php echo $i; ?></div>
+                                    <div class="<?php echo $i == 1 || $i == 6 ? 'w-1/2' : 'w-[99.2%]'; ?> h-[2px] border-t-[1px] border-dotted border-t-[#8117EE50] absolute top-[10%] lg:top-[12%] xl:top-[15%] <?php echo $i == 5 || $i == 9 ? '!w-1/2 left-0' : 'right-0'; ?>  z-[-1]"></div>
 
                                     <?php if ($item['description']) : ?>
                                         <div class="prose prose-sm md:prose-base"><?php echo $item['description']; ?></div>
