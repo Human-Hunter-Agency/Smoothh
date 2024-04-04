40ca73655c5835197fd376c67de254db4b1c33e5
Kamil <k.medrala99@gmail.com>
2024-04-04 15:52:57 +0200

    process tiles connected base test

---
+++ theme/flexible-content/sections/list_tiles_connected.php
@@ -0,0 +1,39 @@
+<?php
+/** Template to display 'Sekcja z procesem rekrutacji' - tiles_process */
+
+    $header = $args['header'];
+    $list = $args['list'];
+
+?>
+
+<div class="w-full py-10 md:py-[70px] mb:pb-20 text-white bg-gradient-to-b from-secondary to-primary mt-10 md:mt-20 mb-20">
+
+    <div class="relative z-0 container">
+        <?php if ($header) : ?>
+            <div class="prose-h3:text-3xl prose-h3:md:text-5xl prose-h3:font-bold mb-20 md:mb-24" ><?php echo esc_html($header); ?></div>
+        <?php endif; ?>        
+    </div>
+    <div class="w-full overflow-hidden ">
+        <?php if ($list) : ?>
+            <div class="swiper !container !overflow-visible" data-js="swiper-tiles-mobile-narrow">
+                <div class="swiper-wrapper xl:!transform-none xl:!flex-wrap">
+                    <?php 
+                        $i = 0;
+                        $listLength = count($list);
+                        foreach($list as $item) : 
+                        $i++
+                        ?>
+                        <div class="swiper-slide !h-auto xl:flex-1 xl:!basis-1/5 px-3 md:px-5">
+                            <div class="flex flex-col items-center text-center ">
+                                <div class="flex items-center justify-center size-[72px] rounded-2xl border border-primary"><?php echo $i; ?></div>
+                                <?php if ($item['description']) : ?>
+                                    <div class="prose prose-sm md:prose-base"><?php echo $item['description']; ?></div>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        <?php endif; ?>
+    </div>
+</div>
