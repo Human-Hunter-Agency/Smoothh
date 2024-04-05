2c627b4038c351b03bb467973403886aeaca61af
Kamil <k.medrala99@gmail.com>
2024-04-05 09:36:16 +0200

    swiper tiles case studies fix

--- theme/flexible-content/sections/swiper-tiles-case-studies.php
+++ theme/flexible-content/sections/swiper-tiles-case-studies.php
@@ -2,7 +2,7 @@
 /** Template to display 'Swiper z kafelkami case studies' - swiper_tiles_case_studies */
 
     $header = $args['header'];
-    $tiles_list = $args['tiles_list'];
+    $posts_list = $args['tiles_list'];
 
 ?>
 
@@ -19,35 +19,24 @@
     <?php endif; ?>
 
     <div class="relative z-0 w-full overflow-hidden !pb-5">
-        <?php if ($tiles_list) : ?>
+        <?php if ($posts_list) : ?>
             <div class="swiper !container !overflow-visible" data-js="swiper-tiles-default">
                 <div class="swiper-wrapper">
-                    <?php print_r($tiles_list); ?>
-                    <!-- <?php foreach($tiles_list as $tile) : ?>
+                    <?php foreach($posts_list as $post) : ?>
                         <div class="swiper-slide !h-auto !flex items-center flex-col border-2 border-[#EFEFEF] rounded-2xl opacity-0 !transition duration-500 [&.swiper-slide-visible]:opacity-100">
-                            <?php if ($tile['image'] && $tile['image']['url'] ) : ?>
-                                <div class="w-full relative mb-5 rounded-t-[14px] overflow-hidden">
-                                    <img src="<?php echo $tile['image']['url']; ?>" class="object-cover w-full h-[120px] md:h-[220px]" >
-                                    <div class="absolute inset-0 bg-gradient-to-b from-primary/20 to-secondary/20"></div>
-                                </div>
-                            <?php endif; ?>
-                            <div class="text-center p-3 md:p-6 !pt-0">
-                                <?php if ($tile['title']) : ?>
-                                    <h3 class="text-base md:text-xl text-primary mb-9 font-semibold"><?php echo $tile['title']; ?></h3>
-                                <?php endif; ?>
-                                <?php if ($tile['description']) : ?>
-                                    <p class="text-sm md:text-base italic font-medium"><?php echo $tile['description']; ?></p>
-                                <?php endif; ?>
+                            <div class="w-full relative mb-5 rounded-t-[14px] overflow-hidden">
+                                <img src="<?php echo get_the_post_thumbnail_url($post->ID); ?>" class="object-cover w-full h-[120px] md:h-[220px]" >
+                                <div class="absolute inset-0 bg-gradient-to-b from-primary/20 to-secondary/20"></div>
                             </div>
-                            <?php if( $tile['button'] ): 
-                                $btn_url = $tile['button']['url'];
-                                $btn_title = $tile['button']['title'];
-                                $btn_target = $tile['button']['target'] ? $tile['button']['target'] : '_self';
-                                ?>
-                                <a href="<?php echo esc_url( $btn_url ); ?>" target="<?php echo esc_attr( $btn_target ); ?>" class="translate-y-1/2 rounded-[14px] text-[13px] font-bold py-2 px-7 text-white bg-primary hover:bg-secondary transition duration-200" ><?php echo esc_html( $btn_title ); ?></a>
-                            <?php endif; ?>
+                            <div class="text-center p-3 md:p-6 !pt-0">
+                                <?php if ($post -> post_title) : ?>
+                                    <h3 class="text-base md:text-xl text-primary mb-9 font-semibold"><?php echo $post -> post_title; ?></h3>
+                                <?php endif; ?>
+                                <p class="text-sm md:text-base italic font-medium"><?php echo get_the_excerpt($post->ID); ?></p>
+                            </div>
+                            <a href="<?php echo get_permalink($post->ID); ?>" class="translate-y-1/2 rounded-[14px] text-[13px] font-bold py-2 px-7 text-white bg-primary hover:bg-secondary transition duration-200" >Czytaj więcej</a>
                         </div>
-                    <?php endforeach; ?> -->
+                    <?php endforeach; ?>
                 </div>
                 <div class="swiper-button-prev">
                     <svg width="12" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
