2604e1687df01738c9b153009ec4fccd095ce91f
Kamil <k.medrala99@gmail.com>
2024-04-09 08:33:17 +0200

    single product page test

--- theme/single.php
+++ theme/single.php
@@ -18,7 +18,7 @@ get_header();
 		/* Start the Loop */
 		while (have_posts()) :
 			the_post();
-			get_template_part('template-parts/content/content', 'single');
+			get_template_part('template-parts/content/content', get_post_format());
 
 		// End the loop.
 		endwhile;
