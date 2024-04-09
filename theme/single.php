29f73459f1edc31311de3cf61f8e67cf0611596c
Kamil <k.medrala99@gmail.com>
2024-04-09 09:43:41 +0200

    single page template test

--- theme/single.php
+++ theme/single.php
@@ -1,48 +1,34 @@
 <?php
+
 /**
- * The template for displaying all single posts and attachments
+ * The template for displaying all single posts
  *
- * @package WordPress
- * @subpackage Twenty_Fifteen
- * @since Twenty Fifteen 1.0
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package Smoothh
  */
- 
-get_header(); ?>
- 
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
- 
-        <?php
-        // Start the loop.
-        while ( have_posts() ) : the_post();
- 
-            /*
-             * Include the post format-specific template for the content. If you want to
-             * use this in a child theme, then include a file called called content-___.php
-             * (where ___ is the post format) and that will be used instead.
-             */
-            get_template_part( 'content', get_post_format() );
- 
-            // If comments are open or we have at least one comment, load up the comment template.
-            if ( comments_open() || get_comments_number() ) :
-                comments_template();
-            endif;
- 
-            // Previous/next post navigation.
-            the_post_navigation( array(
-                'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
-                    '<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
-                    '<span class="post-title">%title</span>',
-                'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
-                    '<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
-                    '<span class="post-title">%title</span>',
-            ) );
- 
-        // End the loop.
-        endwhile;
-        ?>
- 
-        </main><!-- .site-main -->
-    </div><!-- .content-area -->
- 
-<?php get_footer(); ?>
+
+get_header();
+?>
+
+<section id="primary">
+	<?php echo get_post_format() ?>
+	<main id="main">
+
+		<?php
+		/* Start the Loop */
+		while (have_posts()) :
+			the_post();
+			get_template_part('template-parts/content/content', 'single');
+
+		// End the loop.
+		endwhile;
+		?>
+
+	</main><!-- #main -->
+
+</section><!-- #primary -->
+
+
+<?php
+get_footer();
