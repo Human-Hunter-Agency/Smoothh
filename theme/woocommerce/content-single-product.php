b0bee9878a8e3b9de1e49a9cd611f30ba7ede85b
Kamil <k.medrala99@gmail.com>
2024-04-09 12:17:54 +0200

    single product hide fields, display description

--- theme/woocommerce/content-single-product.php
+++ theme/woocommerce/content-single-product.php
@@ -46,7 +46,7 @@ if (post_password_required()) {
 
 	<div class="container">
 		<div class="prose-smoothh prose prose-base">
-			<?php $product->get_description(); ?>
+			<?php echo $product->get_description(); ?>
 		</div>
 	</div>
 
@@ -54,8 +54,8 @@ if (post_password_required()) {
 	/**
 	 * Hook: woocommerce_after_single_product_summary.
 	 *
-	 * @hooked woocommerce_output_product_data_tabs - 10
-	 * @hooked woocommerce_upsell_display - 15 - removed
+	 * @hooked woocommerce_output_product_data_tabs - 10 - removed
+	 * @hooked woocommerce_upsell_display - 15
 	 * @hooked woocommerce_output_related_products - 20
 	 */
 	do_action('woocommerce_after_single_product_summary');
