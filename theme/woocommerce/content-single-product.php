b6de5394c132994ded72ae76c257f867fd577ead
Kamil <k.medrala99@gmail.com>
2024-04-10 10:06:24 +0200

    product select variant

--- theme/woocommerce/content-single-product.php
+++ theme/woocommerce/content-single-product.php
@@ -45,26 +45,28 @@ if (post_password_required()) {
 		</div>
 	</div>
 
-	<div class="hidden">
-		<?php
-		/**
-		 * Hook: woocommerce_single_product_summary.
-		 *
-		 * @hooked woocommerce_template_single_title - 5
-		 * @hooked woocommerce_template_single_rating - 10
-		 * @hooked woocommerce_template_single_price - 10
-		 * @hooked woocommerce_template_single_excerpt - 20
-		 * @hooked woocommerce_template_single_add_to_cart - 30
-		 * @hooked woocommerce_template_single_meta - 40
-		 * @hooked woocommerce_template_single_sharing - 50
-		 * @hooked WC_Structured_Data::generate_product_data() - 60
-		 */
-		do_action( 'woocommerce_single_product_summary' );
-		?>
-	</div>
 	<div class="container flex flex-col md:flex-row gap-5 md:gap-6 lg:gap-10">
-		<div class="prose-smoothh prose prose-base md:prose-h2:text-xl">
-			<?php the_content() ?>
+		<div>
+			<div class="prose-smoothh prose prose-base md:prose-h2:text-xl">
+				<?php the_content() ?>
+			</div>
+			<div>
+				<?php
+				/**
+				 * Hook: woocommerce_single_product_summary.
+				 *
+				 * @hooked woocommerce_template_single_title - 5 -removed
+				 * @hooked woocommerce_template_single_rating - 10 -removed
+				 * @hooked woocommerce_template_single_price - 10 -removed
+				 * @hooked woocommerce_template_single_excerpt - 20 -removed
+				 * @hooked woocommerce_template_single_add_to_cart - 30
+				 * @hooked woocommerce_template_single_meta - 40 -removed
+				 * @hooked woocommerce_template_single_sharing - 50
+				 * @hooked WC_Structured_Data::generate_product_data() - 60
+				 */
+				do_action( 'woocommerce_single_product_summary' );
+				?>
+			</div>
 		</div>
 		<aside class="md:basis-1/4 md:grow-0 md:shrink-0">
 			<?php if ($cart && !$cart->is_empty()) : ?>
