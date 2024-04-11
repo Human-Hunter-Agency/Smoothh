3e6a53f9050e0283abb146d939cc2e8fc16bd80a
Kamil <k.medrala99@gmail.com>
2024-04-11 10:40:32 +0200

    add to cart btn test

--- theme/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ theme/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -28,14 +28,12 @@ global $product;
 	do_action( 'woocommerce_after_add_to_cart_quantity' );
 	?>
 
-	<?php if (is_user_logged_in()) : ?>
 		<button type="submit" class="single_add_to_cart_button whitespace-nowrap button border-none !bg-gradient-to-b from-primary to-secondary text-white h-[55px] !px-5 xl:!px-12 xl:!pr-8 !rounded-[15px] font-bold !flex items-center justify-center gap-5 alt<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>"><?php echo esc_html( $product->single_add_to_cart_text() ); ?>
 			<svg class="shrink-0 -rotate-90" width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
 				<circle class="stroke-white" cx="9.5" cy="9.5" r="9"></circle>
 				<path class="fill-white" d="M9 12.986L5.75 7.5H7.7L9.468 10.451L11.314 7.5H13.16L9.845 12.986H9Z"></path>
 			</svg>
 		</button>
-	<?php endif; ?>
 
 
 	<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
