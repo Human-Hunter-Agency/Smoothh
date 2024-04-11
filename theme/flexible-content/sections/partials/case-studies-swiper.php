35a75311d8cee742ba0529f6bee0179bf547ed8c
Kamil <k.medrala99@gmail.com>
2024-04-11 09:30:59 +0200

    case studies swiper as partial template

---
+++ theme/flexible-content/sections/partials/case-studies-swiper.php
@@ -0,0 +1,40 @@
+<?php
+/** Template to display inner case-studies swiper */
+
+    $posts = $args['posts'];
+
+?>
+
+<div class="relative z-0 w-full overflow-hidden !pb-5">
+        <?php if ($posts) : ?>
+            <div class="swiper !container !overflow-visible" data-js="swiper-tiles-default">
+                <div class="swiper-wrapper">
+                    <?php foreach($posts as $post) : ?>
+                        <div class="swiper-slide !h-auto !flex items-center flex-col border-2 border-[#EFEFEF] rounded-2xl opacity-0 !transition duration-500 [&.swiper-slide-visible]:opacity-100">
+                            <div class="w-full relative mb-5 rounded-t-[14px] overflow-hidden">
+                                <img src="<?php echo get_the_post_thumbnail_url($post->ID); ?>" class="object-cover w-full !h-[190px] md:!h-[220px]" >
+                                <div class="absolute inset-0 bg-gradient-to-b from-primary/20 to-secondary/20"></div>
+                            </div>
+                            <div class="text-center p-3 md:p-6 !pt-0">
+                                <?php if ($post -> post_title) : ?>
+                                    <h3 class="text-base md:text-xl text-primary mb-9 font-semibold"><?php echo $post -> post_title; ?></h3>
+                                <?php endif; ?>
+                                <p class="text-sm md:text-base italic font-medium"><?php echo get_the_excerpt($post->ID); ?></p>
+                            </div>
+                            <a href="<?php echo get_permalink($post->ID); ?>" class="translate-y-1/2 rounded-[14px] text-[13px] font-bold py-2 px-7 text-white bg-primary hover:bg-secondary transition duration-200" >Czytaj więcej</a>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+                <div class="swiper-button-prev">
+                    <svg width="12" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M-0.00195312 8.99988L11.998 0.33962L11.998 17.6601L-0.00195312 8.99988Z" fill="white"/>
+                    </svg>
+                </div>
+                <div class="swiper-button-next">
+                    <svg width="12" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 9L0 17.6603V0.339746L12 9Z" fill="white"/>
+                    </svg>
+                </div>
+            </div>
+        <?php endif; ?>
+    </div>
--- theme/flexible-content/sections/swiper-tiles-case-studies.php
+++ theme/flexible-content/sections/swiper-tiles-case-studies.php
@@ -2,7 +2,7 @@
 /** Template to display 'Swiper z kafelkami case studies' - swiper_tiles_case_studies */
 
     $header = $args['header'];
-    $posts_list = $args['tiles_list'];
+    $posts = $args['tiles_list'];
 
 ?>
 
@@ -16,39 +16,9 @@
                 </h2>
             </div>
         </div>
-    <?php endif; ?>
-
-    <div class="relative z-0 w-full overflow-hidden !pb-5">
-        <?php if ($posts_list) : ?>
-            <div class="swiper !container !overflow-visible" data-js="swiper-tiles-default">
-                <div class="swiper-wrapper">
-                    <?php foreach($posts_list as $post) : ?>
-                        <div class="swiper-slide !h-auto !flex items-center flex-col border-2 border-[#EFEFEF] rounded-2xl opacity-0 !transition duration-500 [&.swiper-slide-visible]:opacity-100">
-                            <div class="w-full relative mb-5 rounded-t-[14px] overflow-hidden">
-                                <img src="<?php echo get_the_post_thumbnail_url($post->ID); ?>" class="object-cover w-full !h-[190px] md:!h-[220px]" >
-                                <div class="absolute inset-0 bg-gradient-to-b from-primary/20 to-secondary/20"></div>
-                            </div>
-                            <div class="text-center p-3 md:p-6 !pt-0">
-                                <?php if ($post -> post_title) : ?>
-                                    <h3 class="text-base md:text-xl text-primary mb-9 font-semibold"><?php echo $post -> post_title; ?></h3>
-                                <?php endif; ?>
-                                <p class="text-sm md:text-base italic font-medium"><?php echo get_the_excerpt($post->ID); ?></p>
-                            </div>
-                            <a href="<?php echo get_permalink($post->ID); ?>" class="translate-y-1/2 rounded-[14px] text-[13px] font-bold py-2 px-7 text-white bg-primary hover:bg-secondary transition duration-200" >Czytaj więcej</a>
-                        </div>
-                    <?php endforeach; ?>
-                </div>
-                <div class="swiper-button-prev">
-                    <svg width="12" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M-0.00195312 8.99988L11.998 0.33962L11.998 17.6601L-0.00195312 8.99988Z" fill="white"/>
-                    </svg>
-                </div>
-                <div class="swiper-button-next">
-                    <svg width="12" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 9L0 17.6603V0.339746L12 9Z" fill="white"/>
-                    </svg>
-                </div>
-            </div>
-        <?php endif; ?>
-    </div>
+    <?php endif; 
+    
+    get_template_part( 'flexible-content/sections/partials/case-studies-swiper', '', array($posts) );
+    ?>
+    
 </div>
