4375d6a5cc5032b16fa26ef24b2773cde543d684
Kamil <k.medrala99@gmail.com>
2024-04-11 12:25:31 +0200

    contact page fixes
    map width/contact info position

--- theme/flexible-content/sections/map.php
+++ theme/flexible-content/sections/map.php
@@ -9,13 +9,13 @@ $brand_contact = get_field('brand_contact', 'option');
 
 ?>
 
-<div class="w-full mx-auto max-w-[1920px] flex flex-col-reverse lg:flex-row items-center">
+<div class="w-full flex flex-col-reverse lg:flex-row items-center">
     <div class="grow w-full relative border border-[#317FA8]">
         <iframe class="relative z-0 !w-full h-[400px] md:h-[557px]" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=558&amp;hl=en&amp;q=<?php echo esc_html($search_text) ?>&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
         <div class="absolute z-10 pointer-events-none inset-0 bg-gradient-to-b from-primary/10 to-secondary/10"></div>
     </div>
     <?php if ($display_info) : ?>
-        <div class="w-full p-5 lg:p-10 lg:pl-20 lg:w-1/2 shrink-0 flex flex-col justify-center gap-5 md:gap-10 mb-10 lg:mb-0 ">
+        <div class="w-full container lg:p-10 lg:pl-20 lg:w-1/2 shrink-0 flex flex-col justify-center gap-5 md:gap-10 mb-10 lg:mb-0 ">
             <div class="max-w-full">
 				<?php echo file_get_contents( get_stylesheet_directory_uri() . '/assets/img/logo.svg' ); ?>
             </div>
