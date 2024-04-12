d99c1f0ea997ccc6b9760993fda30ee934a1b45d
Kamil <k.medrala99@gmail.com>
2024-04-12 13:31:47 +0200

    pagecontent order test

--- theme/page.php
+++ theme/page.php
@@ -22,11 +22,9 @@ get_header();
 			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
-
-				get_template_part( 'template-parts/content/content', 'page' );
-
+				
 				$sections = get_field( 'sections' );
-
+				
 				if ( $sections ) :
 					foreach ( $sections as $section ) :
 						$section_visible = $section['isSectionVisible'];
@@ -36,6 +34,8 @@ get_header();
 						}
 					endforeach;
 				endif;
+				
+				get_template_part( 'template-parts/content/content', 'page' );
 
 			endwhile; // End of the loop.
 			?>
