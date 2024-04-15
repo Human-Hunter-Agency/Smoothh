5c46053c08774cd94f8a3ed4b1410e86b08d2694
Kamil <k.medrala99@gmail.com>
2024-04-15 15:34:17 +0200

    product swiper template padding fixes

--- theme/flexible-content/sections/swiper-products.php
+++ theme/flexible-content/sections/swiper-products.php
@@ -22,12 +22,12 @@ if (isset($args['products_list']) && !empty($args['products_list'])) {
 }
 ?>
 
-<section class="relative pb-10 mb:pb-[60px]">
+<section class="relative py-10 mb:py-[60px] md:pt-[70px]">
 
     <?php if ($header) : ?>
         <div class="container">
             <div class="relative z-0">
-                <h2 class="text-center font-bold text-2xl md:text-3xl lg:text-5xl mb-9 md:mb-14 text-white">
+                <h2 class="text-center font-bold text-2xl md:text-3xl lg:text-5xl mb-9 md:mb-14">
                     <?php echo esc_html($header); ?>
                 </h2>
             </div>
