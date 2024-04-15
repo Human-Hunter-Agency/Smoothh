6f474e0b40da93dc0eb8955d9a1998ce2ba3ff47
Kamil <k.medrala99@gmail.com>
2024-04-15 10:53:14 +0200

    cart columns widths

--- theme/woocommerce/cart/cart.php
+++ theme/woocommerce/cart/cart.php
@@ -135,11 +135,11 @@ do_action('woocommerce_before_cart'); ?>
         </ul>
             <div class="flex justify-end text-xl">
                 <div class="grow border-t border-[#F2F2F2]"></div>
-                <span class="w-[15%] lg:w-[10%] pr-2.5 lg:mr-5 text-right pt-2.5 text-primary font-semibold border-t border-[#F2F2F2]"><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?>:</span>
-			    <span class="w-[15%] lg:w-[10%] pr-1.5 mr-1 lg:mr-5 text-right pt-2.5 text-primary font-semibold border-t border-[#F2F2F2]" data-title="<?php esc_attr_e( 'Subtotal', 'woocommerce' ); ?>"><?php wc_cart_totals_subtotal_html(); ?></span>
+                <span class="w-[15%] lg:w-[10%] pr-2.5 lg:pr-5 text-right pt-2.5 text-primary font-semibold border-t border-[#F2F2F2]"><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?>:</span>
+			    <span class="w-[15%] lg:w-[10%] pr-1.5 mr-1 lg:pr-4 text-right pt-2.5 text-primary font-semibold border-t border-[#F2F2F2]" data-title="<?php esc_attr_e( 'Subtotal', 'woocommerce' ); ?>"><?php wc_cart_totals_subtotal_html(); ?></span>
                 <?php if(WC()->cart->has_discount()) : ?>
                     <div class="grow-0 w-[15%] lg:w-[10%] px-5 lg:px-7 bg-primary rounded-b-[15px] min-w-32 lg:min-w-40 ">
-                        <span class="text-right pt-2.5 lg:pb-7 text-white border-t border-white"><?php echo WC()->cart->get_total_discount(); ?></span>
+                        <div class="text-right pt-2.5 lg:pb-7 text-white border-t border-white"><?php echo WC()->cart->get_total_discount(); ?></div>
                     </div>
                 <?php endif; ?>
             </div>
