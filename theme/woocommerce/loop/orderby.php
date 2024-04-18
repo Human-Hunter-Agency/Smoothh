b14316b2804f148df60d66f074c5b8b6388cfd02
Kamil <k.medrala99@gmail.com>
2024-04-18 13:22:25 +0200

    basic select dropdown style fixes

--- theme/woocommerce/loop/orderby.php
+++ theme/woocommerce/loop/orderby.php
@@ -22,12 +22,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <form class="woocommerce-ordering" method="get">
     <div class="relative">
-        <select name="orderby" class="orderby relative z-0 h-9 rounded-[15px] border border-primary pl-5 pr-10 transition duration-200 hover:border-secondary accent-primary w-full !m-0 appearance-none" aria-label="<?php esc_attr_e( 'Shop order', 'woocommerce' ); ?>">
+        <select name="orderby" class="peer orderby relative z-0 h-9 rounded-[15px] border border-primary pl-5 pr-7 transition duration-200 hover:border-secondary accent-primary w-full !m-0 appearance-none" aria-label="<?php esc_attr_e( 'Shop order', 'woocommerce' ); ?>">
             <?php foreach ( $catalog_orderby_options as $id => $name ) : ?>
                 <option value="<?php echo esc_attr( $id ); ?>" <?php selected( $orderby, $id ); ?>><?php echo esc_html( $name ); ?></option>
             <?php endforeach; ?>
         </select>
-        <svg class="absolute z-10 right-3 top-3 transition duration-300 pointer-events-none" width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg class="peer-focus:rotate-180 absolute z-10 right-3 top-3.5 transition duration-300 pointer-events-none" width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12.9595 0.75L7 9.1368L1.04047 0.75H12.9595Z" fill="#8117EE" stroke="#8117EE" />
         </svg>
     </div>
