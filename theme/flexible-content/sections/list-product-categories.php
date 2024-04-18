069ccf8bc14ff4a59ea6bc9331c995beb4ff84dd
Kamil <k.medrala99@gmail.com>
2024-04-18 10:42:18 +0200

    ehole cat tile clickable

--- theme/flexible-content/sections/list-product-categories.php
+++ theme/flexible-content/sections/list-product-categories.php
@@ -30,34 +30,36 @@
 
                     $min_price = min($all_prices);
             ?>
-                <li class="flex items-center flex-col border-2 border-[#EFEFEF] rounded-2xl">
-                    <div class="relative overflow-hidden rounded-t-[14px] w-full !h-[190px] md:!h-[220px] [&_img]:object-cover [&_img]:w-full [&_img]:h-full">
-                        <?php
-                            $thumbnail_id = get_term_meta( $cat->term_id, 'thumbnail_id', true );
-                            $image = wp_get_attachment_url( $thumbnail_id ); 
-                            echo "<img src='{$image}' alt='{$cat->name}' width='762' height='365' />";
-                        ?>
-                        <div class="absolute inset-0 bg-gradient-to-b from-primary/20 to-secondary/20"></div>
-                    </div>
-                    <div class="flex-1 p-3 md:p-6">
-                        <div class="flex gap-2.5 md:gap-5 justify-between mb-5">
-                            <h4 class="text-lg md:text-xl text-primary font-semibold">
-                                <?php echo $cat->name ?>
-                            </h4>
-                            <?php if (is_user_logged_in()) : ?>
-                                <span class="text-lg md:text-xl">
-                                    <?php
-                                        echo number_format( $min_price, wc_get_price_decimals(), wc_get_price_decimal_separator(), wc_get_price_thousand_separator());
-                                    ?> netto
-                                </span>
-                            <?php endif; ?>
+                <li>
+                    <a href="<?php echo get_term_link( $cat->term_id, 'product_cat' ); ?>" class="group flex items-center flex-col border-2 border-[#EFEFEF] rounded-2xl">
+                        <div class="relative overflow-hidden rounded-t-[14px] w-full !h-[190px] md:!h-[220px] [&_img]:object-cover [&_img]:w-full [&_img]:h-full">
+                            <?php
+                                $thumbnail_id = get_term_meta( $cat->term_id, 'thumbnail_id', true );
+                                $image = wp_get_attachment_url( $thumbnail_id ); 
+                                echo "<img src='{$image}' alt='{$cat->name}' width='762' height='365' />";
+                            ?>
+                            <div class="absolute inset-0 bg-gradient-to-b from-primary/20 to-secondary/20"></div>
                         </div>
-                        <p class="text-sm md:text-base prose-strong:font-semibold">
-                            <?php echo $cat->description ?>
-                        </p>
-                    </div>
-                    <a href="<?php echo get_term_link( $cat->term_id, 'product_cat' ); ?>" class="translate-y-1/2 rounded-[14px] text-[13px] font-bold py-2 px-7 text-white bg-primary hover:bg-secondary transition duration-200">
-                        Pokaż
+                        <div class="flex-1 p-3 md:p-6">
+                            <div class="flex gap-2.5 md:gap-5 justify-between mb-5">
+                                <h4 class="text-lg md:text-xl text-primary font-semibold">
+                                    <?php echo $cat->name ?>
+                                </h4>
+                                <?php if (is_user_logged_in()) : ?>
+                                    <span class="text-lg md:text-xl">
+                                        <?php
+                                            echo number_format( $min_price, wc_get_price_decimals(), wc_get_price_decimal_separator(), wc_get_price_thousand_separator());
+                                        ?> netto
+                                    </span>
+                                <?php endif; ?>
+                            </div>
+                            <p class="text-sm md:text-base prose-strong:font-semibold">
+                                <?php echo $cat->description ?>
+                            </p>
+                        </div>
+                        <span class="translate-y-1/2 rounded-[14px] text-[13px] font-bold py-2 px-7 text-white bg-primary group-hover:bg-secondary transition duration-200">
+                            Pokaż
+                        </span>
                     </a>
                 </li>
             <?php endforeach;?>
