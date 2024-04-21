096e0a5779ed8ff721b90bd1dd4c032b8fb7ed8c
Tomek <TK-54-4B>
2024-04-21 16:32:42 +0200

    registration fields test

--- theme/functions.php
+++ theme/functions.php
@@ -353,28 +353,8 @@ function is_prod_guest_available($product)
 	return $guest_available;
 }
 
-// custom fields registration form
-function wooc_extra_register_fields()
-{ ?>
-	<p class="form-row form-row-wide">
-		<label for="reg_billing_phone"><?php _e('Phone', 'woocommerce'); ?></label>
-		<input type="text" class="input-text p-2 border" name="billing_phone" id="reg_billing_phone" value="<?php esc_attr_e(!empty($_POST['billing_phone'])); ?>" />
-	</p>
-	<p class="form-row form-row-first">
-		<label for="reg_billing_first_name"><?php _e('First name', 'woocommerce'); ?><span class="required">*</span></label>
-		<input type="text" class="input-text p-2 border" name="billing_first_name" id="reg_billing_first_name" value="<?php if (!empty($_POST['billing_first_name'])) esc_attr_e($_POST['billing_first_name']); ?>" />
-	</p>
-	<p class="form-row form-row-last">
-		<label for="reg_billing_last_name"><?php _e('Last name', 'woocommerce'); ?><span class="required">*</span></label>
-		<input type="text" class="input-text p-2 border" name="billing_last_name" id="reg_billing_last_name" value="<?php if (!empty($_POST['billing_last_name'])) esc_attr_e($_POST['billing_last_name']); ?>" />
-	</p>
-	<div class="clear"></div>
-<?php
-}
-add_action('woocommerce_register_form_start', 'wooc_extra_register_fields');
-
 /**
- * Add billing fields 
+ * Add billing fields in registration fields
  *
  */
 function my_custom_function()
