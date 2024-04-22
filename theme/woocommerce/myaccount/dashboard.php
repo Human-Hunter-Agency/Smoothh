8a2c5a8cdddc500704de415f2b6ec5df3b1ec927
Tomek <TK-54-4B>
2024-04-22 18:48:21 +0200

    [my account page] adding styles

--- theme/woocommerce/myaccount/dashboard.php
+++ theme/woocommerce/myaccount/dashboard.php
@@ -33,14 +33,14 @@ $allowed_html = array(
   <?php
   printf(
     /* translators: 1: user display name 2: logout url */
-    wp_kses(__('Hello %1$s <br> not %1$s? <a href="%2$s">Log out</a>', 'woocommerce'), $allowed_html),
+    wp_kses(__('Hello %1$s (not %1$s? <a href="%2$s">Log out</a>)', 'woocommerce'), $allowed_html),
     '<strong>' . esc_html($current_user->display_name) . '</strong>',
     esc_url(wc_logout_url())
   );
   ?>
 </p>
 
-<p>
+<p class="text-lg">
   <?php
   /* translators: 1: Orders URL 2: Address URL 3: Account URL. */
   $dashboard_desc = __('From your account dashboard you can view your <a href="%1$s">recent orders</a>, manage your <a href="%2$s">billing address</a>, and <a href="%3$s">edit your password and account details</a>.', 'woocommerce');
