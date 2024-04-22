4ec61435fd4a48f2917abe49f9ab56845765e3b1
Tomek <TK-54-4B>
2024-04-22 18:28:00 +0200

    [my account page] adding styles

--- theme/woocommerce/myaccount/navigation.php
+++ theme/woocommerce/myaccount/navigation.php
@@ -30,7 +30,7 @@ do_action('woocommerce_before_account_navigation');
     <ul>
       <?php foreach (wc_get_account_menu_items() as $endpoint => $label) : ?>
         <li class="py-1 <?php echo wc_get_account_menu_item_classes($endpoint); ?>">
-          <a href="<?php echo esc_url(wc_get_account_endpoint_url($endpoint)); ?>" class=" text-lgtransition duration-200 hover:text-primary"><?php echo esc_html($label); ?></a>
+          <a href="<?php echo esc_url(wc_get_account_endpoint_url($endpoint)); ?>" class="text-lg transition duration-200 hover:text-primary"><?php echo esc_html($label); ?></a>
         </li>
       <?php endforeach; ?>
     </ul>
