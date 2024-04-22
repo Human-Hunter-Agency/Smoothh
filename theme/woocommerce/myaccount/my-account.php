cae6b4267a7bff15dc2545c143eabab217e8ddd1
Tomek <TK-54-4B>
2024-04-22 18:45:33 +0200

    [my account page] added  dashboard template

--- theme/woocommerce/myaccount/my-account.php
+++ theme/woocommerce/myaccount/my-account.php
@@ -25,7 +25,7 @@ defined('ABSPATH') || exit;
  */
 do_action('woocommerce_account_navigation'); ?>
 
-<div class="woocommerce-MyAccount-content py-10 [&>p]:mb-10">
+<div class="woocommerce-MyAccount-content py-10">
   <?php
   /**
    * My Account content.
