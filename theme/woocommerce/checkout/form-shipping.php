983e4fbd4ccafa3f11df0eb5ebbad3eb934fc2c9
Tomek <TK-54-4B>
2024-04-23 18:16:38 +0200

    [checkout] input styles

--- theme/woocommerce/checkout/form-shipping.php
+++ theme/woocommerce/checkout/form-shipping.php
@@ -22,9 +22,9 @@ defined('ABSPATH') || exit;
 <div class="woocommerce-shipping-fields">
   <?php if (true === WC()->cart->needs_shipping_address()) : ?>
 
-    <h3 id="ship-to-different-address text-primary">
+    <h3 id="ship-to-different-address">
       <label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
-        <input id="ship-to-different-address-checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox px-4 py-5" <?php checked(apply_filters('woocommerce_ship_to_different_address_checked', 'shipping' === get_option('woocommerce_ship_to_destination') ? 1 : 0), 1); ?> type="checkbox" name="ship_to_different_address" value="1" /> <span><?php esc_html_e('Ship to a different address?', 'woocommerce'); ?></span>
+        <input id="ship-to-different-address-checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox px-4 py-5" <?php checked(apply_filters('woocommerce_ship_to_different_address_checked', 'shipping' === get_option('woocommerce_ship_to_destination') ? 1 : 0), 1); ?> type="checkbox" name="ship_to_different_address" value="1" /> <span class="ml-1 text-primary"><?php esc_html_e('Ship to a different address?', 'woocommerce'); ?></span>
       </label>
     </h3>
 
