743efa43052daad20405ed29b2ab87da6a6ab7b3
Tomek <TK-54-4B>
2024-04-23 18:46:39 +0200

    [checkout] order review styles

--- theme/woocommerce/checkout/review-order.php
+++ theme/woocommerce/checkout/review-order.php
@@ -39,7 +39,7 @@ defined('ABSPATH') || exit;
             <?php echo wp_kses_post(apply_filters('woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key)) . '&nbsp;'; ?>
             <?php echo apply_filters('woocommerce_checkout_cart_item_quantity', ' <strong class="product-quantity">' . sprintf('&times;&nbsp;%s', $cart_item['quantity']) . '</strong>', $cart_item, $cart_item_key); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
             ?>
-            <?php print_r($cart_item) ?>
+            <?php print_r($cart_item['sku']); ?>
             <?php echo wc_get_formatted_cart_item_data($cart_item); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
             ?>
           </td>
