b3b42578c2c522d3051e708b060cea9a5ab76b3e
Tomek <TK-54-4B>
2024-04-23 18:28:15 +0200

    [checkout] styles party

--- theme/woocommerce/checkout/form-shipping.php
+++ theme/woocommerce/checkout/form-shipping.php
@@ -22,7 +22,7 @@ defined('ABSPATH') || exit;
 <div class="woocommerce-shipping-fields">
   <?php if (true === WC()->cart->needs_shipping_address()) : ?>
 
-    <h3 id="ship-to-different-address !mt-0">
+    <h3 id="ship-to-different-address" class="!mt-0">
       <label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
         <input id="ship-to-different-address-checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox px-4 py-5" <?php checked(apply_filters('woocommerce_ship_to_different_address_checked', 'shipping' === get_option('woocommerce_ship_to_destination') ? 1 : 0), 1); ?> type="checkbox" name="ship_to_different_address" value="1" /> <span class="ml-1 text-primary"><?php esc_html_e('Ship to a different address?', 'woocommerce'); ?></span>
       </label>
