152561d7d5be8a571f077c22c6863ae7353e8ede
Tomek <TK-54-4B>
2024-04-23 16:23:56 +0200

    [my account page] styles adjustments

--- theme/woocommerce/myaccount/form-edit-account.php
+++ theme/woocommerce/myaccount/form-edit-account.php
@@ -37,7 +37,7 @@ do_action('woocommerce_before_edit_account_form'); ?>
 
 	<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
 		<label for="account_display_name"><?php esc_html_e('Display name', 'woocommerce'); ?>&nbsp;<span class="required">*</span></label>
-		<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_display_name" id="account_display_name" value="<?php echo esc_attr($user->display_name); ?>" /> <span><em><?php esc_html_e('This will be how your name will be displayed in the account section and in reviews', 'woocommerce'); ?></em></span>
+		<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_display_name" id="account_display_name" value="<?php echo esc_attr($user->display_name); ?>" /> <span class="text-base text-gray-500"><em><?php esc_html_e('This will be how your name will be displayed in the account section and in reviews', 'woocommerce'); ?></em></span>
 	</p>
 
 	<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
@@ -55,7 +55,9 @@ do_action('woocommerce_before_edit_account_form'); ?>
 	?>
 
 	<fieldset class="w-full">
-		<legend><?php esc_html_e('Password change', 'woocommerce'); ?></legend>
+		<legend>
+			<h3 class="mt-5 mb-2 text-2xl font-semibold"><?php esc_html_e('Password change', 'woocommerce'); ?></h3>
+		</legend>
 
 		<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
 			<label for="password_current"><?php esc_html_e('Current password (leave blank to leave unchanged)', 'woocommerce'); ?></label>
