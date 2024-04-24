2058f4b40e1f0704da323696b186e2014ace0a65
Kamil <k.medrala99@gmail.com>
2024-04-24 13:14:56 +0200

    Revert "[checkout] move payment methods update test"
    This reverts commit 34b52e6097dd888da7931743e6b5a9f246940f88.

--- theme/woocommerce/checkout/payment.php
+++ theme/woocommerce/checkout/payment.php
@@ -22,9 +22,6 @@ if (!wp_doing_ajax()) {
   do_action('woocommerce_review_order_before_payment');
 }
 ?>
-<div>
-  test
-</div>
 <div id="payment" class="woocommerce-checkout-payment !bg-white">
   <?php if (WC()->cart->needs_payment()) : ?>
     <ul class="wc_payment_methods payment_methods methods">
