c39e25882605356804e43759ccf65adbb70ecd30
Kamil <k.medrala99@gmail.com>
2024-04-24 13:02:31 +0200

    Revert "move payment methods"
    This reverts commit 57d6347ee4d6d1ff7a7e27f96989826f23759424.

--- theme/woocommerce/checkout/form-checkout.php
+++ theme/woocommerce/checkout/form-checkout.php
@@ -45,25 +45,6 @@ if (!$checkout->is_registration_enabled() && $checkout->is_registration_required
         <?php do_action('woocommerce_checkout_shipping'); ?>
       </div>
       <?php do_action('woocommerce_checkout_after_customer_details'); ?>
-
-      <div class="px-5 xl:px-8 py-9 border border-[#888] rounded-[15px]">
-        <h3 id="order_review_heading" class="!mt-0 mb-9 text-2xl text-primary"><?php esc_html_e('Payment methods', 'woocommerce'); ?></h3>
-        <?php if (WC()->cart->needs_payment()) : ?>
-          <ul class="wc_payment_methods payment_methods methods">
-            <?php
-            if (!empty($available_gateways)) {
-              foreach ($available_gateways as $gateway) {
-                wc_get_template('checkout/payment-method.php', array('gateway' => $gateway));
-              }
-            } else {
-              echo '<li>';
-              wc_print_notice(apply_filters('woocommerce_no_available_payment_methods_message', WC()->customer->get_billing_country() ? esc_html__('Sorry, it seems that there are no available payment methods. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce') : esc_html__('Please fill in your details above to see available payment methods.', 'woocommerce')), 'notice'); // phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
-              echo '</li>';
-            }
-            ?>
-          </ul>
-        <?php endif; ?>
-      </div>
     </div>
   
     <?php endif; ?>
