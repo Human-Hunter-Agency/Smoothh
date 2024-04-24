becc27a4189f890f56a36a9de6de55adf563ece4
Kamil <k.medrala99@gmail.com>
2024-04-24 10:49:11 +0200

    checkout layout

--- theme/woocommerce/checkout/form-checkout.php
+++ theme/woocommerce/checkout/form-checkout.php
@@ -30,14 +30,14 @@ if (!$checkout->is_registration_enabled() && $checkout->is_registration_required
 
 ?>
 
-<form name="checkout" method="post" class="checkout woocommerce-checkout flex justify-between gap-6" action="<?php echo esc_url(wc_get_checkout_url()); ?>" enctype="multipart/form-data">
+<form name="checkout" method="post" class="checkout woocommerce-checkout flex justify-between gap-6 [&_.form-row]:!p-0" action="<?php echo esc_url(wc_get_checkout_url()); ?>" enctype="multipart/form-data">
 
   <?php if ($checkout->get_checkout_fields()) : ?>
 
     <?php do_action('woocommerce_checkout_before_customer_details'); ?>
 
-    <div id="customer_details !w-1/2">
-      <div class="px-5 md:px-8 py-9 border border-[#888] rounded-[15px]">
+    <div id="customer_details flex-1">
+      <div class="px-5 md:px-8 py-9 border border-[#888] rounded-[15px] mb-5">
         <?php do_action('woocommerce_checkout_billing'); ?>
       </div>
 
@@ -54,7 +54,7 @@ if (!$checkout->is_registration_enabled() && $checkout->is_registration_required
 
   <?php do_action('woocommerce_checkout_before_order_review'); ?>
 
-  <div class="!w-1/2 relative ">
+  <div class="md:w-1/2 xl:w-1/3 relative ">
     <div id="order_review" class="woocommerce-checkout-review-order md:sticky md:top-[115px] px-5 md:px-8 py-9 border border-[#888] rounded-[15px]">
       <h3 id="order_review_heading" class="!mt-0 mb-9 text-2xl text-primary"><?php esc_html_e('Your order', 'woocommerce'); ?></h3>
   
