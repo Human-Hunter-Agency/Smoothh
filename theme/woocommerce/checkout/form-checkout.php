62a1e206381dcc915c95f805873e7e2e53fd833f
Kamil <k.medrala99@gmail.com>
2024-04-24 11:11:37 +0200

    [checkout] notices position

--- theme/woocommerce/checkout/form-checkout.php
+++ theme/woocommerce/checkout/form-checkout.php
@@ -30,12 +30,12 @@ if (!$checkout->is_registration_enabled() && $checkout->is_registration_required
 
 ?>
 
-<form name="checkout" method="post" class="checkout woocommerce-checkout flex flex-col md:flex-row justify-between gap-6 xl:gap-12 [&_.form-row]:!p-0 mb-20" action="<?php echo esc_url(wc_get_checkout_url()); ?>" enctype="multipart/form-data">
+<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url(wc_get_checkout_url()); ?>" enctype="multipart/form-data">
   
 <?php if ($checkout->get_checkout_fields()) : ?>
   
-  
-  <div id="customer_details" class="flex-1">
+  <div class="flex flex-col md:flex-row justify-between gap-6 xl:gap-12 [&_.form-row]:!p-0 mb-20">
+    <div id="customer_details" class="flex-1">
       <?php do_action('woocommerce_checkout_before_customer_details'); ?>
       <div class="px-5 xl:px-8 py-9 border border-[#888] rounded-[15px] mb-6">
         <?php do_action('woocommerce_checkout_billing'); ?>
@@ -44,25 +44,21 @@ if (!$checkout->is_registration_enabled() && $checkout->is_registration_required
       <div class="px-5 xl:px-8 py-9 border border-[#888] rounded-[15px]">
         <?php do_action('woocommerce_checkout_shipping'); ?>
       </div>
+      <?php do_action('woocommerce_checkout_after_customer_details'); ?>
     </div>
-
-    <?php do_action('woocommerce_checkout_after_customer_details'); ?>
-
-  <?php endif; ?>
-
-  <?php do_action('woocommerce_checkout_before_order_review_heading'); ?>
-
-  <?php do_action('woocommerce_checkout_before_order_review'); ?>
-
-  <div class="md:w-1/2 xl:w-1/3 relative shrink-0">
-    <div id="order_review" class="woocommerce-checkout-review-order md:sticky md:top-[115px] px-5 xl:px-8 py-9 border border-[#888] rounded-[15px]">
-      <h3 id="order_review_heading" class="!mt-0 mb-9 text-2xl text-primary"><?php esc_html_e('Your order', 'woocommerce'); ?></h3>
   
-      <?php do_action('woocommerce_checkout_order_review'); ?>
+    <?php endif; ?>
+    
+    <div class="md:w-1/2 xl:w-1/3 relative shrink-0">
+      <?php do_action('woocommerce_checkout_before_order_review_heading'); ?>
+      <?php do_action('woocommerce_checkout_before_order_review'); ?>
+      <div id="order_review" class="woocommerce-checkout-review-order md:sticky md:top-[115px] px-5 xl:px-8 py-9 border border-[#888] rounded-[15px]">
+        <h3 id="order_review_heading" class="!mt-0 mb-9 text-2xl text-primary"><?php esc_html_e('Your order', 'woocommerce'); ?></h3>
+        <?php do_action('woocommerce_checkout_order_review'); ?>
+      </div>
+      <?php do_action('woocommerce_checkout_after_order_review'); ?>
     </div>
   </div>
-
-  <?php do_action('woocommerce_checkout_after_order_review'); ?>
 
 </form>
 
