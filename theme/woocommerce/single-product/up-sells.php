4cd7f5cdc4e88fe1e98c4510740483eb6353be91
Kamil <k.medrala99@gmail.com>
2024-04-25 13:31:29 +0200

    prod tiles sale price styles other sections

--- theme/woocommerce/single-product/up-sells.php
+++ theme/woocommerce/single-product/up-sells.php
@@ -55,9 +55,19 @@ if ($upsells) : ?>
                                         <?php echo get_the_title($upsell ->get_id()) ?>
                                     </h4>
                                     <?php if (is_user_logged_in() || is_prod_guest_available($upsell)) : ?>
-                                        <span class="text-lg md:text-xl">
-                                            <?php echo number_format( wc_get_price_excluding_tax($upsell), wc_get_price_decimals(), wc_get_price_decimal_separator(), wc_get_price_thousand_separator()) ?> netto
-                                        </span>
+                                        <div class="flex text-lg md:text-xl shrink-0">
+                                            <div class="flex flex-col items-end mr-2">
+                                                <span>
+                                                    <?php echo number_format( wc_get_price_excluding_tax($product), wc_get_price_decimals(), wc_get_price_decimal_separator(), wc_get_price_thousand_separator()) ?>
+                                                </span>
+                                                <?php if($product->is_on_sale()): ?>
+                                                    <span class="!text-lg !leading-4 h-5 text-black opacity-50 line-through font-normal">
+                                                        <?php echo number_format( $product->get_regular_price(), wc_get_price_decimals(), wc_get_price_decimal_separator(), wc_get_price_thousand_separator()) ?>                        
+                                                    </span>
+                                                <?php endif ?>
+                                            </div>
+                                            <span><?php esc_html_e('net','smoothh') ?></span>
+                                        </div>
                                     <?php endif; ?>
                                 </div>
                                 <p class="text-sm md:text-base prose-strong:font-semibold">
