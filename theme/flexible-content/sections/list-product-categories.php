75575f5d03dacc3eb68ed719734b20fbc301ec73
Kamil <k.medrala99@gmail.com>
2024-04-25 13:37:22 +0200

    'net' translations

--- theme/flexible-content/sections/list-product-categories.php
+++ theme/flexible-content/sections/list-product-categories.php
@@ -46,10 +46,10 @@
                                     <?php echo $cat->name ?> 
                                 </h4>
                                 <?php if (is_user_logged_in() || is_category_guest_available($cat)) : ?>
-                                    <span class="text-lg md:text-xl">
+                                    <span class="text-lg md:text-xl shrink-0">
                                         <?php
                                             echo number_format( $min_price, wc_get_price_decimals(), wc_get_price_decimal_separator(), wc_get_price_thousand_separator());
-                                        ?> netto
+                                        ?> <?php esc_html_e('net','smoothh') ?>
                                     </span>
                                 <?php endif; ?>
                             </div>
