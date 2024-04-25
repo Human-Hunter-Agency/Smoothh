c7a9e10ffb9400b315412a3d8f594fa6c6ea4555
Kamil <k.medrala99@gmail.com>
2024-04-25 15:11:06 +0200

    case studies swiper fix test

--- theme/flexible-content/sections/partials/case-studies-swiper.php
+++ theme/flexible-content/sections/partials/case-studies-swiper.php
@@ -21,19 +21,21 @@
             <div class="swiper !container !overflow-visible" data-js="swiper-tiles-default">
                 <div class="swiper-wrapper">
                     <?php foreach($posts as $post) : ?>
-                        <a href="<?php echo get_permalink($post->ID) ?>" class="swiper-slide !h-auto !flex items-center flex-col border-2 border-[#EFEFEF] rounded-2xl opacity-0 !transition duration-500 [&.swiper-slide-visible]:opacity-100">
-                            <div class="w-full relative mb-5 rounded-t-[14px] overflow-hidden">
-                                <img src="<?php echo get_the_post_thumbnail_url($post->ID); ?>" class="object-cover w-full !h-[190px] md:!h-[220px]" >
-                                <div class="absolute inset-0 bg-gradient-to-b from-primary/20 to-secondary/20"></div>
-                            </div>
-                            <div class="text-center p-3 md:p-6 !pt-0">
-                                <?php if ($post -> post_title) : ?>
-                                    <h3 class="text-base md:text-xl text-primary mb-9 font-semibold"><?php echo $post -> post_title; ?></h3>
-                                <?php endif; ?>
-                                <p class="text-sm md:text-base italic font-medium"><?php echo get_the_excerpt($post->ID); ?></p>
-                            </div>
-                            <span class="translate-y-1/2 rounded-[14px] text-[13px] font-bold py-2 px-7 text-white bg-primary hover:bg-secondary transition duration-200" ><?php esc_html_e('Read more','smoothh') ?></span>
-                        </a>
+                        <div>
+                            <a href="<?php echo get_permalink($post->ID) ?>" class="swiper-slide !h-auto !flex items-center flex-col border-2 border-[#EFEFEF] rounded-2xl opacity-0 !transition duration-500 [&.swiper-slide-visible]:opacity-100">
+                                <div class="w-full relative mb-5 rounded-t-[14px] overflow-hidden">
+                                    <img src="<?php echo get_the_post_thumbnail_url($post->ID); ?>" class="object-cover w-full !h-[190px] md:!h-[220px]" >
+                                    <span class="absolute inset-0 bg-gradient-to-b from-primary/20 to-secondary/20"></span>
+                                </div>
+                                <div class="text-center p-3 md:p-6 !pt-0">
+                                    <?php if ($post -> post_title) : ?>
+                                        <h3 class="text-base md:text-xl text-primary mb-9 font-semibold"><?php echo $post -> post_title; ?></h3>
+                                    <?php endif; ?>
+                                    <p class="text-sm md:text-base italic font-medium"><?php echo get_the_excerpt($post->ID); ?></p>
+                                </div>
+                                <span class="translate-y-1/2 rounded-[14px] text-[13px] font-bold py-2 px-7 text-white bg-primary hover:bg-secondary transition duration-200" ><?php esc_html_e('Read more','smoothh') ?></span>
+                            </a>
+                        </div>
                     <?php endforeach; ?>
                 </div>
                 <div class="swiper-button-prev">
