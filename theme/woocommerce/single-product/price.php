d3e1eae64fa83edbc368c1dd3ba962273bf783ba
Kamil <k.medrala99@gmail.com>
2024-04-25 12:00:45 +0200

    price visibility

--- theme/woocommerce/single-product/price.php
+++ theme/woocommerce/single-product/price.php
@@ -23,8 +23,8 @@ global $product;
 
 ?>
 <?php if (is_user_logged_in() || is_prod_guest_available($product)) : ?>
-    <div class="<?php echo esc_attr( apply_filters( 'woocommerce_product_price_class', 'price' ) ); ?> mb-2 flex items-end shrink-0 [&_.woocommerce-Price-currencySymbol]:hidden text-primary [&_bdi]:text-4xl [&_bdi]:!font-normal [&_bdi]:text-primary [&_ins]:no-underline [&_del_bdi]:!text-xl [&_del_bdi]:!text-black [&_del]:h-8 [&_del]:!decoration-black [&_del_bdi]:mr-1.5">
-        <p class="flex flex-col"><?php echo $product->get_price_html(); ?> </p>
+    <p class="<?php echo esc_attr( apply_filters( 'woocommerce_product_price_class', 'price' ) ); ?> mb-2 flex items-end shrink-0 [&_.woocommerce-Price-currencySymbol]:hidden text-primary [&_bdi]:text-4xl [&_bdi]:!font-normal [&_bdi]:text-primary [&_ins]:no-underline [&_del_bdi]:!text-xl [&_del_bdi]:!text-black [&_del]:h-8 [&_del]:!decoration-black [&_del_bdi]:mr-1.5">
+        <span class="flex flex-col"><?php echo $product->get_price_html(); ?> </span>
         <span class="net-label text-primary font-normal text-xl md:text-2xl"><?php esc_html_e('net','smoothh') ?></span>
-    </div>
+    </p>
 <?php endif; ?>
