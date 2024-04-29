c8dd6aa7bd899646baa883d03d09f027f180e90e
Kamil <k.medrala99@gmail.com>
2024-04-29 16:07:22 +0200

    form description fix

--- theme/flexible-content/sections/form-cv.php
+++ theme/flexible-content/sections/form-cv.php
@@ -10,10 +10,10 @@
 <div class="w-full py-10 md:py-[70px] mb:pb-20 text-white bg-gradient-to-b from-secondary to-primary">
     <div class="container">
         <?php if ($header) : ?>
-            <h2 class="text-3xl md:text-5xl text-bold font-bold mb-9 text-center" ><?php echo esc_html($header); ?></h1>
+            <h2 class="text-3xl md:text-5xl font-bold mb-9 text-center" ><?php echo esc_html($header); ?></h1>
         <?php endif; ?>        
         <?php if ($description) : ?>
-            <div class="prose-smoothh prose md:prose-xl text-white text-center mb-10" ><?php echo $description; ?></div>
+            <div class="prose-smoothh prose md:prose-xl text-white text-center mb-10 prose-h2:!text-white prose-h2:font-bold prose-h2:text-3xl prose-h2:md:text-5xl prose-h2:mb-9" ><?php echo $description; ?></div>
         <?php endif; ?>
         <div class="!max-w-[1410px] mx-auto <?php if ($hide_message_field) {echo '[&_.message-container]:hidden';} ?>">
             <?php echo do_shortcode('[contact-form-7 id="841318a" title="CV"]'); ?>
