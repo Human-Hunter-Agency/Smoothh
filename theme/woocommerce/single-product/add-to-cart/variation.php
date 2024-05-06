92a545fae47ae3f986a12e80fef2184fed6af49c
Kamil <k.medrala99@gmail.com>
2024-05-06 12:56:10 +0200

    variation quantity, price styles fix

--- theme/woocommerce/single-product/add-to-cart/variation.php
+++ theme/woocommerce/single-product/add-to-cart/variation.php
@@ -15,7 +15,7 @@ defined( 'ABSPATH' ) || exit;
 ?>
 <script type="text/template" id="tmpl-variation-template">
 	<div class="woocommerce-variation-description">{{{ data.variation.variation_description }}}</div>
-	<div class="woocommerce-variation-price mb-2 flex items-end shrink-0 [&_.woocommerce-Price-currencySymbol]:hidden text-primary [&_bdi]:text-4xl [&_del_bdi]:!text-xl [&_bdi]:!font-normal [&_bdi]:text-primary [&_ins]:no-underline [&_del_bdi]:!text-black [&_del]:h-8 [&_del]:!decoration-black [&_del_bdi]:mr-1.5">
+	<div class="woocommerce-variation-price mb-2 flex items-end justify-end shrink-0 [&_.woocommerce-Price-currencySymbol]:hidden text-primary [&_bdi]:text-4xl [&_del_bdi]:!text-xl [&_bdi]:!font-normal [&_bdi]:text-primary [&_ins]:no-underline [&_del_bdi]:!text-black [&_del]:h-8 [&_del]:!decoration-black [&_del_bdi]:mr-1.5">
         <span class="flex flex-col">
             {{{ data.variation.price_html }}}
         </span>    
