c6d37d640bf52d86d3c68ff02971df7d13497191
Kamil <k.medrala99@gmail.com>
2024-05-08 15:36:08 +0200

    checkout required fields

--- theme/woocommerce/checkout/form-shipping.php
+++ theme/woocommerce/checkout/form-shipping.php
@@ -36,6 +36,10 @@ defined('ABSPATH') || exit;
         <?php
         $fields = $checkout->get_checkout_fields('shipping');
 
+        if (!isset($field['custom_attributes']) && isset($field['required']) && $field['required'] === true) {
+          $field['custom_attributes'] = array('required' => 'required');
+        }
+
         foreach ($fields as $key => $field) {
           woocommerce_form_field($key, $field, $checkout->get_value($key));
         }
