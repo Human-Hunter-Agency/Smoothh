83cc6eefcb3608fea5d46374ed5bb5794b167fd1
Kamil <k.medrala99@gmail.com>
2024-05-09 13:56:18 +0200

    register terms checkbox required attr

--- theme/functions.php
+++ theme/functions.php
@@ -446,7 +446,7 @@ function smoothh_terms_and_conditions_to_registration()
 	?>
 		<p class="form-row terms wc-terms-and-conditions">
 			<label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox flex items-start justify-center gap-x-2 [&_a]:text-primary [&_a]:font-semibold [&_a:hover]:underline">
-				<input type="checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox mt-1 accent-secondary " name="terms" <?php checked(apply_filters('woocommerce_terms_is_checked_default', isset($_POST['terms'])), true); ?> id="terms" />
+				<input type="checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox mt-1 accent-secondary " name="terms" required <?php checked(apply_filters('woocommerce_terms_is_checked_default', isset($_POST['terms'])), true); ?> id="terms" />
 				<span>
 					<?php esc_html_e('I&rsquo;ve read and accept the', 'smoothh'); ?>
 					<?php printf(__('<a href="%s" target="_blank" class="woocommerce-terms-and-conditions-link">terms and conditions</a>', 'smoothh'), esc_url(wc_get_page_permalink('terms'))); ?></span> <span class="required">*</span>
