8076c68bf2badfa68619475ca020057abfe38015
Kamil <k.medrala99@gmail.com>
2024-05-10 10:59:45 +0200

    FAQ schema

--- theme/flexible-content/sections/list-faq.php
+++ theme/flexible-content/sections/list-faq.php
@@ -8,7 +8,7 @@ $list = $args['list'];
 ?>
 
 <div class="relative pb-5 pt-10 md:pt-[60px]">
-    <div class="container !max-w-[1410px]">
+    <div class="container !max-w-[1410px]" itemscope itemtype="https://schema.org/FAQPage">
         <?php if ($header) : ?>
             <h2 class="text-center font-bold text-2xl md:text-3xl lg:text-5xl mb-9 md:mb-14">
                 <?php echo esc_html($header); ?>
@@ -25,13 +25,10 @@ $list = $args['list'];
             $answer = $item['answer'];
         ?>
             <?php if ($question && $answer) : ?>
-                <div class="border-2 border-primary rounded-2xl mb-2.5" data-js="dropdown">
-                    <div class="group flex justify-between py-5 px-6 cursor-pointer" aria-expanded="<?php if (++$i === $numItems) {
-                                                                                                        echo 'true';
-                                                                                                    } else {
-                                                                                                        echo 'false';
-                                                                                                    } ?>" data-js="dropdown-toggle">
-                        <h3 class="text-base md:text-[20px] md:leading-5 group-hover:text-primary transition duration-200 font-semibold"><?php echo esc_html($question); ?></h3>
+                <div class="border-2 border-primary rounded-2xl mb-2.5" data-js="dropdown" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
+                    <div class="group flex justify-between py-5 px-6 cursor-pointer" 
+                    aria-expanded="<?php if (++$i === $numItems) { echo 'true';} else { echo 'false';} ?>" data-js="dropdown-toggle">
+                        <h3 class="text-base md:text-[20px] md:leading-5 group-hover:text-primary transition duration-200 font-semibold" itemprop="name"><?php echo esc_html($question); ?></h3>
                         <div class="p-1 relative">
                             <svg class="group-aria-expanded:rotate-180 transition duration-300" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <line y1="10.0015" x2="20" y2="10.0015" stroke="#8117EE" stroke-width="2" />
@@ -42,8 +39,8 @@ $list = $args['list'];
                         </div>
                     </div>
                     <div class="overflow-hidden h-0 transition-all duration-300" data-js="dropdown-container">
-                        <div class="py-5 px-6">
-                            <p class="text-sm md:text-base">
+                        <div class="py-5 px-6" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+                            <p class="text-sm md:text-base" itemprop="text">
                                 <?php echo esc_html($answer); ?>
                             </p>
                         </div>
@@ -52,31 +49,4 @@ $list = $args['list'];
             <?php endif; ?>
         <?php endforeach; ?>
     </div>
-    <script type="application/ld+json">
-        {
-            "@context": "https://schema.org",
-            "@type": "FAQPage",
-            "mainEntity": [
-            <?php 
-                foreach ($list as $item) :
-                $question = $item['question'];
-                $answer = $item['answer'];
-                    if ($question && $answer) :
-            ?>
-                {
-                    "@type": "Question",
-                    "name": "<?php echo esc_html($question); ?>",
-                    "acceptedAnswer": {
-                        "@type": "Answer",
-                        "text": "<?php echo esc_html($answer); ?>"
-                    }
-                }, 
-            
-            <?php
-                    endif;
-                endforeach; 
-            ?>
-            ]
-        }
-    </script>
 </div>
