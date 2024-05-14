8509892fb3d8a951cd0986f31ca58a81aaea762f
Kamil <k.medrala99@gmail.com>
2024-05-14 13:03:11 +0200

    account_type default type test

--- theme/functions.php
+++ theme/functions.php
@@ -407,8 +407,7 @@ function smoothh_save_extra_fields($customer_id)
 	if (isset($_POST['account_type'])) {
 		update_user_meta($customer_id, 'account_type', sanitize_text_field($_POST['account_type']));
 	}else{
-		$default_type = 'candidate';
-		update_user_meta($customer_id, 'account_type', sanitize_text_field($default_type));
+		smoothh_save_user_default_type($customer_id);
 	}
 }
 function smoothh_validate_extra_fields($errors)
@@ -459,10 +458,16 @@ function smoothh_terms_and_conditions_to_registration()
 	}
 }
 
+function smoothh_save_user_default_type($user_id){
+	$default_type = 'candidate';
+	update_user_meta($user_id, 'account_type', $default_type);
+}
+
 add_action('woocommerce_register_form_start', 'smoothh_my_account_page_woocommerce', 15);
 add_action('woocommerce_register_form', 'smoothh_terms_and_conditions_to_registration', 20);
 add_action('woocommerce_register_post', 'smoothh_validate_extra_fields_my_account', 10, 3);
 add_action('woocommerce_created_customer', 'smoothh_save_extra_fields');
+add_action('user_register', 'smoothh_save_user_default_type', 10, 1 );
 
 add_filter('woocommerce_billing_fields', 'smoothh_billing_address_add_nip');
 function smoothh_billing_address_add_nip($fields)
