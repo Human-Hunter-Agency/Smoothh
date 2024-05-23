81d109491c67656e7ec1b47e0af67c13e000b85e
Kamil <k.medrala99@gmail.com>
2024-05-23 16:08:10 +0200

    text before button

--- theme/woocommerce/checkout/terms.php
+++ theme/woocommerce/checkout/terms.php
@@ -34,7 +34,8 @@ if ( apply_filters( 'woocommerce_checkout_show_terms', true ) && function_exists
 			</p>
 		<?php endif; ?>
 	</div>
-	<?php
+	
+    <?php
 
 	do_action( 'woocommerce_checkout_after_terms_and_conditions' );
 }
