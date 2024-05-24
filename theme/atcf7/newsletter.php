3cab3e7384407e5df7cb7ca3748c2ec3c4359c61
Kamil <k.medrala99@gmail.com>
2024-05-24 17:11:51 +0200

    new consent_marketing GDPR consent ( newsletter)

--- theme/atcf7/newsletter.php
+++ theme/atcf7/newsletter.php
@@ -6,10 +6,24 @@ if (!defined('ABSPATH')) {
  * Template Name: newsletter
  * */
 ?>
-
-<div class="newsletter-input">
-[email* your-email id:your-email placeholder "Adres e-mail"]
-    <div class="submit-container">
-        [submit "Zapisz mnie"]
+<div>
+  <div class="newsletter-input">
+  [email* your-email id:your-email placeholder "Adres e-mail"]
+      <div class="submit-container">
+          [submit "Zapisz mnie"]
+      </div>
+  </div>
+  <div class="max-w-[505px] has-tooltip">
+      [acceptance consent_marketing] <?=
+      __('I consent to the processing of my personal data (name, e-mail address) by the Service Provider (here please provide the name and surname / name and address of the Service Provider) for marketing purposes.', 'smoothh' )
+      ?> 
+      <div class="tooltip">
+        <div class="tooltip-icon">i</div>
+        <span class="tooltip-text">
+          <?= sprintf(__('Expressing consent is voluntary. I have the right to withdraw consent at any time without affecting the lawfulness of processing based on consent before its withdrawal. I have the right to access my data and rectify it, delete it, limit processing, and the right to transfer data on the terms contained in the %sprivacy policy%s of the website. Personal data on the website are processed in accordance with the %sprivacy policy%s. We encourage you to read the policy before agreeing.', 'gdpr-framework'),
+		      "<a href='{$policyPageUrl}' target='_blank'>","</a>","<a href='{$policyPageUrl}' target='_blank'>","</a>") ?>
+        </span>
+      </div>
+      [/acceptance]
     </div>
 </div>
