8b7b21fe840746a1c4603b381ffd244ae09ddbb0
Kamil <k.medrala99@gmail.com>
2024-05-24 16:19:15 +0200

    quote tooltip styles test

--- theme/atcf7/cv.php
+++ theme/atcf7/cv.php
@@ -31,7 +31,7 @@ if (!defined('ABSPATH')) {
   </div>
 
   <div class="max-w-[520px] mb-5 text-center [&_a]:!text-white hover:[&_a]:!text-white [&_input]:!accent-secondary">
-    <div class="mb-5 relative [&_p]:inline-block">
+    <div class="mb-5 relative">
       [acceptance gdpr_woo_consent] <?php
       $policyPageUrl = get_permalink(wc_privacy_policy_page_id()); 
       echo sprintf(
@@ -41,7 +41,7 @@ if (!defined('ABSPATH')) {
       ); 
       ?> 
       <div class="tooltip">
-        <em class="rounded-full text-foreground bg-[#EFEFEF] text-xs size-4 pt-px font-semibold">i</em>
+        <em class="size-3 rounded-full text-white bg-gray-600">i</em>
         <span class="tooltip-text">
           <?php wc_registration_privacy_policy_text() ?>
         </span>
