d941d96b9de66d689bbc818cb23d0c01f3bf7a2a
Kamil <k.medrala99@gmail.com>
2024-05-24 16:49:54 +0200

    tooltip text 10px, quote form test

--- theme/atcf7/quote.php
+++ theme/atcf7/quote.php
@@ -28,20 +28,27 @@ if (!defined('ABSPATH')) {
   <div class="w-full mb-5">
     [textarea your-message x3 placeholder "Twoja wiadomość"]
   </div>
-  <div class="max-w-[520px]">
-    <div class="mb-5">
-      [acceptance gdpr_woo_consent] <?php
+  <div class="max-w-[540px]">
+    <div class="mb-5 has-tooltip">
+    [acceptance gdpr_woo_consent] <?php
       $policyPageUrl = get_permalink(wc_privacy_policy_page_id()); 
       echo sprintf(
         __('I accept the %sPrivacy Policy%s', 'gdpr-framework'),
         "<a href='{$policyPageUrl}' target='_blank'>",
         "</a>"
       ); 
-      ?> [/acceptance]
+      ?> 
+      <div class="tooltip">
+        <div class="tooltip-icon">i</div>
+        <span class="tooltip-text">
+          <?php wc_registration_privacy_policy_text() ?>
+        </span>
+      </div>
+      [/acceptance]
     </div>
     <?php wc_registration_privacy_policy_text() ?>
   </div>
-  
+
   <div class="btn-color relative text-center max-w-full w-fit mx-auto [&_.wpcf7-spinner]:absolute [&_.wpcf7-spinner]:right-[-7px] [&_.wpcf7-spinner]:top-4 mb-9 ">
     [submit "Wyślij wiadomość"]
     <svg class="absolute right-5 top-[18px] -rotate-90" width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
