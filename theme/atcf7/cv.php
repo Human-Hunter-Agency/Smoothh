2bc073942cd80c523a60ef4d97d8c28002d7184b
Kamil <k.medrala99@gmail.com>
2024-05-24 16:12:51 +0200

    quote tooltip

--- theme/atcf7/cv.php
+++ theme/atcf7/cv.php
@@ -31,7 +31,7 @@ if (!defined('ABSPATH')) {
   </div>
 
   <div class="max-w-[520px] mb-5 text-center [&_a]:!text-white hover:[&_a]:!text-white [&_input]:!accent-secondary">
-    <div class="mb-5 relative">
+    <div class="mb-5 relative [&_p]:inline-block">
       [acceptance gdpr_woo_consent] <?php
       $policyPageUrl = get_permalink(wc_privacy_policy_page_id()); 
       echo sprintf(
@@ -41,7 +41,7 @@ if (!defined('ABSPATH')) {
       ); 
       ?> 
       <div class="tooltip">
-        <em class="size-3 rounded-full text-white bg-gray-600">i</em>
+        <span class="rounded-full text-foreground bg-[#EFEFEF] text-xs size-[18px] pt-0.5 font-semibold">i</span>
         <span class="tooltip-text">
           <?php wc_registration_privacy_policy_text() ?>
         </span>
