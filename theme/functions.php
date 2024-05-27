b5750d3d7159f28ee2af5d3fc77e0fe2eaa25c7b
Kamil <k.medrala99@gmail.com>
2024-05-27 08:53:49 +0200

    terms description text

--- theme/functions.php
+++ theme/functions.php
@@ -862,7 +862,7 @@ function gdpr_register_smoothh_consents()
 	gdpr('consent')->register(
 		'terms', 
 		sprintf( __( '<a href="%s" target="_blank">Terms and Conditions</a> consent', 'smoothh' ), get_permalink(wc_terms_and_conditions_page_id()) ),
-		__('This consent is visible by default on woocommerce checkout page. If someone wishes to withdraw it, they should simply request to delete all their data','gdpr-framework'),
+		wc_terms_and_conditions_checkbox_text(),
 		true
     );
 	
