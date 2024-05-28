771d498f536a891e4f8230e3ff764a15b9b579c1
Kamil <k.medrala99@gmail.com>
2024-05-28 12:35:12 +0200

    categories tiles fix

--- theme/flexible-content/sections/list-product-categories.php
+++ theme/flexible-content/sections/list-product-categories.php
@@ -36,7 +36,9 @@
                 <li>
                     <a href="<?php echo get_term_link( $cat->term_id, 'product_cat' ); ?>" class="group h-full flex items-center flex-col border-2 border-[#EFEFEF] rounded-2xl">
                         <div class="relative overflow-hidden rounded-t-[14px] w-full !h-[190px] md:!h-[220px] [&_img]:object-cover [&_img]:w-full [&_img]:h-full">
-                            <?php echo wp_get_attachment_image($thumbnail_id,'medium',false,array('loading' => 'lazy','alt' => $cat->name)); ?>
+                            <?php
+                                $thumbnail_id = get_term_meta( $cat->term_id, 'thumbnail_id', true );
+                                echo wp_get_attachment_image($thumbnail_id,'medium',false,array('loading' => 'lazy','alt' => $cat->name)); ?>
                             <div class="absolute inset-0 bg-gradient-to-b from-primary/20 to-secondary/20"></div>
                         </div>
                         <div class="w-full flex-1 p-3 md:p-6">
