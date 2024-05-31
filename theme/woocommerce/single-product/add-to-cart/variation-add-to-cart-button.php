fb879a0f173e089624bcb3b723dec41480bd225c
Kamil <k.medrala99@gmail.com>
2024-05-31 15:45:06 +0200

    display quantity if product is hourly fix

--- theme/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ theme/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -13,19 +13,21 @@ global $product;
 
 $is_hourly = get_field('product_hourly', $product->get_id());
 ?>
-<div class="woocommerce-variation-add-to-cart variations_button flex <?php if(isset($is_hourly) && $is_hourly){echo ' [&_.quantity]:!mr-3 items-center';}else{ echo '[&_.quantity]:!hidden';} ?>">
+<div class="woocommerce-variation-add-to-cart variations_button flex items-center">
 	<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
 	<?php
 	do_action( 'woocommerce_before_add_to_cart_quantity' );
 
-	woocommerce_quantity_input(
-		array(
-			'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
-			'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
-			'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
-		)
-	);
+	if(isset($is_hourly) && $is_hourly){
+		woocommerce_quantity_input(
+			array(
+				'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
+				'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
+				'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
+			)
+		);
+	}
 
 	do_action( 'woocommerce_after_add_to_cart_quantity' );
 	?>
