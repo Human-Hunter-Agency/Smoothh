f42a70cdd751f314e276d515f8e51cf3312985f7
Tomek <TK-54-4B>
2024-05-31 17:04:14 +0200

    cart promo price fix

--- theme/woocommerce/cart/cart.php
+++ theme/woocommerce/cart/cart.php
@@ -87,7 +87,9 @@ do_action('woocommerce_before_cart'); ?>
                         <div class="product-price flex justify-between md:justify-end grow-0 md:w-[15%] text-right" data-title="<?php esc_attr_e('Price', 'woocommerce'); ?>">
                             <span class="md:hidden text-base"><?php esc_html_e('Price', 'woocommerce'); ?>:</span>
                             <div class="flex flex-col items-end">
-                                <span><?php echo $_product->get_price_html(); ?> <?php esc_html_e('net', 'smoothh') ?><?php if(get_field('product_hourly', $_product->get_id())){echo '/h';}?></span>
+                                <span class="[&_ins]:no-underline"><?php echo $_product->get_price_html(); ?> <?php esc_html_e('net', 'smoothh') ?><?php if (get_field('product_hourly', $_product->get_id())) {
+                                                                                                                                                        echo '/h';
+                                                                                                                                                    } ?></span>
                                 <span class="ml-2 text-sm md:text-base mt-1.5 md:mt-0.5 whitespace-nowrap"> <?php echo '( ' . wc_price(wc_get_price_including_tax($_product)) ?> <?php echo esc_html_e('gross', 'smoothh') . ' )'; ?></span>
                             </div>
                         </div>
@@ -122,7 +124,9 @@ do_action('woocommerce_before_cart'); ?>
                         <div class="product-subtotal flex justify-between md:justify-end grow-0 md:w-[15%] text-right" data-title="<?php esc_attr_e('Subtotal', 'woocommerce'); ?>">
                             <span class="md:hidden text-base"><?php esc_html_e('Subtotal', 'woocommerce'); ?>:</span>
                             <div class="flex flex-col items-end">
-                                <span><?php echo number_format($cart_item['line_total'], wc_get_price_decimals(), wc_get_price_decimal_separator(), wc_get_price_thousand_separator()) . ' ' . get_woocommerce_currency_symbol() ?> <?php esc_html_e('net', 'smoothh') ?><?php if(get_field('product_hourly', $_product->get_id())){echo '/h';}?></span>
+                                <span><?php echo number_format($cart_item['line_total'], wc_get_price_decimals(), wc_get_price_decimal_separator(), wc_get_price_thousand_separator()) . ' ' . get_woocommerce_currency_symbol() ?> <?php esc_html_e('net', 'smoothh') ?><?php if (get_field('product_hourly', $_product->get_id())) {
+                                                                                                                                                                                                                                                                                echo '/h';
+                                                                                                                                                                                                                                                                            } ?></span>
                                 <span class="ml-2 text-sm md:text-base mt-1.5 md:mt-0.5 whitespace-nowrap"> <?php echo '( ' . $cart_item['line_total'] + $cart_item['line_tax'] . ' ' . get_woocommerce_currency_symbol(); ?> <?php echo  esc_html_e('gross', 'smoothh') . ' )'; ?></span>
                             </div>
                         </div>
