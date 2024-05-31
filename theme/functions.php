8aa3c8fb6c8f9fa9a71af42d8fb73001d0b4a526
Tomek <TK-54-4B>
2024-05-31 11:29:37 +0200

    cart, gross price fix

--- theme/functions.php
+++ theme/functions.php
@@ -975,7 +975,9 @@ function has_sale($product)
 add_filter('woocommerce_cart_product_price', 'smoothh_woocommerce_cart_product_price_filter', 10, 2);
 function smoothh_woocommerce_cart_product_price_filter($wc_price, $product)
 {
-	$tax_element = '<span class="text-base text-right text-foreground font-normal">( ' . wc_price(wc_get_price_including_tax($product)) . esc_html_e('gross', 'smoothh') . ' )</span>';
+	$tax_element = '<span class="text-base text-right text-foreground font-normal">( ' . wc_price(wc_get_price_including_tax($product));
+	echo esc_html_e('gross', 'smoothh');
+	echo  ' )</span>';
 	return $wc_price . $tax_element;
 }
 
