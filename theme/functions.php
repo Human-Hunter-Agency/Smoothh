1987a564a9a7ac866f6950133beb5498ed1e9ee3
Tomek <TK-54-4B>
2024-05-31 18:01:20 +0200

    aos test

--- theme/functions.php
+++ theme/functions.php
@@ -152,8 +152,6 @@ add_action('widgets_init', 'smoothh_widgets_init');
 function smoothh_scripts()
 {
 	wp_enqueue_style('smoothh-style', get_stylesheet_uri(), array(), SMOOTHH_VERSION);
-	// wp_enqueue_style('aos', get_template_directory_uri() . '/aos/dist/aos.css', array(), SMOOTHH_VERSION);
-	// wp_enqueue_style('aos2', get_template_directory_uri() . '/theme/js/script.css', array(), SMOOTHH_VERSION);
 	wp_enqueue_script('smoothh-script', get_template_directory_uri() . '/js/script.min.js', array('wp-i18n'), SMOOTHH_VERSION, true);
 	wp_set_script_translations('smoothh-script', 'smoothh');
 
