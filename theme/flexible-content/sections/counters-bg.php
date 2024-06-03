1dc74575e1aed3d33cc7992bf0ee0cc110974d76
Tomek <TK-54-4B>
2024-06-03 20:51:15 +0200

    section counter up new design

--- theme/flexible-content/sections/counters-bg.php
+++ theme/flexible-content/sections/counters-bg.php
@@ -7,7 +7,7 @@ $tiles_list = $args['tiles_list'];
 
 ?>
 
-<div class="relative w-full py-10 pb-5 md:pt-[70px] mt-10 md:mt-20">
+<div class="relative w-full py-10 pb-5 md:pt-[70px] mt-10 md:mb-20">
     <div class="z-[-1] w-full h-full absolute left-0 bg-gradient-to-r from-[rgba(129,23,238,0.1)] to-[rgba(129,23,238,0)]"></div>
     <div class="pt-14 relative z-0 container">
         <?php if ($header) : ?>
@@ -20,7 +20,7 @@ $tiles_list = $args['tiles_list'];
                 <?php foreach ($tiles_list as $tile) :
                     $tileID++;
                 ?>
-                    <div class="relative flex flex-col items-center text-left <?php if ($tileID == 1 || $tileID == 2) : ?> before:content-[''] before:w-1/2 before:bg-[#1F97D4] before:h-[2px] before:absolute before:bottom-[-70px] before:left-0 <?php endif; ?>">
+                    <div class="relative flex flex-col items-center text-left <?php if ($tileID == 1 || $tileID == 2) : ?> lg:before:content-[''] before:w-1/2 before:bg-[#1F97D4] before:h-[2px] before:absolute before:bottom-[-70px] before:left-0 <?php endif; ?>">
                         <div class="mb-5 md:mb-7 self-start">
                             <?php if ($tile['title']) : ?>
                                 <h3 class="text-2xl md:text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-[rgba(31,151,212,1)] to-[rgba(129,23,238,1)]"><?php echo $tile['title']; ?></h3>
