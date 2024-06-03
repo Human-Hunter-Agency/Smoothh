b9df0589abdc59bea04560a481a4e76c937a55ac
Kamil <k.medrala99@gmail.com>
2024-06-03 16:40:48 +0200

    leave number form

--- theme/woocommerce/checkout/form-checkout.php
+++ theme/woocommerce/checkout/form-checkout.php
@@ -77,33 +77,36 @@ if (!$checkout->is_registration_enabled() && $checkout->is_registration_required
               <button data-js-popup-toggle="leave-number-form" type="button" class="!min-h-[55px] !px-5 xl:!px-10 text-foreground font-bold bg-white group-hover:bg-secondary group-hover:text-white transition duration-200 !rounded-[14px]"><?php esc_html_e('Leave your number','smoothh')?></button>
             </div>
           </div>
-          <div data-js-popup-container="leave-number-form" class="popup-container popup-hidden">
-            <div class="popup-inner">
-              <div class="flex justify-end p-1">
-                <button data-js-popup-toggle="leave-number-form" class="p-1 md:p-2 group">				
-                  <svg class="fill-black transition duration-200 group-hover:fill-primary" height="18px" width="18px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
-                    viewBox="0 0 460.775 460.775" xml:space="preserve">
-                  <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55
-                    c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55
-                    c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505
-                    c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55
-                    l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719
-                    c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"/>
-                  </svg>
-                </button>
-              </div>
-              <div>
-                <?php 
-                  $shortcode = '[contact-form-7 id="fd3ecea" title="Zostaw numer"]';
-                  echo do_shortcode($shortcode); ?>
-              </div>
-            </div>
-          </div>
         <?php endif;?>
       </div>
     </div>
   </div>
 
 </form>
+
+<?php if ($display_number_form): ?>
+  <div data-js-popup-container="leave-number-form" class="popup-container popup-hidden not-prose">
+    <div class="popup-inner">
+      <div class="flex justify-end p-1">
+        <button data-js-popup-toggle="leave-number-form" class="p-1 md:p-2 group">				
+          <svg class="fill-black transition duration-200 group-hover:fill-primary" height="18px" width="18px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+            viewBox="0 0 460.775 460.775" xml:space="preserve">
+          <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55
+            c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55
+            c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505
+            c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55
+            l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719
+            c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"/>
+          </svg>
+        </button>
+      </div>
+      <div>
+        <?php 
+          $shortcode = '[contact-form-7 id="fd3ecea" title="Zostaw numer"]';
+          echo do_shortcode($shortcode); ?>
+      </div>
+    </div>
+  </div>
+<?php endif;?>
 
 <?php do_action('woocommerce_after_checkout_form', $checkout); ?>
