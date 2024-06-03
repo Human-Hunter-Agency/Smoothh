70e98ef190bee8d0aa94554d4a2b1c1829c4e1c0
Tomek <TK-54-4B>
2024-06-03 21:40:34 +0200

    new cta design

--- theme/flexible-content/sections/cta-without-img.php
+++ theme/flexible-content/sections/cta-without-img.php
@@ -8,7 +8,7 @@ $button = $args['button'];
 ?>
 
 <div class="container mx-auto">
-    <div class="py-10 md:py-16 relative flex flex-row items-center justify-between  drop-shadow-2xl rounded-xl bg-gradient-to-b from-secondary to-primary">
+    <div class="px-24 py-10 md:py-16 relative flex flex-row items-center justify-between  drop-shadow-2xl rounded-xl bg-gradient-to-b from-secondary to-primary">
         <?php if ($header) : ?>
             <h3 class="text-3xl md:text-5xl text-bold text-white font-bold text-center"><?php echo esc_html($header); ?></h1>
             <?php endif; ?>
@@ -18,7 +18,7 @@ $button = $args['button'];
                 $btn_title = $button['title'];
                 $btn_target = $button['target'] ? $button['target'] : '_self';
             ?>
-                <a href="<?php echo esc_url($btn_url); ?>" target="<?php echo esc_attr($btn_target); ?>" class="rounded-[14px] text-xl font-bold py-2 px-5 md:px-[70px] border-2 border-white text-white bg-transparent hover:bg-white/20 transition duration-200"><?php echo esc_html($btn_title); ?></a>
+                <a href="<?php echo esc_url($btn_url); ?>" target="<?php echo esc_attr($btn_target); ?>" class="rounded-3xl text-xl font-bold py-2 px-5 md:px-12 border-2 border-white text-white bg-transparent hover:bg-white/20 transition duration-200"><?php echo esc_html($btn_title); ?></a>
             <?php endif; ?>
     </div>
 
