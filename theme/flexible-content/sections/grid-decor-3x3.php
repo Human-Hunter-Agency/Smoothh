965c2197f40133a65a20e647b22907fbb0a5a23e
Tomek <TK-54-4B>
2024-06-03 18:14:48 +0200

    [new design] section with grid decor

--- theme/flexible-content/sections/grid-decor-3x3.php
+++ theme/flexible-content/sections/grid-decor-3x3.php
@@ -9,13 +9,13 @@ $description = $args['description'];
 $logo = $args['logo'];
 ?>
 
-<div class="relative py-10 md:py-20">
+<div class="relative py-10 md:py-20 mt-20">
     <div class="bg-container z-[-1] w-[85%] h-[1200px] absolute top-8 right-0 bg-gradient-to-l from-[rgba(129,23,238,0)] to-[rgba(129,23,238,0.102)]"></div>
     <div class="container">
         <div class="mb-44 flex flex-col lg:flex-row gap-24">
             <div class="z-0 w-1/2">
                 <?php if ($image_3x3) : ?>
-                    <div class="decor-3x3 -mt-14">
+                    <div class="decor-3x3 -mt-20">
                         <?php echo smoothh_img_responsive($image_3x3, '', null, 'lazy') ?>
                     </div>
                 <?php else : ?>
@@ -41,12 +41,12 @@ $logo = $args['logo'];
             <?php endif; ?>
 
             <?php if ($description) : ?>
-                <div class="py-12 px-28 bg-white rounded-[45px] drop-shadow-2xl">
-                    <div class="prose-smoothh prose md:prose-xl prose-h3:text-2xl md:prose-h3:text-5xl lg:prose-h3:text-5xl prose-img:mt-0 prose-img:mx-auto prose-img:px-5 md:prose-p:leading-8">
+                <div class="py-12 px-28 mb-20 bg-white rounded-[45px] drop-shadow-2xl">
+                    <div class="mb-20 prose-smoothh prose md:prose-xl prose-h3:text-2xl md:prose-h3:text-5xl lg:prose-h3:text-5xl prose-img:mt-0 prose-img:mx-auto prose-img:px-5 md:prose-p:leading-8">
                         <?php echo $description; ?>
                     </div>
                     <?php if ($logo) : ?>
-                        <div class="">
+                        <div class="w-full flex items-center justify-center">
                             <?php echo smoothh_img_responsive($logo, '', null, 'lazy') ?>
                         </div>
                     <?php endif; ?>
