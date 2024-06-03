488058ee9106103b736cea82bbe6ecf75ff381d7
Tomek <TK-54-4B>
2024-06-03 18:52:59 +0200

    section steps redesign

--- theme/flexible-content/sections/steps-bg.php
+++ theme/flexible-content/sections/steps-bg.php
@@ -12,7 +12,7 @@ $steps_list = $args['steps_list'];
 
     <div class="relative z-0 container">
         <?php if ($header) : ?>
-            <div class="mb-12 prose-smoothh prose md:prose-xl prose-h3:text-2xl md:prose-h3:text-5xl lg:prose-h3:text-5xl prose-img:mt-0 prose-img:mx-auto prose-img:px-5 md:prose-p:leading-8">
+            <div class="mb-6 prose-smoothh prose md:prose-xl prose-h3:text-2xl md:prose-h3:text-5xl lg:prose-h3:text-5xl prose-img:mt-0 prose-img:mx-auto prose-img:px-5 md:prose-p:leading-8">
                 <?php echo $header; ?>
             </div>
         <?php endif; ?>
@@ -28,18 +28,18 @@ $steps_list = $args['steps_list'];
                         <div class="swiper-slide !h-auto xl:flex-1">
                             <div class="flex flex-col items-center text-center p-3 md:p-6 pb-5 md:pb-10">
                                 <?php if ($step['icon'] && $step['icon']['url']) : ?>
-                                    <?php echo smoothh_img_responsive($step['icon'], 'object-contain size-16 mb-8 md:mb-[50px]', array(130, 130), 'lazy'); ?>
+                                    <?php echo smoothh_img_responsive($step['icon'], 'object-contain h-16 mb-8 md:mb-[50px]', array(130, 130), 'lazy'); ?>
                                 <?php endif; ?>
                                 <div class="min-h-[90px] lg:min-h-[110px] font-semibold">
                                     <?php if ($step['title']) : ?>
-                                        <h3 class="text-2xl md:text-[32px]"><?php echo $step['title']; ?></h3>
+                                        <h3 class="text-lg md:text-xl"><?php echo $step['title']; ?></h3>
                                     <?php endif; ?>
                                     <?php if ($step['subtitle']) : ?>
                                         <h4 class="text-lg md:text-xl"><?php echo $step['subtitle']; ?></h4>
                                     <?php endif; ?>
                                 </div>
                                 <?php if ($step['description']) : ?>
-                                    <p class="text-sm md:text-base mt-10"><?php echo $step['description']; ?></p>
+                                    <p class="text-sm md:text-base"><?php echo $step['description']; ?></p>
                                 <?php endif; ?>
                             </div>
                         </div>
