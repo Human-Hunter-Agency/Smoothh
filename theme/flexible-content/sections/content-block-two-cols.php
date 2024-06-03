0d2dfb7452c972faa3503317e63040df5599aba3
Tomek <TK-54-4B>
2024-06-03 16:39:52 +0200

    [new design] new section with big decor right side

--- theme/flexible-content/sections/content-block-two-cols.php
+++ theme/flexible-content/sections/content-block-two-cols.php
@@ -11,11 +11,6 @@ $decoration = $args['decoration'];
     <div class="container">
         <div class="flex gap-10">
             <div class="z-0 w-1/2">
-                <?php if ($header) : ?>
-                    <h2 class="text-left font-bold text-2xl md:text-3xl lg:text-5xl mb-9 md:mb-14">
-                        <?php echo esc_html($header); ?>
-                    </h2>
-                <?php endif; ?>
                 <?php if ($content) : ?>
                     <div class="prose-smoothh prose md:prose-xl prose-h3:text-2xl md:prose-h3:text-5xl lg:prose-h3:text-5xl prose-img:mt-0 prose-img:mx-auto prose-img:px-5 md:prose-p:leading-8">
                         <?php echo $content; ?>
