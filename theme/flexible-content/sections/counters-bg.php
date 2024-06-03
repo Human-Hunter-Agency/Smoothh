416bf62fb661ff96359eb82cbe7372a208cd8145
Tomek <TK-54-4B>
2024-06-03 20:49:03 +0200

    section counter up new design

--- theme/flexible-content/sections/counters-bg.php
+++ theme/flexible-content/sections/counters-bg.php
@@ -19,9 +19,8 @@ $tiles_list = $args['tiles_list'];
             <div class="py-10 lg:py-20 px-8 lg:px-36 grid grid-cols-1 gap-10 sm:grid-cols-2 lg:gap-x-[110px] lg:gap-y-[90px] xl:gap-10 2xl:gap-[140px] bg-white rounded-[45px] drop-shadow-2xl">
                 <?php foreach ($tiles_list as $tile) :
                     $tileID++;
-                    echo $tileID;;
                 ?>
-                    <div class="relative flex flex-col items-center text-left before:content-[''] before:w-1/2 before:bg-[#1F97D4] before:h-[2px] before:absolute before:bottom-[-70px] before:left-0">
+                    <div class="relative flex flex-col items-center text-left <?php if ($tileID == 1 || $tileID == 2) : ?> before:content-[''] before:w-1/2 before:bg-[#1F97D4] before:h-[2px] before:absolute before:bottom-[-70px] before:left-0 <?php endif; ?>">
                         <div class="mb-5 md:mb-7 self-start">
                             <?php if ($tile['title']) : ?>
                                 <h3 class="text-2xl md:text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-[rgba(31,151,212,1)] to-[rgba(129,23,238,1)]"><?php echo $tile['title']; ?></h3>
