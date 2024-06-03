99a45466255860304294b7b0c8fbb66c8dedff83
Tomek <TK-54-4B>
2024-06-03 17:54:29 +0200

    [new design] section with grid decor

--- theme/flexible-content/sections/grid-decor-3x3.php
+++ theme/flexible-content/sections/grid-decor-3x3.php
@@ -4,17 +4,19 @@
 
 $content = $args['content'];
 $image_3x3 = $args['image_3x3'];
-
+$title2 = $args['title2'];
+$description = $args['description'];
+$logo = $args['logo'];
 ?>
 
 <div class="relative py-10 md:py-20">
-    <div class="bg-container w-[85%] h-[1200px] absolute top-0 right-0 bg-gradient-to-l from-[rgba(129,23,238,0)] to-[rgba(129,23,238,0.102)]"></div>
+    <div class="bg-container w-[85%] h-[1200px] absolute top-8 right-0 bg-gradient-to-l from-[rgba(129,23,238,0)] to-[rgba(129,23,238,0.102)]"></div>
     <div class="container">
         <div class="flex flex-col lg:flex-row gap-24">
             <div class="z-0 w-1/2">
                 <?php if ($image_3x3) : ?>
                     <div class="decor-3x3 -mt-14">
-                        <?php echo smoothh_img_responsive($image_3x3, 'test', null, 'lazy') ?>
+                        <?php echo smoothh_img_responsive($image_3x3, '', null, 'lazy') ?>
                     </div>
                 <?php else : ?>
                     <div class="decor-3x3-default -m-10">
@@ -29,6 +31,28 @@ $image_3x3 = $args['image_3x3'];
                     </div>
                 <?php endif; ?>
             </div>
+        </div>
+
+        <div class="meet-sgp">
+            <?php if ($content2) : ?>
+                <div class="prose-smoothh prose md:prose-xl prose-h3:text-2xl md:prose-h3:text-5xl lg:prose-h3:text-5xl prose-img:mt-0 prose-img:mx-auto prose-img:px-5 md:prose-p:leading-8">
+                    <?php echo $content2; ?>
+                </div>
+            <?php endif; ?>
+
+            <?php if ($description) : ?>
+                <div class="py-12 px-30 rounded-xl shadow-lg">
+                    <div class="prose-smoothh prose md:prose-xl prose-h3:text-2xl md:prose-h3:text-5xl lg:prose-h3:text-5xl prose-img:mt-0 prose-img:mx-auto prose-img:px-5 md:prose-p:leading-8">
+                        <?php echo $description; ?>
+                    </div>
+                </div>
+            <?php endif; ?>
+
+            <?php if ($logo) : ?>
+                <div class="prose-smoothh prose md:prose-xl prose-h3:text-2xl md:prose-h3:text-5xl lg:prose-h3:text-5xl prose-img:mt-0 prose-img:mx-auto prose-img:px-5 md:prose-p:leading-8">
+                    <?php echo smoothh_img_responsive($logo, '', null, 'lazy') ?>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
 
