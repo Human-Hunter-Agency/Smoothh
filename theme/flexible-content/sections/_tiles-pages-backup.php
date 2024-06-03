472878a5b27fe91f1cfce88f54561cd0612d3760
Tomek <TK-54-4B>
2024-06-03 19:05:48 +0200

    section tiles pages swiper redesign

--- theme/flexible-content/sections/_tiles-pages-backup.php
+++ theme/flexible-content/sections/_tiles-pages-backup.php
@@ -1,46 +1,44 @@
 <?php
-
 /** Template to display 'Sekcja z kafelkami stron' - tiles_pages */
 
-$header = $args['header'];
-$description = $args['description'];
-$decoration = $args['decoration'];
-$titles_primary = $args['titles_primary'];
-$tiles_list = $args['tiles_list'];
+    $header = $args['header'];
+    $description = $args['description'];
+    $decoration = $args['decoration'];
+    $titles_primary = $args['titles_primary'];
+    $tiles_list = $args['tiles_list'];
 
 ?>
 
 <div class="relative py-10 md:py-[60px] mb:pb-[60px]">
-    <div class="z-[-1] xl:w-[85%] 2xl:w-[88%] lg:h-[1024px] absolute top-8 right-0 bg-gradient-to-l from-[rgba(31,151,212,0)] to-[rgba(31, 151, 212, 0.102)]"></div>
-    <?php if ($decoration) : ?>
+    <?php if ($decoration) : ?>        
         <div class="absolute right-7 top-7 -z-10 opacity-5 max-w-[90px] lg:max-w-[180px]">
-            <?php echo file_get_contents(get_stylesheet_directory_uri() . '/assets/img/decor.svg'); ?>
+            <?php echo file_get_contents( get_stylesheet_directory_uri() . '/assets/img/decor.svg' ); ?>
         </div>
     <?php endif; ?>
-
+    
     <div class="container">
-        <div class="mx-auto relative z-0 max-w-[650px]">
+        <div class="relative z-0">
             <?php if ($header) : ?>
-                <div class="text-center font-bold text-2xl md:text-3xl lg:text-5xl mb-9 md:mb-14">
+                <h2 class="text-center font-bold text-2xl md:text-3xl lg:text-5xl mb-9 md:mb-14">
                     <?php echo esc_html($header); ?>
-                </div>
+                </h2>
             <?php endif; ?>
             <?php if ($description) : ?>
-                <div class="prose-smoothh prose md:prose-xl text-center mb-10"><?php echo $description; ?></div>
+                <div class="prose-smoothh prose md:prose-xl text-center mb-10" ><?php echo $description; ?></div>
             <?php endif; ?>
         </div>
     </div>
 
     <div class="relative z-0 w-full overflow-hidden !pb-5">
         <?php if ($tiles_list) : ?>
-            <div class="swiper !container !overflow-visible" data-js="swiper-tiles-default">
+            <div class="swiper !container !overflow-visible" data-js="swiper-tiles-mobile">
                 <div class="swiper-wrapper xl:!gap-y-20 xl:!gap-x-[90px] xl:!flex-wrap xl:justify-center xl:!transform-none">
-                    <?php foreach ($tiles_list as $tile) : ?>
-                        <div class="swiper-slide !h-auto !flex items-center flex-col xl:!basis-[calc(33%_-_56px)] drop-shadow-xl rounded-2xl">
-                            <?php if ($tile['image'] && $tile['image']['url']) : ?>
+                    <?php foreach($tiles_list as $tile) : ?>
+                        <div class="swiper-slide !h-auto !flex items-center flex-col xl:!basis-[calc(33%_-_56px)] border-2 border-[#EFEFEF] rounded-2xl">
+                            <?php if ($tile['image'] && $tile['image']['url'] ) : ?>
                                 <div class="w-full relative mb-5 rounded-t-[14px] overflow-hidden">
-                                    <?php echo smoothh_img_responsive($tile['image'], 'object-cover w-full !h-[190px] md:!h-[220px]', array(360, 220), 'lazy'); ?>
-                                    <div class="absolute inset-0 bg-gradient-to-b from-[#1F97D4] to-[#8117EE]"></div>
+                                    <?php echo smoothh_img_responsive($tile['image'],'object-cover w-full !h-[190px] md:!h-[220px]',array(360,220),'lazy'); ?>
+                                    <div class="absolute inset-0 bg-gradient-to-b from-primary/20 to-secondary/20"></div>
                                 </div>
                             <?php endif; ?>
                             <div class="text-center p-3 md:p-6 !pt-0">
@@ -51,12 +49,12 @@ $tiles_list = $args['tiles_list'];
                                     <p class="text-sm md:text-base"><?php echo $tile['description']; ?></p>
                                 <?php endif; ?>
                             </div>
-                            <?php if ($tile['button']) :
+                            <?php if( $tile['button'] ): 
                                 $btn_url = $tile['button']['url'];
                                 $btn_title = $tile['button']['title'];
                                 $btn_target = $tile['button']['target'] ? $tile['button']['target'] : '_self';
-                            ?>
-                                <a href="<?php echo esc_url($btn_url); ?>" target="<?php echo esc_attr($btn_target); ?>" class="translate-y-1/2 rounded-[14px] text-[13px] font-bold py-2 px-7 text-white bg-primary hover:bg-secondary transition duration-200"><?php echo esc_html($btn_title); ?></a>
+                                ?>
+                                <a href="<?php echo esc_url( $btn_url ); ?>" target="<?php echo esc_attr( $btn_target ); ?>" class="translate-y-1/2 rounded-[14px] text-[13px] font-bold py-2 px-7 text-white bg-primary hover:bg-secondary transition duration-200" ><?php echo esc_html( $btn_title ); ?></a>
                             <?php endif; ?>
                         </div>
                     <?php endforeach; ?>
