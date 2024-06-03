270adb32f2088e00698b1d808149200dc08c3dd4
Tomek <TK-54-4B>
2024-06-03 20:37:58 +0200

    section counter up new design

--- theme/flexible-content/sections/counters-bg.php
+++ theme/flexible-content/sections/counters-bg.php
@@ -14,9 +14,9 @@ $tiles_list = $args['tiles_list'];
             <div class="text-3xl md:text-5xl text-bold font-bold mb-9 md:mb-14 text-center"><?php echo $header; ?></div>
         <?php endif; ?>
         <?php if ($tiles_list) : ?>
-            <div class="py-20 px-36 grid grid-cols-1 gap-10 sm:grid-cols-2 lg:gap-x-[110px] lg:gap-y-[90px] xl:gap-10 2xl:gap-[140px] bg-white rounded-[45px] drop-shadow-2xl">
+            <div class="py-10 lg:py-20 px-8 lg:px-36 grid grid-cols-1 gap-10 sm:grid-cols-2 lg:gap-x-[110px] lg:gap-y-[90px] xl:gap-10 2xl:gap-[140px] bg-white rounded-[45px] drop-shadow-2xl">
                 <?php foreach ($tiles_list as $tile) : ?>
-                    <div class="flex flex-col items-center text-left">
+                    <div class="flex flex-col items-center text-left before:content-[''] before:w-1/2 before:bg-[#1F97D4] before:h-[2px]">
                         <div class="mb-5 md:mb-7 self-start">
                             <?php if ($tile['title']) : ?>
                                 <h3 class="text-2xl md:text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-[rgba(31,151,212,1)] to-[rgba(129,23,238,1)]"><?php echo $tile['title']; ?></h3>
