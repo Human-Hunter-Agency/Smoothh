1443091ef96995758028d3f42b80dcc3ff9ca674
Tomek <TK-54-4B>
2024-06-03 17:38:07 +0200

    [new design] section with grid decor

--- theme/flexible-content/sections/grid-decor-3x3.php
+++ theme/flexible-content/sections/grid-decor-3x3.php
@@ -8,28 +8,28 @@ $image_3x3 = $args['image_3x3'];
 ?>
 
 <div class="relative py-10 md:py-20">
-    <div class="bg-container w-[90%] bg-gradient-to-l from-[rgba(129,23,238,0)] to-[rgba(129,23,238,0.102)]">
-        <div class="container">
-            <div class="flex flex-col lg:flex-row gap-24">
-                <div class="z-0 w-1/2">
-                    <?php if ($image_3x3) : ?>
-                        <div class="decor-3x3 -mt-10">
-                            <?php echo smoothh_img_responsive($image_3x3, 'test', null, 'lazy') ?>
-                        </div>
-                    <?php else : ?>
-                        <div class="decor-3x3-default -m-10">
-                            <?php echo file_get_contents(get_stylesheet_directory_uri() . '/assets/img/gallery-3x3.png'); ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-                <div class="w-1/2 pt-48">
-                    <?php if ($content) : ?>
-                        <div class="prose-smoothh prose md:prose-xl prose-h3:text-2xl md:prose-h3:text-5xl lg:prose-h3:text-5xl prose-img:mt-0 prose-img:mx-auto prose-img:px-5 md:prose-p:leading-8">
-                            <?php echo $content; ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
+    <div class="bg-container w-[90%] h-full bg-gradient-to-l from-[rgba(129,23,238,0)] to-[rgba(129,23,238,0.102)]"></div>
+    <div class="container">
+        <div class="flex flex-col lg:flex-row gap-24">
+            <div class="z-0 w-1/2">
+                <?php if ($image_3x3) : ?>
+                    <div class="decor-3x3 -mt-14">
+                        <?php echo smoothh_img_responsive($image_3x3, 'test', null, 'lazy') ?>
+                    </div>
+                <?php else : ?>
+                    <div class="decor-3x3-default -m-10">
+                        <?php echo file_get_contents(get_stylesheet_directory_uri() . '/assets/img/gallery-3x3.png'); ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+            <div class="w-1/2 pt-64">
+                <?php if ($content) : ?>
+                    <div class="prose-smoothh prose md:prose-xl prose-h3:text-2xl md:prose-h3:text-5xl lg:prose-h3:text-5xl prose-img:mt-0 prose-img:mx-auto prose-img:px-5 md:prose-p:leading-8">
+                        <?php echo $content; ?>
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
     </div>
+
 </div>
