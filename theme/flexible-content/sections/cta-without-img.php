9c16b1134a5732718f7894e6745afaf4dec0005c
Tomek <TK-54-4B>
2024-06-03 21:59:00 +0200

    new cta design

--- theme/flexible-content/sections/cta-without-img.php
+++ theme/flexible-content/sections/cta-without-img.php
@@ -10,7 +10,7 @@ $button = $args['button'];
 <div class="container mx-auto">
     <div class="px-8 md:px-24 py-8 md:py-14 relative flex flex-col lg:flex-row items-center justify-between  drop-shadow-2xl rounded-3xl bg-gradient-to-b from-secondary to-primary">
         <?php if ($header) : ?>
-            <h3 class="mb-10 mlg:mb-0 text-3xl md:text-5xl text-bold text-white font-bold text-center"><?php echo esc_html($header); ?></h1>
+            <h3 class="mb-10 lg:mb-0 text-3xl md:text-5xl text-bold text-white font-bold text-center"><?php echo esc_html($header); ?></h1>
             <?php endif; ?>
 
             <?php if ($button) :
