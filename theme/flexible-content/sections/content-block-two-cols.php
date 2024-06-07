1324ae63769fd1aaabaa5797820665ea451209c6
Tomek <TK-54-4B>
2024-06-07 15:41:30 +0200

    fonts fix

--- theme/flexible-content/sections/content-block-two-cols.php
+++ theme/flexible-content/sections/content-block-two-cols.php
@@ -12,7 +12,7 @@ $decoration = $args['decoration'];
         <div class="flex flex-col lg:flex-row gap-10">
             <div class="z-0 w-full lg:w-1/2">
                 <?php if ($content) : ?>
-                    <div class="prose-smoothh prose md:prose-xl prose-h2:text-2xl md:prose-h2:text-5xl lg:prose-h2:text-[46px] lg:prose-h2:font-extrabold lg:prose-h2:leading-[55px] prose-p:text-[16px] prose-p:leading-[26px] prose-img:mt-0 prose-img:mx-auto prose-img:px-5">
+                    <div class="prose-smoothh prose md:prose-xl h2:text-2xl md:h2:text-5xl lg:h2:text-[46px] lg:h2:font-extrabold lg:h2:leading-[55px] p:text-[16px] prose-p:leading-[26px] prose-img:mt-0 prose-img:mx-auto prose-img:px-5">
                         <?php echo $content; ?>
                     </div>
                 <?php endif; ?>
