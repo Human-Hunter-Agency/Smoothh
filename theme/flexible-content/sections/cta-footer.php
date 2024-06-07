ab3013a3662cc7207161f6d8cfca083175d6d8bc
Tomek <TK-54-4B>
2024-06-07 17:06:39 +0200

    fonts fix

--- theme/flexible-content/sections/cta-footer.php
+++ theme/flexible-content/sections/cta-footer.php
@@ -8,7 +8,7 @@ $button = $args['button'];
 ?>
 
 <div class="container mx-auto relative">
-    <div class="h-[165px] mx-auto w-full translate-y-20 px-4 md:px-8 lg:px-24 py-8 md:py-14 flex flex-row gap-2 md:gap- items-center justify-between drop-shadow-2xl rounded-3xl bg-gradient-to-b from-secondary to-primary">
+    <div class="mx-auto w-full translate-y-20 px-4 md:px-8 lg:px-24 py-8 md:py-14 flex flex-row gap-2 md:gap- items-center justify-between drop-shadow-2xl rounded-3xl bg-gradient-to-b from-secondary to-primary">
         <?php if ($header) : ?>
             <h3 class="mb-0 text-xl sm:text-2xl md:text-3xl lg:text-5xl text-bold text-white font-bold text-center"><?php echo esc_html($header); ?></h1>
             <?php endif; ?>
