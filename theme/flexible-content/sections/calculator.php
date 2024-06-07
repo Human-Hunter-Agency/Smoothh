42ac8949805f09c13b2e2d6f35743d24c2d7cfbd
Kamil <k.medrala99@gmail.com>
2024-06-07 16:10:57 +0200

    calculator section display test

--- theme/flexible-content/sections/calculator.php
+++ theme/flexible-content/sections/calculator.php
@@ -5,15 +5,18 @@
 
 ?>
 
-<div class="relative py-10 md:py-[70px] bg-gradient-to-b from-secondary to-primary">
+<div class="relative py-10 md:py-[70px]">
     <div class="container">
         <?php if ($header) : ?>
             <div class="container mb-10 md:mb-[50px]">
-                <h2 class="text-center font-bold text-2xl md:text-3xl lg:text-5xl text-white">
+                <h2 class="text-center font-bold text-2xl md:text-3xl lg:text-5xl text-foreground">
                     <?php echo esc_html($header); ?>
                 </h2>
             </div>
         <?php endif; ?>
-            <?php echo do_shortcode('[product_page id="1186"]'); ?>
+            <?php 
+                $product_id=1186;
+                do_action("woocommerce_tm_epo",$product_id); 
+            ?>
         </div>
     </div>
