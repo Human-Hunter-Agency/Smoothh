e31721246d8685ef7cdc715abafb17e601c8898d
Tomek <TK-54-4B>
2024-06-07 17:51:44 +0200

    swiper test

--- theme/flexible-content/sections/steps-bg.php
+++ theme/flexible-content/sections/steps-bg.php
@@ -27,7 +27,7 @@ $steps_list = $args['steps_list'];
             <div class="swiper !container !overflow-visible" data-js="swiper-tiles-mobile">
                 <div class="swiper-wrapper">
                     <?php foreach ($steps_list as $step) : ?>
-                        <div class="swiper-slide !h-auto">
+                        <div class="swiper-slide !h-auto xl:flex-1">
                             <div class="flex flex-col items-center text-center p-3 md:p-6 pb-5 md:pb-10">
                                 <?php if ($step['icon'] && $step['icon']['url']) : ?>
                                     <?php echo smoothh_img_responsive($step['icon'], 'object-contain h-16 mb-8 md:mb-[50px]', array(130, 130), 'lazy'); ?>
