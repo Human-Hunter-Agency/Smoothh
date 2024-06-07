3d8ef252d136af882071cd62ad1b002e58a7ad97
Kamil <k.medrala99@gmail.com>
2024-06-07 09:00:57 +0200

    reviews basic styles test swiper

--- theme/woocommerce/single-product-reviews.php
+++ theme/woocommerce/single-product-reviews.php
@@ -130,7 +130,7 @@ if ( ! comments_open() ) {
 					</select></div>';
 				}
 
-				$comment_form['comment_field'] .= '<p class="comment-form-comment"><label for="comment" class="p-1 rounded-2xl border border-primary hover:border-secondary focus:border-secondary transition duration-200 ">' . esc_html__( 'Your review', 'woocommerce' ) . '&nbsp;<span class="required">*</span></label><textarea id="comment" name="comment" cols="45" rows="8" required></textarea></p>';
+				$comment_form['comment_field'] .= '<p class="comment-form-comment"><label for="comment">' . esc_html__( 'Your review', 'woocommerce' ) . '&nbsp;<span class="required">*</span></label><textarea id="comment" name="comment" cols="45" rows="8" class="p-1 rounded-2xl border border-primary hover:border-secondary focus:border-secondary transition duration-200  min-h-40 max-h-96" required></textarea></p>';
 
 				comment_form( apply_filters( 'woocommerce_product_review_comment_form_args', $comment_form ) );
 				?>
