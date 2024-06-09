cb134a449d4a1d294b1de4b352787c11daa215da
Tomek <TK-54-4B>
2024-06-09 20:03:39 +0200

    swiper tiles fix

--- theme/flexible-content/sections/tiles-icons.php
+++ theme/flexible-content/sections/tiles-icons.php
@@ -40,8 +40,8 @@ $button = $args['button'];
                         <div class="swiper-slide xl:flex-1 !flex items-center flex-col text-center">
                             <?php if ($tile['icon'] && $tile['icon']['url']) : ?>
                                 <div class="w-full
-                                    <?php if ($grid == 3) : ?> px-4 py-8  md:py-12 md:px-6 lg:py-[77px] mb-8 md:mb-14 border-2 border-[#EFEFEF] rounded-2xl [&_img]:h-32
-                                    <?php elseif ($grid == 5) : ?> p-4 md:p-6 mb-9 border-2 border-[#EFEFEF] rounded-2xl [&_img]:h-24
+                                    <?php if ($grid == 3) : ?> px-4 py-8  md:py-12 md:px-6 lg:py-[77px] mb-7 md:mb-14 border-2 border-[#EFEFEF] rounded-2xl [&_img]:h-32
+                                    <?php elseif ($grid == 5) : ?> p-4 md:p-6 mb-7 border-2 border-[#EFEFEF] rounded-2xl [&_img]:h-24
                                     <?php else : ?> p-4 md:p-6 mb-5  
                                     <?php endif; ?>">
                                     <?php echo smoothh_img_responsive($tile['icon'], 'mx-auto object-contain h-16', array(125, 125), 'lazy'); ?>
