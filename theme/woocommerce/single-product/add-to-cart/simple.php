06f6c973651d56397f9e1fe39c43518d9a3bcd28
Kamil <k.medrala99@gmail.com>
2024-06-10 14:24:39 +0200

    calculator basic prod quantity styles

--- theme/woocommerce/single-product/add-to-cart/simple.php
+++ theme/woocommerce/single-product/add-to-cart/simple.php
@@ -38,15 +38,23 @@ if ($product->is_in_stock() && $has_variable_price == false && !$product->is_dow
 		do_action('woocommerce_before_add_to_cart_quantity');
 
 		$is_hourly = get_field('product_hourly', $product->get_id());
-		woocommerce_quantity_input(
-			array(
-				'min_value'   => apply_filters('woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product),
-				'max_value'   => apply_filters('woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product),
-				'input_value' => isset($_POST['quantity']) ? wc_stock_amount(wp_unslash($_POST['quantity'])) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.,
-				'classes'	  => !isset($is_hourly) || !$is_hourly ? array('!hidden') : array()
-			)
-		);
+		?>
 
+		<div class="<?php if((isset($is_hourly) && $is_hourly) || $product->get_id()==1186){echo '';}else{echo 'hidden';}?>">
+			<?php
+				if (isset($is_hourly) && $is_hourly) {
+					woocommerce_quantity_input(
+						array(
+							'min_value'   => apply_filters('woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product),
+							'max_value'   => apply_filters('woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product),
+							'input_value' => isset($_POST['quantity']) ? wc_stock_amount(wp_unslash($_POST['quantity'])) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
+						)
+					);
+				}
+			?>
+		</div>
+
+		<?php
 		do_action('woocommerce_after_add_to_cart_quantity');
 		?>
 
