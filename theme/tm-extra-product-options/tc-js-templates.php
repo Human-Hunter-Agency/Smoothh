1f7064fdffe625a78e21cd1ecdc3d3da1621679e
Kamil <k.medrala99@gmail.com>
2024-06-11 11:16:25 +0200

    price display test

--- theme/tm-extra-product-options/tc-js-templates.php
+++ theme/tm-extra-product-options/tc-js-templates.php
@@ -45,7 +45,7 @@ if ( ! isset( $formatted_sale_price ) ) {
 </script>
 <script class="tm-hidden" type="text/template" id="tmpl-tc-final-totals">
 	<dl class="tm-extra-product-options-totals tm-custom-price-totals test">
-		<span><?php echo get_price_with_tax_string( data.product_total_price ); ?></span>
+		<span>{{{data.product_total_price}}}</span>
 		<# if (data.show_unit_price==true){ #><?php do_action( 'wc_epo_template_before_unit_price' ); ?>
 		<dt class="tm-unit-price">{{{ data.unit_price }}}</dt>
 		<dd class="tm-unit-price">
