fcecef48968a9bdd019f1bd4ee55bb800fc73f1c
Tomek <TK-54-4B>
2024-06-17 17:36:54 +0200

    tiles icon style

--- theme/flexible-content/sections/tiles-icons.php
+++ theme/flexible-content/sections/tiles-icons.php
@@ -32,7 +32,7 @@ $grid = $args['grid'];
         <?php if ($tiles_list) : ?>
             <div class="swiper !container !overflow-visible" data-js="swiper-tiles-icons">
                 <div class="swiper-wrapper  
-                    <?php if ($grid) : ?> xl:!transform-none xl:px-[20px] xl:box-border xl:flex-wrap xl:!gap-x-[10px] xl:!gap-y-20 xl:justify-center xl:[&_.swiper-slide]:basis-[calc(25%_-_50px)] 
+                    <?php if ($grid) : ?> xl:!transform-none xl:px-[20px] xl:box-border xl:flex-wrap xl:!gap-x-[10px] xl:!gap-y-0 xl:justify-center xl:[&_.swiper-slide]:basis-[calc(25%_-_50px)] 
                     <?php endif; ?>">
 
                     <?php foreach ($tiles_list as $tile) : ?>
