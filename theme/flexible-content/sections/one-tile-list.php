15b4f28687034696e9010190938326ae1c258fb4
Tomek <TK-54-4B>
2024-06-17 20:42:08 +0200

    one-tile-list block styles

--- theme/flexible-content/sections/one-tile-list.php
+++ theme/flexible-content/sections/one-tile-list.php
@@ -10,21 +10,22 @@ $tiles_list = $args['tiles_list'];
 <div class="relative w-full py-10 pb-5 md:pt-[70px] mt-10 mb-20">
     <div class="z-[-1] w-[100%] lg:w-[95%] h-full absolute top-8 left-0 bg-gradient-to-r to-[rgba(31,151,212,0.1)] from-[rgba(31,151,212,0)] rounded-[45px]"></div>
 
-    <div class="pt-14 relative z-0 container">
+    <div class="pt-14 relative z-0 container flex basis">
         <?php if ($header) : ?>
-            <div class="mb-10 xl:mb-20 mx-auto max-w-[900px] text-2xl md:text-4xl lg:text-[46px] font-bold lg:font-extrabold lg:leading-[55px]">
-                <?php echo $header; ?>
+            <div class="basis-2/3 mb-10 xl:mb-20 mx-auto text-2xl md:text-4xl lg:text-[46px] font-bold lg:font-extrabold lg:leading-[55px]">
+                <div class="max-w-[550px]">
+                    <?php echo $header; ?>
+                </div>
             </div>
         <?php endif; ?>
-        <?php if ($tiles_list) :
-            $tileID = 0;
-        ?>
-            <div class="py-10 lg:py-20 px-8 lg:px-20 xl:px-36 bg-white rounded-[45px] drop-shadow-2xl">
+
+        <?php if ($tiles_list) : ?>
+            <div class="basis-1/3 py-10 lg:py-16 px-8 lg:px-20 xl:px-16 bg-white rounded-[45px] drop-shadow-2xl">
                 <?php foreach ($tiles_list as $tile) :
                 ?>
                     <?php if ($tile['text']) : ?>
                         <div class="flex gap-2 items-start">
-                            <div class="w-4 h-4 bg-primary rounded-full"></div>
+                            <div class="w-[10px] h-[10px] bg-primary rounded-full"></div>
                             <p class="text-sm md:text-base"><?php echo $tile['text']; ?></p>
                         </div>
                     <?php endif; ?>
