c2b799fa88741b94bf367e62103a625bf4e9107b
Tomek <TK-54-4B>
2024-06-17 19:26:30 +0200

    two-tiles-pages styles

--- theme/flexible-content/sections/two-tiles-pages.php
+++ theme/flexible-content/sections/two-tiles-pages.php
@@ -20,11 +20,11 @@ $tiles_list = $args['tiles_list'];
                         <?php endif; ?>
 
                         <?php if ($tile['title']) : ?>
-                            <h3 class="max-w-[250px] text-center text-white text-base md:text-[20px] mb-9 font-semibold"><?php echo $tile['title']; ?></h3>
+                            <h3 class="max-w-[280px] text-center text-white text-base md:text-[20px] mb-9 font-semibold"><?php echo $tile['title']; ?></h3>
                         <?php endif; ?>
 
                         <?php if ($tile['description']) : ?>
-                            <p class="max-w-[250px] text-center text-sm md:text-base opacity-0 group-hover:!opacity-100 transition duration-200"><?php echo $tile['description']; ?></p>
+                            <div class="max-w-[280px] text-center text-sm md:text-base hidden group-hover:!block transition duration-200"><?php echo $tile['description']; ?></div>
                         <?php endif; ?>
 
                         <?php if ($tile['button']) :
