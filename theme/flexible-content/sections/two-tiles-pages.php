4443d8e091d95bac361b10a2a18355d2fa650074
Tomek <TK-54-4B>
2024-06-17 19:18:28 +0200

    two-tiles-pages styles

--- theme/flexible-content/sections/two-tiles-pages.php
+++ theme/flexible-content/sections/two-tiles-pages.php
@@ -11,23 +11,21 @@ $tiles_list = $args['tiles_list'];
         <?php if ($tiles_list) : ?>
             <div class="tiles-wrapper flex gap-10">
                 <?php foreach ($tiles_list as $tile) : ?>
-                    <div class="tile relative h-[400px] !flex items-center justify-center flex-col xl:!basis-1/2 drop-shadow-lg lg:drop-shadow-2xl rounded-2xl">
+                    <div class="tile group relative h-[400px] !flex items-center justify-center flex-col xl:!basis-1/2 drop-shadow-lg lg:drop-shadow-2xl rounded-2xl">
                         <?php if ($tile['image'] && $tile['image']['url']) : ?>
-                            <div class="w-full h-full mb-5 rounded-t-[14px] overflow-hidden absolute top-0 left-0 z-[-1]">
+                            <div class="w-full h-full rounded-[14px] overflow-hidden absolute top-0 left-0 z-[-1] group-hover:opacity-0 transition duration-200">
                                 <?php echo smoothh_img_responsive($tile['image'], 'object-cover w-full !h-full', array(360, 220), 'lazy'); ?>
-                                <div class="absolute inset-0 bg-gradient-to-b from-secondary to-primary mix-blend-multiply opacity-90"></div>
-
-
+                                <div class="absolute inset-0 bg-gradient-to-b from-secondary to-primary mix-blend-multiply opacity-90 group-hover:!opacity-0 transition duration-200"></div>
                             </div>
                         <?php endif; ?>
-                        <div class="text-center p-3 md:p-6 !pt-0">
-                            <?php if ($tile['title']) : ?>
-                                <h3 class="text-white text-base md:text-[20px] mb-9 font-semibold"><?php echo $tile['title']; ?></h3>
-                            <?php endif; ?>
-                            <?php if ($tile['description']) : ?>
-                                <p class="text-sm md:text-base"><?php echo $tile['description']; ?></p>
-                            <?php endif; ?>
-                        </div>
+
+                        <?php if ($tile['title']) : ?>
+                            <h3 class="text-white text-base md:text-[20px] mb-9 font-semibold"><?php echo $tile['title']; ?></h3>
+                        <?php endif; ?>
+
+                        <?php if ($tile['description']) : ?>
+                            <p class="text-sm md:text-base opacity-0 group-hover:!opacity-100 transition duration-200"><?php echo $tile['description']; ?></p>
+                        <?php endif; ?>
 
                         <?php if ($tile['button']) :
                             $btn_url = $tile['button']['url'];
