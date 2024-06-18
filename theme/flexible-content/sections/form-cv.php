40632db5423a6af3e040933678840969ecff01ba
Tomek <TK-54-4B>
2024-06-18 22:15:01 +0200

    cv form styles

--- theme/flexible-content/sections/form-cv.php
+++ theme/flexible-content/sections/form-cv.php
@@ -1,22 +1,25 @@
 <?php
+
 /** Template to display 'Formularz CV' - form_cv */
 
-    $header = $args['header'];
-    $description = $args['description'];
-    $hide_message_field = $args['hide_message_field'];
+$header = $args['header'];
+$description = $args['description'];
+$hide_message_field = $args['hide_message_field'];
 
 ?>
 
 <div class="w-full py-10 md:py-[70px] mb:pb-20 text-white bg-gradient-to-b from-secondary to-primary">
     <div class="container">
         <?php if ($header) : ?>
-            <h2 class="text-3xl md:text-5xl font-bold mb-9 text-center" ><?php echo esc_html($header); ?></h1>
-        <?php endif; ?>        
-        <?php if ($description) : ?>
-            <div class="prose-smoothh prose md:prose-xl text-white text-center mb-10 prose-h2:!text-white prose-h2:font-bold prose-h2:text-3xl prose-h2:md:text-5xl prose-h2:mb-9" ><?php echo $description; ?></div>
-        <?php endif; ?>
-        <div class="!max-w-[1410px] mx-auto <?php if ($hide_message_field) {echo '[&_.message-container]:hidden';} ?>">
-            <?php echo do_shortcode('[contact-form-7 id="841318a" title="CV"]'); ?>
-        </div>
+            <h2 class="text-3xl md:text-5xl font-semibold mb-9 text-center"><?php echo esc_html($header); ?></h1>
+            <?php endif; ?>
+            <?php if ($description) : ?>
+                <div class="mx-auto maxw-[900px] prose-smoothh prose md:prose-xl text-white text-center mb-10 prose-h2:!text-white prose-h2:font-bold prose-h2:text-3xl prose-h2:md:text-5xl prose-h2:mb-9"><?php echo $description; ?></div>
+            <?php endif; ?>
+            <div class="!max-w-[1410px] mx-auto <?php if ($hide_message_field) {
+                                                    echo '[&_.message-container]:hidden';
+                                                } ?>">
+                <?php echo do_shortcode('[contact-form-7 id="841318a" title="CV"]'); ?>
+            </div>
     </div>
 </div>
