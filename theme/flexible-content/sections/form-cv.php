97a91d65110d4189a6959f0e5244af20f66dfa43
Tomek <TK-54-4B>
2024-06-18 22:23:14 +0200

    cv form styles

--- theme/flexible-content/sections/form-cv.php
+++ theme/flexible-content/sections/form-cv.php
@@ -11,7 +11,7 @@ $hide_message_field = $args['hide_message_field'];
 <div class="w-full py-10 md:py-[70px] mb:pb-20 text-white bg-gradient-to-b from-secondary to-primary">
     <div class="container">
         <?php if ($header) : ?>
-            <h2 class="mx-auto maxw-[900px] text-3xl md:text-5xl font-semibold mb-9 text-center"><?php echo esc_html($header); ?></h2>
+            <h2 class="mx-auto max-w-[900px] text-3xl md:text-5xl font-semibold mb-9 text-center"><?php echo esc_html($header); ?></h2>
         <?php endif; ?>
         <?php if ($description) : ?>
             <div class="prose-smoothh prose md:prose-xl text-white text-center mb-10 prose-h2:!text-white prose-h2:font-bold prose-h2:text-3xl prose-h2:md:text-5xl prose-h2:mb-9"><?php echo $description; ?></div>
