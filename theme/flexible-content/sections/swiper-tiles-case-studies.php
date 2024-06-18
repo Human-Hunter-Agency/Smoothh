1ca39b604a64aaa61d11dd657eee6dd47d5d2ad9
Tomek <TK-54-4B>
2024-06-18 18:21:22 +0200

    new styles for case studies tile

--- theme/flexible-content/sections/swiper-tiles-case-studies.php
+++ theme/flexible-content/sections/swiper-tiles-case-studies.php
@@ -1,24 +1,25 @@
 <?php
+
 /** Template to display 'Swiper z kafelkami case studies' - swiper_tiles_case_studies */
 
-    $header = $args['header'];
-    $posts = $args['tiles_list'];
+$header = $args['header'];
+$posts = $args['tiles_list'];
 
 ?>
 
 <div class="relative py-10 md:py-[60px] mb:pb-[60px]">
-    
+
     <?php if ($header) : ?>
         <div class="container">
             <div class="relative z-0">
-                <h2 class="text-center font-bold text-2xl md:text-3xl lg:text-5xl mb-9 md:mb-14">
+                <div class="text-center font-bold text-2xl md:text-3xl lg:text-5xl mb-9 md:mb-14">
                     <?php echo esc_html($header); ?>
-                </h2>
+                </div>
             </div>
         </div>
-    <?php endif; 
-    
-    get_template_part( 'flexible-content/sections/partials/case-studies-swiper', '', array('posts' => $posts) );
+    <?php endif;
+
+    get_template_part('flexible-content/sections/partials/case-studies-swiper', '', array('posts' => $posts));
     ?>
-    
+
 </div>
