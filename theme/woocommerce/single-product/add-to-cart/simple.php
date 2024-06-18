d525b800c8a65270b972cbcf92d410e3af365f32
Kamil <k.medrala99@gmail.com>
2024-06-18 15:20:10 +0200

    negotiate btn styles fix

--- theme/woocommerce/single-product/add-to-cart/simple.php
+++ theme/woocommerce/single-product/add-to-cart/simple.php
@@ -112,7 +112,7 @@ if ($product->is_in_stock() && $has_variable_price == false && !$product->is_dow
 				</button>
 
 				<?php if($product->get_id()==1253):?>
-				<button data-js-popup-toggle="negotiate-form" type="button" value="<?php echo esc_attr($product->get_id()); ?>" class="!hidden whitespace-nowrap button border-none !bg-gradient-to-b from-primary via-secondary to-secondary bg-size-200 bg-pos-0 hover:bg-pos-100 focus:bg-pos-100  disabled:!bg-[#C9C9C9] [&.disabled]:!bg-[#C9C9C9] disabled:!bg-none [&.disabled]:!bg-none disabled:!opacity-100 [&.disabled]:!opacity-100  transition-all duration-200 text-white h-[55px] !px-5 xl:!px-12 xl:!pr-8 !rounded-[15px] font-bold flex items-center justify-center gap-5">
+				<button data-js-popup-toggle="negotiate-form" type="button" value="<?php echo esc_attr($product->get_id()); ?>" class="!hidden whitespace-nowrap border-none !bg-gradient-to-b from-primary via-secondary to-secondary bg-size-200 bg-pos-0 hover:bg-pos-100 focus:bg-pos-100  disabled:!bg-[#C9C9C9] [&.disabled]:!bg-[#C9C9C9] disabled:!bg-none [&.disabled]:!bg-none disabled:!opacity-100 [&.disabled]:!opacity-100  transition-all duration-200 text-white h-[55px] !px-5 xl:!px-12 xl:!pr-8 !rounded-[15px] font-bold flex items-center justify-center gap-5">
 					<?php echo esc_html_e('Negotiate price', 'smoothh'); ?>
 					<svg class="shrink-0 -rotate-90" width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<circle class="stroke-white" cx="9.5" cy="9.5" r="9"></circle>
