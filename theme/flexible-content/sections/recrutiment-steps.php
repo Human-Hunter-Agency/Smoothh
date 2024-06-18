a06fec26b8c951e1e904fd9ad76b0f3da8667b7f
Tomek <TK-54-4B>
2024-06-18 17:27:59 +0200

    recrutiment-steps block styles

--- theme/flexible-content/sections/recrutiment-steps.php
+++ theme/flexible-content/sections/recrutiment-steps.php
@@ -42,7 +42,7 @@ $list = $args['list'];
                                 <?php else : ?>
 
                                     <div class="m-8 bg-white flex items-center justify-center size-[72px] rounded-full border-[2px] border-primary z-[1] text-[32px] text-primary font-semibold">
-                                        <p class="<?php echo ($i == 3) ? 'mt-[-8px]' : 'mt-[-4px]'; ?>"><?php echo $i; ?></p>
+                                        <p class="<?php echo ($i == 3) ? 'mt-[-7px]' : 'mt-[-4px]'; ?>"><?php echo $i; ?></p>
                                     </div>
 
                                     <?php if ($i == 1) : ?>
