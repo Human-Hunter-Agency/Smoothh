8e6e917dff9a5daedd2fe4b56585e14e7e0cf400
Tomek <TK-54-4B>
2024-06-18 17:45:50 +0200

    recrutiment-steps block styles

--- theme/flexible-content/sections/tiles-pages.php
+++ theme/flexible-content/sections/tiles-pages.php
@@ -5,13 +5,17 @@
 $header = $args['header'];
 $description = $args['description'];
 $decoration = $args['decoration'];
+$IsBackgroundColor = $args['IsBackgroundColor'];
 $titles_primary = $args['titles_primary'];
 $tiles_list = $args['tiles_list'];
 
 ?>
 
 <div class="relative pb-10 pt-20 md:pt-[90px] md:pb-0 mb-20">
-    <div class="z-[-1] w-[100%] lg:w-[85%] h-full absolute top-8 right-0 bg-gradient-to-r from-[rgba(31,151,212,0.1)] to-[rgba(31,151,212,0)] rounded-[45px]"></div>
+    <?php if ($IsBackgroundColor) : ?>
+        <div class="z-[-1] w-[100%] lg:w-[85%] h-full absolute top-8 right-0 bg-gradient-to-r from-[rgba(31,151,212,0.1)] to-[rgba(31,151,212,0)] rounded-[45px]"></div>
+    <?php endif; ?>
+
     <?php if ($decoration) : ?>
         <div class="absolute right-7 top-7 -z-10 opacity-5 max-w-[90px] lg:max-w-[180px]">
             <?php echo file_get_contents(get_stylesheet_directory_uri() . '/assets/img/decor.svg'); ?>
