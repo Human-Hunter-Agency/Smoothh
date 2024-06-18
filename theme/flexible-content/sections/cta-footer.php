7975f39b7ddc385c39d613ef8178611fa7284fcf
Tomek <TK-54-4B>
2024-06-18 21:18:39 +0200

    cta footer fix

--- theme/flexible-content/sections/cta-footer.php
+++ theme/flexible-content/sections/cta-footer.php
@@ -4,11 +4,12 @@
 
 $header = $args['header'];
 $button = $args['button'];
+$isAboveFooter = $args['isAboveFooter'];
 
 ?>
 
 <div class="container mx-auto relative">
-    <div class="mx-auto w-full translate-y-1/2 px-4 md:px-8 lg:px-24 py-8 md:py-14 flex flex-col md:flex-row gap-2 items-center justify-between drop-shadow-2xl rounded-3xl bg-gradient-to-b from-secondary to-primary">
+    <div class="mx-auto w-full <?php echo $isAboveFooter == true ? 'translate-y-1/2' : 'translate-y-0'; ?> px-4 md:px-8 lg:px-24 py-8 md:py-14 flex flex-col md:flex-row gap-2 items-center justify-between drop-shadow-2xl rounded-3xl bg-gradient-to-b from-secondary to-primary">
         <?php if ($header) : ?>
             <h3 class="mb-0 text-xl sm:text-2xl md:text-3xl lg:text-5xl text-bold text-white font-bold text-center"><?php echo esc_html($header); ?></h1>
             <?php endif; ?>
