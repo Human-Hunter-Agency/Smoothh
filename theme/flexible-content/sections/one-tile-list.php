0083dae89b7beb2ba7bc7982c1f333c8505c5a7c
Tomek <TK-54-4B>
2024-06-19 20:49:37 +0200

    added description in tile fix

--- theme/flexible-content/sections/one-tile-list.php
+++ theme/flexible-content/sections/one-tile-list.php
@@ -43,7 +43,7 @@ $tiles_list = $args['tiles_list'];
             </div>
 
         <?php elseif ($description) :  ?>
-            <div class="">
+            <div class="basis-[45%] py-10 lg:py-10 px-8 lg:px-10 bg-white rounded-[45px] drop-shadow-2xl">
                 <p class="text-sm md:text-base"><?php echo $description; ?></p>
             </div>
         <?php endif; ?>
