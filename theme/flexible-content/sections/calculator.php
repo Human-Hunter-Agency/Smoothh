9951ef8083c0d03e7c43f8b8571be6c9fc7ce0cd
Tomek <TK-54-4B>
2024-06-19 21:31:50 +0200

    calc style

--- theme/flexible-content/sections/calculator.php
+++ theme/flexible-content/sections/calculator.php
@@ -1,11 +1,13 @@
 <?php
+
 /** Template to display 'Sekcja z listą ikon' - list_icons */
 
-    $header = $args['header'];
+$header = $args['header'];
 
 ?>
 
 <div class="relative py-10 md:py-[70px]">
+    <div class="z-[-1] w-[100%] lg:w-[95%] h-full absolute top-0 left-0 bg-gradient-to-r to-[rgba(31,151,212,0.1)] from-[rgba(31,151,212,0)] rounded-[45px]"></div>
     <div class="container calculator">
         <?php if ($header) : ?>
             <div class="mb-10 md:mb-[50px]">
@@ -16,20 +18,20 @@
         <?php endif; ?>
         <ul class="p-2 rounded-2xl bg-[#F2F2F2] flex items-center gap-2 max-w-screen-md w-fit flex-wrap mx-auto mb-5 md:mb-10">
             <li class="flex-1">
-                <button data-js-calc-tab-btn="1" class="active tab-btn"><?php echo __('Basic','smoothh') ?></button>
+                <button data-js-calc-tab-btn="1" class="active tab-btn"><?php echo __('Basic', 'smoothh') ?></button>
             </li>
             <li class="flex-1">
-                <button data-js-calc-tab-btn="2" class="tab-btn"><?php echo __('Advanced','smoothh') ?></button>
+                <button data-js-calc-tab-btn="2" class="tab-btn"><?php echo __('Advanced', 'smoothh') ?></button>
             </li>
         </ul>
         <div data-js-calc-content="1" class="">
-            <?php 
-                echo do_shortcode('[product_page id="1186"]');
+            <?php
+            echo do_shortcode('[product_page id="1186"]');
             ?>
         </div>
         <div data-js-calc-content="2" class="hidden">
-            <?php 
-                echo do_shortcode('[product_page id="1253"]');
+            <?php
+            echo do_shortcode('[product_page id="1253"]');
             ?>
         </div>
     </div>
