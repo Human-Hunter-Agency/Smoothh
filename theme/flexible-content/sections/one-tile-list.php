c4ef80704b1bd81feaff169ef818047f10ae07ee
Tomek <TK-54-4B>
2024-06-19 20:29:57 +0200

    left col fix

--- theme/flexible-content/sections/one-tile-list.php
+++ theme/flexible-content/sections/one-tile-list.php
@@ -3,6 +3,7 @@
 /** Template to display 'Sekcja z przesuniętą kafelką listy' - one_tile_list */
 
 $header = $args['header'];
+$description = $args['description'];
 $tiles_list = $args['tiles_list'];
 
 ?>
@@ -11,13 +12,21 @@ $tiles_list = $args['tiles_list'];
     <div class="z-[-1] w-[100%] lg:w-[75%] h-[70%] absolute top-8 left-0 bg-gradient-to-r to-[rgba(31,151,212,0.1)] from-[rgba(31,151,212,0)] rounded-[45px]"></div>
 
     <div class="pt-14 relative z-0 container flex flex-col lg:flex-row basis">
-        <?php if ($header) : ?>
-            <div class="basis-[55%] mb-10 xl:mb-20 mx-auto text-2xl md:text-4xl font-bold lg:font-extrabold lg:leading-[45px]">
-                <div class="max-w-[550px]">
+
+        <div class="basis-[55%] mb-10 xl:mb-20 mx-auto text-2xl md:text-4xl font-bold lg:font-extrabold lg:leading-[45px]">
+            <div class="max-w-[550px]">
+                <?php if ($header) : ?>
                     <?php echo $header; ?>
-                </div>
+                <?php endif; ?>
+
+                <?php if ($description) : ?>
+                    <div class="text-[16px] font-normal leading-[26px]">
+                        <?php echo $description; ?>
+                    </div>
+                <?php endif; ?>
             </div>
-        <?php endif; ?>
+        </div>
+
 
         <?php if ($tiles_list) : ?>
             <div class="basis-[45%] py-10 lg:py-10 px-8 lg:px-10 bg-white rounded-[45px] drop-shadow-2xl">
