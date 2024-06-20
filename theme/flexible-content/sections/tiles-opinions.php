6dcbf919ccc1951b2a3974af3cd0bcddd3980832
Tomek <TK-54-4B>
2024-06-20 21:22:03 +0200

    tiles-opinions styles

--- theme/flexible-content/sections/tiles-opinions.php
+++ theme/flexible-content/sections/tiles-opinions.php
@@ -30,7 +30,7 @@ $isSwiper = $args['isSwiper'];
         <?php if ($opinion_list) : ?>
             <div class="swiper !container !overflow-visible" data-js="swiper-tiles-default">
                 <div class="swiper-wrapper <?php if (!$isSwiper) :  ?> xl:!transform-none xl:!flex-wrap xl:justify-center xl:gap-y-12 <?php endif; ?>">
-                    <?php foreach ($opinion_list as $tile) : ?>
+                    <?php foreach ($opinion_list as $tile) : print_r($tile); ?>
                         <div class="swiper-slide !h-auto !flex items-center flex-col xl:!basis-[calc(33%_-_56px)] bg-white drop-shadow-lg lg:drop-shadow-2xl rounded-2xl">
                             <?php if ($tile['image'] && $tile['image']['url']) : ?>
                                 <div class="w-full relative mb-5 <?php if (!$isSwiper) :  ?> !mb-0  <?php endif; ?> rounded-t-[14px] overflow-hidden">
