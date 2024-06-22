2a30242c340c524b6a066cbd4809d5af6e78f43f
Tomek <TK-54-4B>
2024-06-22 19:10:21 +0200

    contact form styles

--- theme/flexible-content/sections/one-tile-list.php
+++ theme/flexible-content/sections/one-tile-list.php
@@ -24,7 +24,7 @@ $tiles_list = $args['tiles_list'];
 
     <div class="pt-14 relative z-0 container flex flex-col lg:flex-row basis">
 
-        <div class="basis-[55%] mb-10 xl:mb-20 mx-auto text-2xl md:text-4xl font-bold lg:font-extrabold lg:leading-[45px]">
+        <div class="basis-[55%] mb-10 xl:mb-20 mx-auto text-2xl md:text-[46px] font-bold lg:font-extrabold lg:leading-[45px]">
             <div class="max-w-[580px]">
                 <?php if ($header) : ?>
                     <?php echo $header; ?>
