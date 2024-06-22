c305c225518a2445a99919505b1982b581455e6c
Tomek <TK-54-4B>
2024-06-22 18:53:56 +0200

    contact form styles

--- theme/flexible-content/sections/form-contact.php
+++ theme/flexible-content/sections/form-contact.php
@@ -1,17 +1,53 @@
 <?php
+
 /** Template to display 'Formularz kontakt' - form_contact */
 
-    $header = $args['header'];
-    $header = $args['header'];
+$header = $args['header'];
+$description = $args['description'];
+$bgColor = $args['bgColor'];
 
 ?>
 
-<div class="container mt-[50px] mb-10">    
+
+<div class="relative w-full py-10 pb-5 md:pt-[70px] mt-10 mb-20">
+    <?php if ($bgColor == 'Różowy') : ?>
+        <div class="z-[-1] w-[100%] lg:w-[70%] h-[80%] absolute top-0 left-0 bg-gradient-to-r from-[rgba(129,23,238,0)] to-[rgba(129,23,238,0.102)] rounded-[45px]"></div>
+    <?php endif; ?>
+
+    <?php if ($bgColor == 'Niebieski') : ?>
+        <div class="z-[-1] w-[100%] lg:w-[70%] h-[80%] absolute top-0 left-0 bg-gradient-to-r to-[rgba(31,151,212,0.1)] from-[rgba(31,151,212,0)] rounded-[45px]"></div>
+    <?php endif; ?>
+
+
+    <div class="pt-14 relative z-0 container flex flex-col lg:flex-row basis">
+
+        <div class="basis-[55%] mb-10 xl:mb-20 mx-auto text-2xl md:text-4xl font-bold lg:font-extrabold lg:leading-[45px]">
+            <div class="max-w-[580px]">
+                <?php if ($header) : ?>
+                    <?php echo $header; ?>
+                <?php endif; ?>
+
+                <?php if ($description) : ?>
+                    <div class="mt-10 text-[16px] font-normal leading-[26px]">
+                        <?php echo $description; ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+        </div>
+
+        <div class="basis-[45%] py-10 lg:py-[53px] px-8 lg:px-[56px] bg-white rounded-[45px] drop-shadow-2xl text-sm md:text-base [&_.circled]:bg-primary  [&_.circled]:inline-flex [&_.circled]:items-center [&_.circled]:justify-center [&_.circled]:font-bold [&_.circled]:text-[30px] [&_.circled]:text-white [&_.circled]:size-20 [&_.circled]:rounded-xl [&_.circled]:my-4 [&_.circled]:whitespace-nowrap">
+            <?php echo do_shortcode('[contact-form-7 id="f3dc97a" title="Kontakt"]'); ?>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="container mt-[50px] mb-10">
     <?php if ($header) : ?>
-        <h3 class="text-3xl md:text-5xl text-bold font-bold mb-10 md:mb-14 text-center" ><?php echo esc_html($header); ?></h3>
-    <?php endif; ?>        
+        <h3 class="text-3xl md:text-5xl text-bold font-bold mb-10 md:mb-14 text-center"><?php echo esc_html($header); ?></h3>
+    <?php endif; ?>
     <div class="!max-w-[1040px] mx-auto">
         <?php echo do_shortcode('[contact-form-7 id="f3dc97a" title="Kontakt"]'); ?>
     </div>
-
 </div>
