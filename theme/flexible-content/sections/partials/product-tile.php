f9b76702e398d6c88a96aefe4b165e8b5ea8bec3
Tomek <TK-54-4B>
2024-06-22 15:47:02 +0200

    block swiper products styles

--- theme/flexible-content/sections/partials/product-tile.php
+++ theme/flexible-content/sections/partials/product-tile.php
@@ -17,14 +17,14 @@ if (isset($args['swiper']) && !empty($args['swiper'])) {
 
     <a href="<?php echo get_permalink($product->get_id()) ?>" class="!flex items-center flex-col border-2 border-[#EFEFEF] rounded-2xl group  <?php if ($is_swiper_slide) : ?> swiper-slide !h-auto opacity-0 !transition duration-500 [&.swiper-slide-visible]:opacity-100 <?php else : ?> h-full<?php endif ?>">
         <div class="relative overflow-hidden rounded-t-[14px] w-full !h-[190px] md:!h-[220px] [&_img]:object-cover [&_img]:w-full [&_img]:h-full">
-            <?php echo $product->get_image() ?>
             <div class="absolute inset-0 bg-gradient-to-b from-secondary to-primary mix-blend-multiply opacity-90"></div>
+            <h4 class="text-lg md:text-xl text-white font-semibold">
+                <?php echo get_the_title($product->get_id()) ?>
+            </h4>
         </div>
         <div class="w-full flex-1 p-3 md:p-6">
             <div class="flex flex-col gap-2 justify-between mb-5">
-                <h4 class="text-lg md:text-xl text-primary font-semibold">
-                    <?php echo get_the_title($product->get_id()) ?>
-                </h4>
+
                 <?php // if (is_user_logged_in() || is_prod_guest_available($product)) : 
                 ?>
                 <div class="flex text-lg md:text-xl flex-wrap shrink-0">
