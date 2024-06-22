515cf89e94832fe9b00dfc87ef8e031ef3836a04
Tomek <TK-54-4B>
2024-06-22 16:30:00 +0200

    block swiper products styles

--- theme/flexible-content/sections/partials/products-swiper-wrapper.php
+++ theme/flexible-content/sections/partials/products-swiper-wrapper.php
@@ -16,7 +16,7 @@ if (isset($args['products']) && !empty($args['products'])) {
                     <?php get_template_part('flexible-content/sections/partials/product-tile', '', array('product' => $product, 'swiper' => true)); ?>
                 <?php endforeach; ?>
             </div>
-            <div class="swiper-nav w-28  h-20 hidden lg:flex gap-5 !absolute !top-auto !bottom-[-100px] left-[50%] -translate-x-1/2 <?php if (!$isSwiper) :  ?> xl:hidden <?php endif; ?>">
+            <div class="swiper-nav w-28  h-20 hidden lg:flex gap-5 !absolute !top-auto !bottom-[-100px] left-[50%] -translate-x-1/2">
                 <div class="swiper-button-prev rotate-180 scale-150">
                     <svg xmlns="http://www.w3.org/2000/svg" width="39" height="40" viewBox="0 0 39 40" fill="none">
                         <path d="M19.6177 38.1958C9.57757 38.1958 1.43846 30.0567 1.43846 20.0166C1.43846 9.97651 9.57757 1.8374 19.6177 1.8374C29.6578 1.8374 37.7969 9.97651 37.7969 20.0166C37.7969 30.0567 29.6578 38.1958 19.6177 38.1958Z" stroke="#8117EE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
