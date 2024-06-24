5cc5d25b5c9cd2628d9e55b44949187c9f458b7a
Tomek <TK-54-4B>
2024-06-24 18:41:28 +0200

    styles

--- theme/flexible-content/sections/grid-decor-3x3.php
+++ theme/flexible-content/sections/grid-decor-3x3.php
@@ -11,7 +11,7 @@ $logo = $args['logo'];
 ?>
 
 <div class="relative py-10 md:py-20 mt-20">
-    <div class="z-[-1] lg:w-[85%] 2xl:w-[88%] lg:h-[1200px] absolute top-0 right-0 rounded-[45px] bg-gradient-to-l to-[rgba(129,23,238,0)] from-[rgba(129,23,238,0.102)]"></div>
+    <div class="z-[-1] lg:w-[85%] 2xl:w-[88%] lg:h-[1200px] absolute top-0 right-0 bg-gradient-to-l to-[rgba(129,23,238,0)] from-[rgba(129,23,238,0.102)]"></div>
     <div class="container">
         <div class=" mb-20 lg:mb-44 flex flex-col lg:flex-row gap-10 lg:gap-24">
             <div class="z-0 w-full lg:w-1/2">
