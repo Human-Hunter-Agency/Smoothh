fae564feb361e8d883a8615f5d8dd141d41d73c3
Tomek <TK-54-4B>
2024-06-24 17:31:05 +0200

    footer styles

--- theme/template-parts/layout/footer-content.php
+++ theme/template-parts/layout/footer-content.php
@@ -135,7 +135,7 @@ $social_links = get_field('social_links', 'option');
 	</div>
 
 	<div class="">
-		<div class="container relative flex flex-col md:flex-row md:items-center justify-between gap-5 py-5 md:py-10 text-[#A7A7A7] text-[14px] before:content-[''] before:w-full before:h-[1px] before:bg-[#A7A7A7] before:absolute before:top-0 before:left-0 _border _border-t-[1px] _border-t-[#A7A7A7]">
+		<div class="container relative flex flex-col md:flex-row md:items-center justify-between gap-5 py-5 md:py-10 text-[#A7A7A7] text-[14px] before:content-[''] before:w-full before:h-[1px] before:bg-[#A7A7A7] before:absolute before:top-0 before:left-0 before:container _border _border-t-[1px] _border-t-[#A7A7A7]">
 			<?php
 			$smoothh_blog_info = get_bloginfo('name');
 			if (!empty($smoothh_blog_info)) :
