1fb656a685fb6edefa2029f5d724d9d645c9ba22
Kamil <k.medrala99@gmail.com>
2024-06-25 16:19:03 +0200

    DEPOSIT_AMOUNT_MIN test

--- theme/functions.php
+++ theme/functions.php
@@ -1134,3 +1134,32 @@ function tm_epo_js_loader(){
 	do_action( 'woocommerce_tm_epo_enqueue_scripts');
 }
 add_action( 'wp_enqueue_scripts', 'tm_epo_js_loader' );
+
+
+add_filter( 'awcdp_product_deposit_amount', 'awcdp_product_deposit_amount', 10, 2 );
+
+
+function awcdp_product_deposit_amount($amount, $product_id) {
+	$DEPOSIT_AMOUNT_MIN = 990;
+    $product = wc_get_product($product_id);
+    $price = $product->get_price();
+	$deposit_temp = $price * 0.1; 
+    If($deposit_temp > $DEPOSIT_AMOUNT_MIN ){
+        return 10;
+    } else {
+        return $DEPOSIT_AMOUNT_MIN;
+    }
+}
+
+add_filter( 'awcdp_product_deposit_type', 'awcdp_product_deposit_type', 10, 2 );
+function awcdp_product_deposit_type($type,$product_id) { 
+	$DEPOSIT_AMOUNT_MIN = 990;
+    $product = wc_get_product($product_id);
+    $price = $product->get_price();
+	$deposit_temp = $price * 0.1; 
+    If($deposit_temp > $DEPOSIT_AMOUNT_MIN ){
+        return 'percent';
+    } else {
+        return 'fixed';
+    }    
+}
--- theme/woocommerce/checkout/review-order.php
+++ theme/woocommerce/checkout/review-order.php
@@ -42,7 +42,7 @@ defined('ABSPATH') || exit;
             <?php echo wc_get_formatted_cart_item_data($cart_item); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
             ?>
           </td>
-          <td class="product-total !border-none">
+          <td class="product-total !border-none [&_small]:!hidden">
             <?php echo apply_filters('woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal($_product, $cart_item['quantity']), $cart_item, $cart_item_key); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
             ?>
           </td>
