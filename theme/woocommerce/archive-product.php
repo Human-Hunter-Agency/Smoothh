bc6c3e919efbede327c5174d2915f5099829fd7c
Tomek <TK-54-4B>
2024-06-26 19:38:03 +0200

    disabled woo hooks

--- theme/woocommerce/archive-product.php
+++ theme/woocommerce/archive-product.php
@@ -71,8 +71,8 @@ do_action('woocommerce_before_main_content');
         /**
          * Hook: woocommerce_before_shop_loop.
          *
-         * @hooked woocommerce_output_all_notices - 10
-         * @hooked woocommerce_result_count - 20
+         * _@hooked woocommerce_output_all_notices - 10
+         * _@hooked woocommerce_result_count - 20
          * @hooked woocommerce_catalog_ordering - 30
          */
         do_action('woocommerce_before_shop_loop');
