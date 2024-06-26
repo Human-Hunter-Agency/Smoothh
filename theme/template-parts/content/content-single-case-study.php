991449ece0cb543b161ba70ac9c9346c2a5df676
Tomek <TK-54-4B>
2024-06-26 18:10:02 +0200

    case study cta

--- theme/template-parts/content/content-single-case-study.php
+++ theme/template-parts/content/content-single-case-study.php
@@ -120,26 +120,23 @@ $logoHeader = get_field('header_clients_logos');
 		$header = $cta['header'];
 		$button = $cta['button'];
 	?>
-		<div class="relative w-full flex flex-col items-center justify-center py-10 md:py-[70px]">
-			<?php if (isset($bg_url)) : ?>
-				<?php echo smoothh_img_responsive($background, 'absolute inset-0 -z-20 object-cover h-full w-full', null, 'lazy'); ?>
+		<div class="mx-auto w-full translate-y-1/2 px-4 md:px-8 lg:px-24 py-8 md:py-14 flex flex-col md:flex-row gap-2 items-center justify-between drop-shadow-2xl rounded-3xl bg-gradient-to-b from-secondary to-primary">
+			<?php if ($header) : ?>
+				<div>
+					<h3 class="mb-0 text-xl sm:text-2xl md:text-3xl lg:text-5xl text-bold text-white font-bold text-left"><?php echo esc_html($header); ?></h3>
+					<?php if ($description) : ?>
+						<p class="mt-1 text-[16px] text-white font-normal text-left"><?php echo esc_html($description); ?></p>
+					<?php endif; ?>
+				</div>
 			<?php endif; ?>
 
-			<div class="absolute inset-0 -z-10 bg-gradient-to-b from-primary/60 to-secondary/70"></div>
-
-			<div class="relative z-0 flex flex-col items-center justify-center container">
-				<?php if (isset($header)) : ?>
-					<h3 class="text-3xl md:text-5xl text-bold text-white font-bold mb-9"><?php echo esc_html($header); ?></h1>
-					<?php endif; ?>
-
-					<?php if (isset($button)) :
-						$btn_url = $button['url'];
-						$btn_title = $button['title'];
-						$btn_target = $button['target'] ? $button['target'] : '_self';
-					?>
-						<a href="<?php echo esc_url($btn_url); ?>" target="<?php echo esc_attr($btn_target); ?>" class="rounded-[14px] text-[13px] font-bold py-2 px-7 md:px-[70px] border-2 border-white text-white bg-transparent hover:bg-white/20 transition duration-200"><?php echo esc_html($btn_title); ?></a>
-					<?php endif; ?>
-			</div>
+			<?php if ($button) :
+				$btn_url = $button['url'];
+				$btn_title = $button['title'];
+				$btn_target = $button['target'] ? $button['target'] : '_self';
+			?>
+				<a href="<?php echo esc_url($btn_url); ?>" target="<?php echo esc_attr($btn_target); ?>" class="rounded-3xl text-lg md:text-xl font-semibold md:font-bold py-2 px-5 md:px-12 border-[1px] border-white text-white bg-transparent hover:bg-white/20 transition duration-200"><?php echo esc_html($btn_title); ?></a>
+			<?php endif; ?>
 
 		</div>
 	<?php endif; ?>
