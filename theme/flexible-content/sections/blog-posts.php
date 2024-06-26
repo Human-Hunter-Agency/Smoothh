386e6a7af10eacd34eea4a519d37061290177146
Kamil <k.medrala99@gmail.com>
2024-06-26 16:43:31 +0200

    blog posts header fix

--- theme/flexible-content/sections/blog-posts.php
+++ theme/flexible-content/sections/blog-posts.php
@@ -1,10 +1,5 @@
 <section id="posts-related" class="container mt-10 mb-20 md:my-20">
-	<?php
-		$header = $args['header'];
-	 	if($header) :
-	 ?>
-		<div class="mx-auto max-w-[1040px] mb-10 lg:mb-7 !text-2xl md:!text-4xl lg:!text-[46px] prose-strong:text-primary font-bold lg:!font-extrabold lg:leading-[55px] text-center"><?php echo $header ?></div>
-	<?php endif; ?>
+	<h2 class="mx-auto max-w-[1040px] mb-10 lg:mb-7 !text-2xl md:!text-4xl lg:!text-[46px] prose-strong:text-primary font-bold lg:!font-extrabold lg:leading-[55px] text-center"><?php echo _('See expert materials from <strong>our blog</strong>','smoothh') ?></h2>
 	<ul class="p-2 rounded-2xl flex items-center gap-2 max-w-screen-md w-fit flex-wrap mx-auto mb-5 md:mb-10">
 		<?php
 		$categories = get_categories();
