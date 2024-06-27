eda3561a5c3c88c294ea6bb7ac3d2d1b1ae90b81
Kamil <k.medrala99@gmail.com>
2024-06-27 16:08:59 +0200

    remove page items count hooks

--- theme/functions.php
+++ theme/functions.php
@@ -264,6 +264,9 @@ remove_action('woocommerce_single_product_summary', 'woocommerce_template_single
 remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0);
 remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10, 0);
 
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
+
 add_action('woocommerce_after_single_product_summary','comments_template',10);
 
 add_action('wp_enqueue_scripts', 'smoothh_disable_woocommerce_cart_fragments', 200);
--- theme/woocommerce/archive-product.php
+++ theme/woocommerce/archive-product.php
@@ -72,8 +72,8 @@ do_action('woocommerce_before_main_content');
          * Hook: woocommerce_before_shop_loop.
          *
          * @hooked woocommerce_output_all_notices - 10
-         * @hooked woocommerce_result_count - 20
-         * @hooked woocommerce_catalog_ordering - 30
+         * @hooked woocommerce_result_count - 20 -removed
+         * @hooked woocommerce_catalog_ordering - 30 -removed
          */
         do_action('woocommerce_before_shop_loop');
 
