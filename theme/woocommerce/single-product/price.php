dbe605a8e2e8c6e923060706eb16b758a6fe068a
Tomek <TK-54-4B>
2024-06-27 18:40:58 +0200

    single product page styles

--- theme/woocommerce/single-product/price.php
+++ theme/woocommerce/single-product/price.php
@@ -27,8 +27,18 @@ $is_hourly = get_field('product_hourly');
 ?>
 <?php if (!$product->is_type('variable')) : // if (is_user_logged_in() || is_prod_guest_available($product)) : 
 ?>
-    <div class="<?php echo esc_attr(apply_filters('woocommerce_product_price_class', 'price')); ?> pb-5 flex flex-col gap-1 border border-b-[1px] border-[#D6D6D6]">
-        <p class="lex items-end shrink-0 [&_.woocommerce-Price-currencySymbol]:hidden [&_bdi]:text-[26px] [&_del_bdi]:!text-xl [&_bdi]:!font-bold [&_ins]:no-underline [&_del_bdi]:!text-foreground [&_del]:h-8 [&_del]:!decoration-foreground [&_del_bdi]:mr-1.5">
+    <div class="<?php echo esc_attr(apply_filters('woocommerce_product_price_class', 'price')); ?> pb-5 flex flex-col gap-1 border-b-[1px] border-[#D6D6D6]">
+        <?php
+        woocommerce_quantity_input(
+            array(
+                'min_value'   => apply_filters('woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product),
+                'max_value'   => apply_filters('woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product),
+                'input_value' => isset($_POST['quantity']) ? wc_stock_amount(wp_unslash($_POST['quantity'])) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
+            )
+        );
+        ?>
+
+        <p class="flex items-end shrink-0 [&_.woocommerce-Price-currencySymbol]:hidden [&_bdi]:text-[26px] [&_del_bdi]:!text-xl [&_bdi]:!font-bold [&_ins]:no-underline [&_del_bdi]:!text-foreground [&_del]:h-8 [&_del]:!decoration-foreground [&_del_bdi]:mr-1.5">
             <?php if (isset($has_variable_price) && $has_variable_price) : ?>
                 <span class="text-foreground font-normal text-xl md:text-2xl mr-2"><?php esc_html_e('From', 'smoothh') ?></span>
             <?php endif ?>
@@ -37,6 +47,6 @@ $is_hourly = get_field('product_hourly');
                                                                                                                                                                                     echo '/h';
                                                                                                                                                                                 } ?></span>
         </p>
-        <span class="text-foreground text-lg md:text-base text-right"><?php echo get_product_tax_formatted($product); ?></span>
+        <span class="text-[#A7A7A7] text-lg md:text-base text-right"><?php echo get_product_tax_formatted($product); ?></span>
     </div>
 <?php endif; ?>
