2b56e8dbd9e9a9409e63b20f423888e97eafb5d8
Tomek <TK-54-4B>
2024-06-27 21:27:40 +0200

    variation add to cart style

--- theme/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ theme/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Single variation cart button
  *
@@ -7,44 +8,46 @@
  * @version 7.0.1
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 global $product;
 
 $is_hourly = get_field('product_hourly', $product->get_id());
 ?>
-<div class="woocommerce-variation-add-to-cart variations_button flex items-center">
-	<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+<div class="woocommerce-variation-add-to-cart variations_button !mt-8 flex items-center">
+	<?php do_action('woocommerce_before_add_to_cart_button'); ?>
 
 	<?php
-	do_action( 'woocommerce_before_add_to_cart_quantity' );
+	do_action('woocommerce_before_add_to_cart_quantity');
 
-	if(isset($is_hourly) && $is_hourly){
+	if (isset($is_hourly) && $is_hourly) {
 		woocommerce_quantity_input(
 			array(
-				'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
-				'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
-				'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
+				'min_value'   => apply_filters('woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product),
+				'max_value'   => apply_filters('woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product),
+				'input_value' => isset($_POST['quantity']) ? wc_stock_amount(wp_unslash($_POST['quantity'])) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
 			)
 		);
 	}
 
-	do_action( 'woocommerce_after_add_to_cart_quantity' );
+	do_action('woocommerce_after_add_to_cart_quantity');
 	?>
 
-	<?php // if (is_user_logged_in() || is_prod_guest_available($product)) : ?>
-		<button type="submit" class="single_add_to_cart_button !float-right whitespace-nowrap button border-none !bg-gradient-to-b from-primary via-secondary to-secondary bg-size-200 bg-pos-0 hover:bg-pos-100 focus:bg-pos-100  disabled:!bg-[#C9C9C9] [&.disabled]:!bg-[#C9C9C9] disabled:!bg-none [&.disabled]:!bg-none disabled:!opacity-100 [&.disabled]:!opacity-100  transition-all duration-200 text-white h-[55px] !px-5 xl:!px-12 xl:!pr-8 !rounded-[15px] font-bold !flex items-center justify-center gap-5 alt<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>"><?php echo esc_html( $product->single_add_to_cart_text() ); ?>
-			<svg class="shrink-0 -rotate-90" width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<circle class="stroke-white" cx="9.5" cy="9.5" r="9"></circle>
-				<path class="fill-white" d="M9 12.986L5.75 7.5H7.7L9.468 10.451L11.314 7.5H13.16L9.845 12.986H9Z"></path>
-			</svg>
-		</button>
-	<?php //endif; ?>
+	<?php // if (is_user_logged_in() || is_prod_guest_available($product)) : 
+	?>
+	<button type="submit" class="single_add_to_cart_button !float-right whitespace-nowrap button border-none !bg-gradient-to-b from-primary via-secondary to-secondary bg-size-200 bg-pos-0 hover:bg-pos-100 focus:bg-pos-100  disabled:!bg-[#C9C9C9] [&.disabled]:!bg-[#C9C9C9] disabled:!bg-none [&.disabled]:!bg-none disabled:!opacity-100 [&.disabled]:!opacity-100  transition-all duration-200 text-white h-[55px] !px-5 xl:!px-12 xl:!pr-8 !rounded-[15px] font-bold !flex items-center justify-center gap-5 alt<?php echo esc_attr(wc_wp_theme_get_element_class_name('button') ? ' ' . wc_wp_theme_get_element_class_name('button') : ''); ?>"><?php echo esc_html($product->single_add_to_cart_text()); ?>
+		<svg class="shrink-0 -rotate-90" width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<circle class="stroke-white" cx="9.5" cy="9.5" r="9"></circle>
+			<path class="fill-white" d="M9 12.986L5.75 7.5H7.7L9.468 10.451L11.314 7.5H13.16L9.845 12.986H9Z"></path>
+		</svg>
+	</button>
+	<?php //endif; 
+	?>
 
 
-	<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+	<?php do_action('woocommerce_after_add_to_cart_button'); ?>
 
-	<input type="hidden" name="add-to-cart" value="<?php echo absint( $product->get_id() ); ?>" />
-	<input type="hidden" name="product_id" value="<?php echo absint( $product->get_id() ); ?>" />
+	<input type="hidden" name="add-to-cart" value="<?php echo absint($product->get_id()); ?>" />
+	<input type="hidden" name="product_id" value="<?php echo absint($product->get_id()); ?>" />
 	<input type="hidden" name="variation_id" class="variation_id" value="0" />
 </div>
