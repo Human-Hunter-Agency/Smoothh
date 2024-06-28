a5b8a7d958c17da5781df88c6017476bad800838
Kamil <k.medrala99@gmail.com>
2024-06-28 09:34:52 +0200

    [cart] swiper products selected list section fix

--- theme/flexible-content/sections/list-selected-products.php
+++ theme/flexible-content/sections/list-selected-products.php
@@ -12,7 +12,7 @@ if (isset($args['products_list']) && !empty($args['products_list'])) {
     }
 }
 ?>
-<?php if ($isset($products) && !empty($products)) : ?>
+<?php if (isset($products) && !empty($products)) : ?>
 <section class="relative py-10 mb:py-[60px] md:pt-[70px] md:!pb-[70px]">
     <div class="container">
         <?php if ($header) : ?>
