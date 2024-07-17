4cc1d3f67d0064cc791718799f47ac9e4a2f656f
Kamil <k.medrala99@gmail.com>
2024-07-17 15:54:39 +0200

    [SMOOTH-61] quote form fields wrap, order change

--- theme/atcf7/quote.php
+++ theme/atcf7/quote.php
@@ -9,20 +9,21 @@ if (!defined('ABSPATH')) {
 
 <div class="form-basic">
   <div class="mb-[18px] flex flex-col lg:flex-row lg:flex-wrap gap-[18px] lg:[&_p]:grow lg:[&_p]:w-[calc(33%_-_12px)] ">
-      [text* your-job placeholder "Stanowisko*"]
 
-      [email* your-email placeholder "Adres e-mail*"]
+    [email* your-email placeholder "Adres e-mail*"]
 
-      [tel* your-phone placeholder "Numer tel.*"]
+    [tel* your-phone placeholder "Numer tel.*"]
 
-      [text* your-location placeholder "Lokalizacja zatrudnienia*"]
+    [text* your-location placeholder "Lokalizacja zatrudnienia*"]
+    
+    [text* your-job placeholder "Stanowisko*"]
 
-      [text your-company placeholder "Firma"]
+    [text your-company placeholder "Firma"]
 
-      [hidden prod-id default:shortcode_attr]
+    [hidden prod-id default:shortcode_attr]
 
-      [hidden prod-name default:shortcode_attr]
-      
+    [hidden prod-name default:shortcode_attr]
+
   </div>
 
   <div class="w-full mb-5">
@@ -30,14 +31,14 @@ if (!defined('ABSPATH')) {
   </div>
   <div class="max-w-[540px]">
     <div class="mb-5 has-tooltip">
-    [acceptance gdpr_woo_consent] <?php
-      $policyPageUrl = get_permalink(wc_privacy_policy_page_id()); 
-      echo sprintf(
-        __('I accept the %sPrivacy Policy%s', 'gdpr-framework'),
-        "<a href='{$policyPageUrl}' target='_blank'>",
-        "</a>"
-      ); 
-      ?> 
+      [acceptance gdpr_woo_consent] <?php
+                                    $policyPageUrl = get_permalink(wc_privacy_policy_page_id());
+                                    echo sprintf(
+                                      __('I accept the %sPrivacy Policy%s', 'gdpr-framework'),
+                                      "<a href='{$policyPageUrl}' target='_blank'>",
+                                      "</a>"
+                                    );
+                                    ?>
       <div class="tooltip">
         <div class="tooltip-icon">i</div>
         <span class="tooltip-text">
@@ -51,8 +52,8 @@ if (!defined('ABSPATH')) {
   <div class="btn-color relative text-center max-w-full w-fit mx-auto [&_.wpcf7-spinner]:absolute [&_.wpcf7-spinner]:right-[-7px] [&_.wpcf7-spinner]:top-4 mb-9 ">
     [submit "Wyślij wiadomość"]
     <svg class="absolute right-5 top-[18px] -rotate-90" width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <circle class="stroke-white" cx="9.5" cy="9.5" r="9"/>
-      <path class="fill-white" d="M9 12.986L5.75 7.5H7.7L9.468 10.451L11.314 7.5H13.16L9.845 12.986H9Z"/>
+      <circle class="stroke-white" cx="9.5" cy="9.5" r="9" />
+      <path class="fill-white" d="M9 12.986L5.75 7.5H7.7L9.468 10.451L11.314 7.5H13.16L9.845 12.986H9Z" />
     </svg>
   </div>
 </div>
