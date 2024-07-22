775f3ae8b4b8c4717d66da8d6cb97eec8572b4bf
Kamil <k.medrala99@gmail.com>
2024-07-22 10:38:35 +0200

    [SMOOTH-73] content box section with shadow

--- theme/flexible-content/sections/content-block-shadow.php
+++ theme/flexible-content/sections/content-block-shadow.php
@@ -9,16 +9,16 @@ $logo = $args['logo'];
 
 <div class="relative py-10 mt-10">
     <div class="container">
-        <?php if ($title2) : ?>
+        <?php if ($header) : ?>
             <div class="mb-12 text-2xl md:text-4xl lg:text-[46px] font-bold lg:font-extrabold lg:leading-[55px]">
-                <?php echo $title2; ?>
+                <?php echo $header; ?>
             </div>
         <?php endif; ?>
 
-        <?php if ($description2) : ?>
+        <?php if ($description) : ?>
             <div class="lg:py-12 p-8 lg:px-28 bg-white rounded-[45px] drop-shadow-2xl">
                 <div class="mb-20 text-[16px] font-normal leading-[26px]">
-                    <?php echo $description2; ?>
+                    <?php echo $description; ?>
                 </div>
                 <?php if ($logo) : ?>
                     <div class="w-full flex items-center justify-center">
