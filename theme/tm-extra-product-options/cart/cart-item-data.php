74b71fbdc7ebaf6fb34964f56fe838c3d5ef8415
Kamil <k.medrala99@gmail.com>
2024-07-23 13:10:37 +0200

    [SMOOTH-84] cart calculator details dropdown button

--- theme/tm-extra-product-options/cart/cart-item-data.php
+++ theme/tm-extra-product-options/cart/cart-item-data.php
@@ -24,53 +24,55 @@ defined('ABSPATH') || exit;
 $separator = THEMECOMPLETE_EPO_DATA_STORE()->get('tm_epo_separator_cart_text');
 ?>
 <div data-js="dropdown">
-    <button data-js="dropdown-toggle" class="ml-auto first:mb-2 w-fit whitespace-nowrap border-2 border-[#F2F2F2] hover:border-primary hover:text-primary transition duration-200 text-black min-h-[55px] px-5 rounded-[15px] font-semibold flex items-center justify-center gap-5">
+    <button type="button" data-js="dropdown-toggle" class="text-sm md:text-sm mt-3 mb-2 w-fit border-2 border-[#F2F2F2] hover:border-primary hover:text-primary transition duration-200 text-black py-3 px-5 rounded-[15px] font-semibold flex items-center justify-center gap-5">
         <?= __('Show configuration', 'smoothh');  ?>
     </button>
-    <dl class="tc-epo-metadata variation text-sm p-3 overflow-hidden h-0 transition-all duration-300" data-js="dropdown-container">
-        <?php
-        if (isset($item_data)) :
-            foreach ($item_data as $data) :
-                $is_epo      = false;
-                $show_dt     = true;
-                $show_dd     = true;
-                $class_name  = '';
-                $class_value = '';
-                if (isset($data['tm_label'])) {
-                    $is_epo      = true;
-                    $class_name  = 'tc-name ';
-                    $class_value = 'tc-value ';
-                }
-    
-                if (!isset($data['display']) && isset($data['value'])) {
-                    $data['display'] = $data['value'];
-                }
-                if ($is_epo && '' === $data['key']) {
-                    $show_dt = false;
-                }
-                if ($is_epo && '' === $data['display']) {
-                    $show_dd = false;
-                }
-                if (('link' === THEMECOMPLETE_EPO_DATA_STORE()->get('tm_epo_cart_field_display') && isset($data['popuplink'])) || !$show_dd) {
-                    $separator = '';
-                }
-    
-                // $data['key'] and $data['display'] contians HTML code
-                // thus the use of wp_kses_post and not esc_html
-        ?>
-                <?php if ($show_dt) : ?>
-                    <dt class="<?php echo esc_attr(sanitize_html_class($class_name)); ?> !mr-3 font-semibold variation-<?php echo esc_attr(sanitize_html_class($data['key'])); ?>"><?php echo apply_filters('wc_epo_kses', wp_kses_post($data['key']), $data['key'], false); // phpcs:ignore WordPress.Security.EscapeOutput 
-                                                                                                                                                                        ?><?php echo esc_html($separator); ?></dt>
-                <?php else : ?>
-                    <dt class="<?php echo esc_attr(sanitize_html_class($class_name)); ?> tc-hidden-variation">&nbsp;</dt>
-                <?php endif; ?>
-                <?php if ($show_dd) : ?>
-                    <dd class="<?php echo esc_attr(sanitize_html_class($class_value)); ?>!mb-2 variation-<?php echo esc_attr(sanitize_html_class($data['key'])); ?>"><?php echo apply_filters('wc_epo_kses', wp_kses_post(wpautop($data['display'])), wpautop($data['display']), false); // phpcs:ignore WordPress.Security.EscapeOutput 
-                                                                                                                                                                        ?></dd>
-                <?php else : ?>
-                    <dd class="<?php echo esc_attr(sanitize_html_class($class_value)); ?> tc-hidden-variation">&nbsp;</dd>
-                <?php endif; ?>
-            <?php endforeach; ?>
-        <?php endif; ?>
-    </dl>
+    <div class="overflow-hidden h-0 transition-all duration-300" data-js="dropdown-container">
+        <dl class="tc-epo-metadata variation text-sm p-3">
+            <?php
+            if (isset($item_data)) :
+                foreach ($item_data as $data) :
+                    $is_epo      = false;
+                    $show_dt     = true;
+                    $show_dd     = true;
+                    $class_name  = '';
+                    $class_value = '';
+                    if (isset($data['tm_label'])) {
+                        $is_epo      = true;
+                        $class_name  = 'tc-name ';
+                        $class_value = 'tc-value ';
+                    }
+        
+                    if (!isset($data['display']) && isset($data['value'])) {
+                        $data['display'] = $data['value'];
+                    }
+                    if ($is_epo && '' === $data['key']) {
+                        $show_dt = false;
+                    }
+                    if ($is_epo && '' === $data['display']) {
+                        $show_dd = false;
+                    }
+                    if (('link' === THEMECOMPLETE_EPO_DATA_STORE()->get('tm_epo_cart_field_display') && isset($data['popuplink'])) || !$show_dd) {
+                        $separator = '';
+                    }
+        
+                    // $data['key'] and $data['display'] contians HTML code
+                    // thus the use of wp_kses_post and not esc_html
+            ?>
+                    <?php if ($show_dt) : ?>
+                        <dt class="<?php echo esc_attr(sanitize_html_class($class_name)); ?> !mr-3 font-semibold variation-<?php echo esc_attr(sanitize_html_class($data['key'])); ?>"><?php echo apply_filters('wc_epo_kses', wp_kses_post($data['key']), $data['key'], false); // phpcs:ignore WordPress.Security.EscapeOutput 
+                                                                                                                                                                            ?><?php echo esc_html($separator); ?></dt>
+                    <?php else : ?>
+                        <dt class="<?php echo esc_attr(sanitize_html_class($class_name)); ?> tc-hidden-variation">&nbsp;</dt>
+                    <?php endif; ?>
+                    <?php if ($show_dd) : ?>
+                        <dd class="<?php echo esc_attr(sanitize_html_class($class_value)); ?>!mb-2 variation-<?php echo esc_attr(sanitize_html_class($data['key'])); ?>"><?php echo apply_filters('wc_epo_kses', wp_kses_post(wpautop($data['display'])), wpautop($data['display']), false); // phpcs:ignore WordPress.Security.EscapeOutput 
+                                                                                                                                                                            ?></dd>
+                    <?php else : ?>
+                        <dd class="<?php echo esc_attr(sanitize_html_class($class_value)); ?> tc-hidden-variation">&nbsp;</dd>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </dl>
+    </div>
 </div>
