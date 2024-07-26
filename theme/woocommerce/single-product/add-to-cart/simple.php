d7987a2ceeab625d5789a46cd342f6019a4cede8
Kamil <k.medrala99@gmail.com>
2024-07-26 15:19:26 +0200

    [SMOOTH-85] basic calculator price preview

--- theme/woocommerce/single-product/add-to-cart/simple.php
+++ theme/woocommerce/single-product/add-to-cart/simple.php
@@ -70,10 +70,6 @@ if ($product->is_in_stock() && $has_variable_price == false && !$product->is_dow
 				</svg>
 			</button>
 
-			<?php
-				if ($product->get_id() == 1253) :
-			?>
-
 			<div data-js-calc-container class="hidden p-5 lg:px-8 rounded-2xl border-2 border-[#EFEFEF] bg-white w-full drop-shadow-2xl">
 				<h4 class="text-2xl md:text-3xl font-semibold text-primary mb-4"><?php echo esc_html_e('Your calculation:', 'smoothh'); ?></h4>
 				<div class="flex flex-col md:flex-row border-b border-b-secondary mb-2 gap-5 pb-3">
@@ -107,14 +103,14 @@ if ($product->is_in_stock() && $has_variable_price == false && !$product->is_dow
 				</div>
 
 				<div class="flex justify-center flex-wrap gap-5">
-					<button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" class=" single_add_to_cart_button button alt<?php echo esc_attr(wc_wp_theme_get_element_class_name('button') ? ' ' . wc_wp_theme_get_element_class_name('button') : ''); ?> whitespace-nowrap button border-none !bg-gradient-to-b from-primary via-secondary to-secondary bg-size-200 bg-pos-0 hover:bg-pos-100 focus:bg-pos-100  disabled:!bg-[#C9C9C9] [&.disabled]:!bg-[#C9C9C9] disabled:!bg-none [&.disabled]:!bg-none disabled:!opacity-100 [&.disabled]:!opacity-100  transition-all duration-200 text-white h-[55px] !px-5 xl:!px-12 xl:!pr-8 !rounded-[15px] font-bold !flex items-center justify-center gap-5 alt<?php echo esc_attr(wc_wp_theme_get_element_class_name('button') ? ' ' . wc_wp_theme_get_element_class_name('button') : ''); ?>"><?php echo esc_html_e('Complete the quote', 'smoothh'); ?>
-						<svg class="shrink-0 -rotate-90" width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<circle class="stroke-white" cx="9.5" cy="9.5" r="9"></circle>
-							<path class="fill-white" d="M9 12.986L5.75 7.5H7.7L9.468 10.451L11.314 7.5H13.16L9.845 12.986H9Z"></path>
-						</svg>
-					</button>
-
 					<?php if ($product->get_id() == 1253) : ?>
+						<button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" class=" single_add_to_cart_button button alt<?php echo esc_attr(wc_wp_theme_get_element_class_name('button') ? ' ' . wc_wp_theme_get_element_class_name('button') : ''); ?> whitespace-nowrap button border-none !bg-gradient-to-b from-primary via-secondary to-secondary bg-size-200 bg-pos-0 hover:bg-pos-100 focus:bg-pos-100  disabled:!bg-[#C9C9C9] [&.disabled]:!bg-[#C9C9C9] disabled:!bg-none [&.disabled]:!bg-none disabled:!opacity-100 [&.disabled]:!opacity-100  transition-all duration-200 text-white h-[55px] !px-5 xl:!px-12 xl:!pr-8 !rounded-[15px] font-bold !flex items-center justify-center gap-5 alt<?php echo esc_attr(wc_wp_theme_get_element_class_name('button') ? ' ' . wc_wp_theme_get_element_class_name('button') : ''); ?>"><?php echo esc_html_e('Complete the quote', 'smoothh'); ?>
+							<svg class="shrink-0 -rotate-90" width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<circle class="stroke-white" cx="9.5" cy="9.5" r="9"></circle>
+								<path class="fill-white" d="M9 12.986L5.75 7.5H7.7L9.468 10.451L11.314 7.5H13.16L9.845 12.986H9Z"></path>
+							</svg>
+						</button>
+
 						<button data-js-popup-toggle="negotiate-form" type="button" value="<?php echo esc_attr($product->get_id()); ?>" class="!hidden whitespace-nowrap border-none !bg-gradient-to-b from-primary via-secondary to-secondary bg-size-200 bg-pos-0 hover:bg-pos-100 focus:bg-pos-100  disabled:!bg-[#C9C9C9] [&.disabled]:!bg-[#C9C9C9] disabled:!bg-none [&.disabled]:!bg-none disabled:!opacity-100 [&.disabled]:!opacity-100  transition-all duration-200 text-white h-[55px] !px-5 xl:!px-12 xl:!pr-8 !rounded-[15px] font-bold flex items-center justify-center gap-5">
 							<?php echo esc_html_e('Negotiate price', 'smoothh'); ?>
 							<svg class="shrink-0 -rotate-90" width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -122,25 +118,20 @@ if ($product->is_in_stock() && $has_variable_price == false && !$product->is_dow
 								<path class="fill-white" d="M9 12.986L5.75 7.5H7.7L9.468 10.451L11.314 7.5H13.16L9.845 12.986H9Z"></path>
 							</svg>
 						</button>
+					<?php else : 
+						$panel_page_id = 650;
+					?>
+
+							<a href="<?php echo get_permalink(wc_get_page_id('myaccount')); ?>?redirect_to=<?php echo get_permalink($panel_page_id) ?>" class="button mx-auto w-fit whitespace-nowrap button border-none !bg-gradient-to-b from-primary via-secondary to-secondary bg-size-200 bg-pos-0 hover:bg-pos-100 focus:bg-pos-100  disabled:!bg-[#C9C9C9] [&.disabled]:!bg-[#C9C9C9] disabled:!bg-none [&.disabled]:!bg-none disabled:!opacity-100 [&.disabled]:!opacity-100  transition-all duration-200 !text-white h-[55px] !px-5 xl:!px-12 xl:!pr-8 !rounded-[15px] font-bold !flex items-center justify-center gap-5">
+								<?php echo esc_html_e('Sign in and order recruitment', 'smoothh'); ?>
+								<svg class="shrink-0 -rotate-90" width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<circle class="stroke-white" cx="9.5" cy="9.5" r="9"></circle>
+									<path class="fill-white" d="M9 12.986L5.75 7.5H7.7L9.468 10.451L11.314 7.5H13.16L9.845 12.986H9Z"></path>
+								</svg>
+							</a>
 					<?php endif; ?>
 				</div>
 			</div>
-			
-			<?php else : 
-				$panel_page_id = 650;
-			?>
-
-			<div data-js-calc-container class="hidden">
-				<a href="<?php echo get_permalink(wc_get_page_id('myaccount')); ?>?redirect_to=<?php echo get_permalink($panel_page_id) ?>" class="button mx-auto w-fit whitespace-nowrap button border-none !bg-gradient-to-b from-primary via-secondary to-secondary bg-size-200 bg-pos-0 hover:bg-pos-100 focus:bg-pos-100  disabled:!bg-[#C9C9C9] [&.disabled]:!bg-[#C9C9C9] disabled:!bg-none [&.disabled]:!bg-none disabled:!opacity-100 [&.disabled]:!opacity-100  transition-all duration-200 !text-white h-[55px] !px-5 xl:!px-12 xl:!pr-8 !rounded-[15px] font-bold !flex items-center justify-center gap-5">
-					<?php echo esc_html_e('Sign in and order recruitment', 'smoothh'); ?>
-					<svg class="shrink-0 -rotate-90" width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<circle class="stroke-white" cx="9.5" cy="9.5" r="9"></circle>
-						<path class="fill-white" d="M9 12.986L5.75 7.5H7.7L9.468 10.451L11.314 7.5H13.16L9.845 12.986H9Z"></path>
-					</svg>
-				</a>
-			</div>
-
-			<?php endif; ?>
 
 		<?php else : ?>
 
