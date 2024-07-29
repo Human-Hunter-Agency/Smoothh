e893d83fae2077dc28a79676d9ab040300898dcd
Tomek <TK-54-4B>
2024-07-29 17:57:42 +0200

    removed esc_html

--- theme/flexible-content/sections/cta-footer.php
+++ theme/flexible-content/sections/cta-footer.php
@@ -13,7 +13,7 @@ $isAboveFooter = $args['isAboveFooter'];
     <div class="mx-auto w-full <?php echo $isAboveFooter == true ? 'mb-[-80px]' : 'mb-20'; ?> px-4 md:px-8 lg:px-24 py-8 md:py-14 flex flex-col md:flex-row gap-2 items-center justify-between drop-shadow-2xl rounded-3xl bg-gradient-to-b from-secondary to-primary">
         <?php if ($header) : ?>
             <div>
-                <h3 class="mb-0 text-xl sm:text-2xl md:text-3xl lg:text-5xl text-bold text-white font-bold text-center md:text-left"><?php echo esc_html($header); ?></h3>
+                <h3 class="mb-0 text-xl sm:text-2xl md:text-3xl lg:text-5xl text-bold text-white font-bold text-center md:text-left"><?php echo $header; ?></h3>
                 <?php if ($description) : ?>
                     <p class="mt-1 text-[16px] text-white font-normal text-center md:text-left"><?php echo $description; ?></p>
                 <?php endif; ?>
