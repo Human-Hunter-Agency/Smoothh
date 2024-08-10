af84d777c4b4077ce1580a05f40657ecc2b443be
Tomek <TK-54-4B>
2024-08-10 17:16:07 +0200

    case study list new design

--- theme/flexible-content/sections/list-case-studies.php
+++ theme/flexible-content/sections/list-case-studies.php
@@ -5,7 +5,6 @@
 $section_ID = $args['section_ID'];
 $header = $args['header'];
 $retainer_fee = $args['retainer_fee'];
-$retainer_fee_value = $args['retainer_fee_value'];
 $realization_time = $args['realization_time'];
 $location = $args['location'];
 
@@ -34,8 +33,7 @@ $posts = get_posts(array(
                                 <div class="hidden z-0 absolute inset-0 bg-gradient-to-b from-secondary to-primary mix-blend-multiply opacity-90"></div>
                                 <h4 class="p-6 z-[1] relative text-center text-lg md:text-[28px] text-white font-semibold"><?php echo $post->post_title; ?></h4>
                                 <div class="about p-[24px_30px] text-center">
-                                    <p class="hidden"><?php echo get_the_excerpt($post->ID); ?></p>
-                                    <p class="font-medium"><?php echo $retainer_fee . ':'; ?><span class="text-primary"><?php echo $retainer_fee_value; ?></span></p>
+                                    <p class="font-medium"><?php echo esc_html_e('Success fee:', 'smoothh'); ?><span class="text-primary"><?php echo $retainer_fee; ?></span></p>
                                 </div>
 
                                 <span href="<?php echo get_permalink($post->ID); ?>"><?php esc_html_e('Read more', 'smoothh'); ?><span class="!-ml-4">></span></span>
