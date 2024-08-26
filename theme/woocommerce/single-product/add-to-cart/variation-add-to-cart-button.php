7b14e85021d98763dfb70d836ce9010f845fa433
Kamil <k.medrala99@gmail.com>
2024-08-26 15:01:16 +0200

    SMOOTHH-109 hide qty buttons

--- theme/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ theme/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -20,15 +20,18 @@ $is_hourly = get_field('product_hourly', $product->get_id());
 	<?php
 	do_action('woocommerce_before_add_to_cart_quantity');
 
-	if (isset($is_hourly) && $is_hourly) {
-		woocommerce_quantity_input(
+	if (isset($is_hourly) && $is_hourly): ?> 
+	<div class="max-[500px]:[&_button]:hidden">
+		<?php woocommerce_quantity_input(
 			array(
 				'min_value'   => apply_filters('woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product),
 				'max_value'   => apply_filters('woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product),
 				'input_value' => isset($_POST['quantity']) ? wc_stock_amount(wp_unslash($_POST['quantity'])) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
 			)
-		);
-	}
+		); ?>
+	</div>
+	<?php
+	endif;
 
 	do_action('woocommerce_after_add_to_cart_quantity');
 	?>
