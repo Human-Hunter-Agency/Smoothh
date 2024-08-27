1678e81be11bc6119192919b6cdc0e4fb013b4fc
Kamil <k.medrala99@gmail.com>
2024-08-27 15:12:38 +0200

    SMOOTHH-115 pasword reset form styles

--- theme/woocommerce/myaccount/form-reset-password.php
+++ theme/woocommerce/myaccount/form-reset-password.php
@@ -28,10 +28,10 @@ defined('ABSPATH') || exit; ?>
 
 		<p class="mb-5 text-[20px] font-semibold"><?php echo apply_filters('woocommerce_reset_password_message', esc_html__('Enter a new password below.', 'woocommerce')); ?></p><?php  ?>
 
-		<p class="woocommerce-form-row woocommerce-form-row--first form-row p-0 !mb-5 w-full max-w-80">
+		<p class="woocommerce-form-row woocommerce-form-row--first form-row p-0 !mb-3 w-full max-w-80">
 			<input type="password" class="woocommerce-Input woocommerce-Input--text input-text placeholder:text-foreground h-[55px] rounded-[15px] border border-primary pl-5 pr-10 transition-all duration-200 hover:border-secondary accent-primary w-full !outline-1 !-outline-offset-2 !outline-transparent ![outline-style:solid] focus:!outline-1 focus:border-secondary focus:!outline-secondary " name="password_1" id="password_1" autocomplete="new-password" placeholder="<?php esc_html_e('New password', 'woocommerce'); ?> *" />
 		</p>
-		<p class="woocommerce-form-row woocommerce-form-row--last form-row p-0 !mb-10 w-full max-w-80">
+		<p class="woocommerce-form-row woocommerce-form-row--last form-row p-0 !mb-8 w-full max-w-80">
 			<input type="password" class="woocommerce-Input woocommerce-Input--text input-text placeholder:text-foreground h-[55px] rounded-[15px] border border-primary pl-5 pr-10 transition-all duration-200 hover:border-secondary accent-primary w-full !outline-1 !-outline-offset-2 !outline-transparent ![outline-style:solid] focus:!outline-1 focus:border-secondary focus:!outline-secondary " name="password_2" id="password_2" autocomplete="new-password" placeholder="<?php esc_html_e('Re-enter new password', 'woocommerce'); ?> *" />
 		</p>
 
