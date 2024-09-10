ea38ce650ad4f6e196676321303df6be4d667b47
Kamil <k.medrala99@gmail.com>
2024-09-10 11:58:51 +0200

    remove search

--- theme/functions.php
+++ theme/functions.php
@@ -1406,8 +1406,8 @@ function wpb_filter_query($query, $error = true)
 {
 	if (is_search()) {
 		$query->is_search = false;
-		$query->query_vars[s] = false;
-		$query->query[s] = false;
+		$query->query_vars['s'] = false;
+		$query->query['s'] = false;
 		if ($error == true)
 			$query->is_404 = true;
 	}
