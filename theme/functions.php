46ec4e54b21b1e45c15293185772daec009ebbcc
Kamil <k.medrala99@gmail.com>
2024-09-10 11:53:07 +0200

    remove search

--- theme/functions.php
+++ theme/functions.php
@@ -1399,3 +1399,23 @@ function filter_wc_order_is_editable($editable, $order)
 	return $editable;
 }
 add_filter('wc_order_is_editable', 'filter_wc_order_is_editable', 10, 2);
+
+
+// Remove search
+function wpb_filter_query($query, $error = true)
+{
+	if (is_search()) {
+		$query->is_search = false;
+		$query->query_vars[s] = false;
+		$query->query[s] = false;
+		if ($error == true)
+			$query->is_404 = true;
+	}
+}
+add_action('parse_query', 'wpb_filter_query');
+add_filter('get_search_form', create_function('$a', "return null;"));
+function remove_search_widget()
+{
+	unregister_widget('WP_Widget_Search');
+}
+add_action('widgets_init', 'remove_search_widget');
