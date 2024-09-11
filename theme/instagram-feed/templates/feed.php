c0ebd0f83398425f7e1c1cde9b18435343880df0
Kamil <k.medrala99@gmail.com>
2024-09-11 13:59:19 +0200

    instagram feed templates

---
+++ theme/instagram-feed/templates/feed.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Smash Balloon Instagram Feed Main Template
+ * Creates the wrapping HTML and adds settings as attributes
+ *
+ * @version 2.9 Instagram Feed by Smash Balloon
+ *
+ */
+// Don't load directly
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
+$feed_styles     = SB_Instagram_Display_Elements::get_feed_style( $settings ); // already escaped
+$feed_atts       = SB_Instagram_Display_Elements::get_feed_container_data_attributes( $settings );
+$sb_images_style = SB_Instagram_Display_Elements::get_sbi_images_style( $settings ); // already escaped
+$feed_classes    = SB_Instagram_Display_Elements::get_feed_container_css_classes( $settings, $additional_classes ); // already escaped
+
+/**
+ * Add HTML or execute code before the feed displays.
+ * sbi_after_feed works the same way but executes
+ * after the feed
+ *
+ * @param array $posts Instagram posts in feed
+ * @param array $settings settings specific to this feed
+ *
+ * @since 2.2
+ */
+do_action( 'sbi_before_feed', $posts, $settings );
+
+sbi_header_html( $settings, $header_data, 'outside' );
+?>
+
+<div id="sb_instagram" <?php echo $feed_classes . $feed_styles; ?> data-feedid="<?php echo esc_attr( $feed_id ); ?>" <?php echo $feed_atts; ?> data-shortcode-atts="<?php echo esc_attr( $shortcode_atts ); ?>" <?php echo $other_atts; ?>>
+	<?php sbi_header_html( $settings, $header_data ); ?>
+
+    <div id="sbi_images" >
+		<div class="relative z-0 w-full overflow-hidden pb-10 lg:!pb-32">
+			<?php if ($tiles_list) : ?>
+				<div class="swiper !container !overflow-visible" data-js="swiper-tiles-default">
+					<div class="swiper-wrapper <?php if (!$isSwiper) :  ?> xl:!transform-none xl:!flex-wrap xl:justify-center xl:gap-y-12 <?php endif; ?>">
+						
+						<?php
+							if ( ! in_array( 'ajaxPostLoad', $flags, true ) ) {
+								$this->posts_loop( $posts, $settings );
+							}
+						?>
+
+					</div>
+					<div class="swiper-nav w-[100px]  h-20 hidden lg:flex gap-5 !absolute !top-auto !bottom-[-120px] left-[50%] -translate-x-1/2 <?php if (!$isSwiper) :  ?> xl:hidden <?php endif; ?>">
+						<div class="swiper-button-prev rotate-180 scale-150">
+							<svg xmlns="http://www.w3.org/2000/svg" width="39" height="40" viewBox="0 0 39 40" fill="none">
+								<path d="M19.6177 38.1958C9.57757 38.1958 1.43846 30.0567 1.43846 20.0166C1.43846 9.97651 9.57757 1.8374 19.6177 1.8374C29.6578 1.8374 37.7969 9.97651 37.7969 20.0166C37.7969 30.0567 29.6578 38.1958 19.6177 38.1958Z" stroke="#8117EE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+								<path d="M20.1627 28.7426L28.8887 20.0165L20.1627 11.2905" stroke="#8117EE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+								<path d="M10.8917 20.0171H28.3438" stroke="#8117EE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+							</svg>
+						</div>
+						<div class="swiper-button-next scale-150">
+							<svg xmlns="http://www.w3.org/2000/svg" width="39" height="40" viewBox="0 0 39 40" fill="none">
+								<path d="M19.6177 38.1958C9.57757 38.1958 1.43846 30.0567 1.43846 20.0166C1.43846 9.97651 9.57757 1.8374 19.6177 1.8374C29.6578 1.8374 37.7969 9.97651 37.7969 20.0166C37.7969 30.0567 29.6578 38.1958 19.6177 38.1958Z" stroke="#8117EE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+								<path d="M20.1627 28.7426L28.8887 20.0165L20.1627 11.2905" stroke="#8117EE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+								<path d="M10.8917 20.0171H28.3438" stroke="#8117EE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+							</svg>
+						</div>
+					</div>
+				</div>
+			<?php endif; ?>
+		</div>
+    </div>
+
+	<?php include sbi_get_feed_template_part( 'footer', $settings ); ?>
+
+	<?php
+	/**
+	 * Things to add before the closing "div" tag for the main feed element. Several
+	 * features rely on this hook such as local images and some error messages
+	 *
+	 * @param object SB_Instagram_Feed
+	 * @param string $feed_id
+	 *
+	 * @since 2.1/5.2
+	 */
+	do_action( 'sbi_before_feed_end', $this, $feed_id ); ?>
+</div>
+
+<?php do_action( 'sbi_after_feed', $posts, $settings );?>
---
+++ theme/instagram-feed/templates/item.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Smash Balloon Instagram Feed Item Template
+ * Adds an image, link, and other data for each post in the feed
+ *
+ * @version 2.9 Instagram Feed by Smash Balloon
+ *
+ */
+
+// Don't load directly
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
+$classes = SB_Instagram_Display_Elements::get_item_classes( $settings, $post );
+$post_id = SB_Instagram_Parse::get_post_id( $post );
+$timestamp = SB_Instagram_Parse::get_timestamp( $post );
+$media_type = SB_Instagram_Parse::get_media_type( $post );
+$permalink = SB_Instagram_Parse::get_permalink( $post );
+$maybe_carousel_icon = $media_type === 'carousel' ? SB_Instagram_Display_Elements::get_icon( 'carousel', $icon_type ) : '';
+$maybe_video_icon = $media_type === 'video' ? SB_Instagram_Display_Elements::get_icon( 'video', $icon_type ) : '';
+$media_url = SB_Instagram_Display_Elements::get_optimum_media_url( $post, $settings, $resized_images );
+$media_full_res = SB_Instagram_Parse::get_media_url( $post );
+$media_all_sizes_json = SB_Instagram_Parse::get_media_src_set( $post, $resized_images );
+
+/**
+ * Text that appears in the "alt" attribute for this image
+ *
+ * @param string $img_alt full caption for post
+ * @param array $post api data for the post
+ *
+ * @since 2.1.5
+ */
+$img_alt = SB_Instagram_Parse::get_caption( $post, sprintf( __( 'Instagram post %s', 'instagram-feed' ), $post_id ) );
+$img_alt = apply_filters( 'sbi_img_alt', $img_alt, $post );
+
+/**
+ * Text that appears in the visually hidden screen reader element
+ *
+ * @param string $img_screenreader first 50 characters for post
+ * @param array $post api data for the post
+ *
+ * @since 2.1.5
+ */
+$img_screenreader = substr( SB_Instagram_Parse::get_caption( $post, sprintf( __( 'Instagram post %s', 'instagram-feed' ), $post_id ) ), 0, 50 );
+$img_screenreader = apply_filters( 'sbi_img_screenreader', $img_screenreader, $post );
+
+?>
+
+<div class="swiper-slide !h-auto min-h-[460px] md:min-h-[580px] !flex items-center flex-col bg-white drop-shadow-lg lg:shadow-2xl rounded-2xl">
+    <?php if ($tile['image'] && $tile['image']['url']) : ?>
+        <div class="w-full relative mb-5 rounded-t-[14px] overflow-hidden">
+            <img class="object-cover w-full !h-[190px] md:!h-[220px]" src="<?php echo esc_url( $media_url ); ?>" alt="<?php echo esc_attr( $img_alt ); ?>">
+            <div class="absolute inset-0 bg-gradient-to-b from-secondary to-primary mix-blend-multiply opacity-90"></div>
+        </div>
+    <?php endif; ?>
+    <div class="grow text-center px-3 md:px-6 pb-6 !pt-0 flex flex-col justify-between">
+        <div class="w-full">
+            <?php if ($tile['description']) : ?>
+                <p class="text-sm md:text-base"><?php echo esc_attr( $img_alt ); ?></p>
+            <?php endif; ?>
+        </div>
+
+            <a href="<?php echo esc_url( $permalink ); ?>" target="_blank" class="block w-[220px] mt-6 mx-auto rounded-[14px] text-base md:text-[20px] text-center font-bold py-3 px-7 text-white bg-secondary hover:bg-primary transition duration-200">
+                <?php echo __('See','smoothh'); ?>
+            </a>
+    </div>
+</div>
